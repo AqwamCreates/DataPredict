40b025db0e1c51fa6b83476d5f28070216449ac2
AqwamCreates <67371914+AqwamCreates@users.noreply.github.com>
2023-06-08 21:07:50 +0800

    Update AdaptiveMomentEstimation.lua

--- ModuleScripts/Optimizers/AdaptiveMomentEstimation.lua
+++ ModuleScripts/Optimizers/AdaptiveMomentEstimation.lua
@@ -6,7 +6,7 @@ local AqwamMatrixLibrary = require(script.Parent.Parent.AqwamRobloxMatrixLibrary
 
 local defaultBeta1 = 0.9
 
-local defaultBeta2 = 0.999
+local defaultBeta2 = 0.9
 
 function AdaptiveMomentEstimationOptimizer.new(Beta1, Beta2)
 
@@ -14,7 +14,9 @@ function AdaptiveMomentEstimationOptimizer.new(Beta1, Beta2)
 
 	setmetatable(NewAdaptiveMomentEstimationOptimizer, AdaptiveMomentEstimationOptimizer)
 
-	NewAdaptiveMomentEstimationOptimizer.PreviousSumOfGradientSquaredMatrix = nil
+	NewAdaptiveMomentEstimationOptimizer.PreviousMomentum = nil
+	
+	NewAdaptiveMomentEstimationOptimizer.PreviousVelocity = nil
 	
 	NewAdaptiveMomentEstimationOptimizer.Beta1 = Beta1 or defaultBeta1
 	
@@ -37,39 +39,47 @@ function AdaptiveMomentEstimationOptimizer:setBeta2(Beta2)
 end
 
 
-function AdaptiveMomentEstimationOptimizer:calculate(ModelParametersDerivatives)
+function AdaptiveMomentEstimationOptimizer:calculate(costFunctionDerivatives)
 
-	if (self.PreviousSumOfGradientSquaredMatrix == nil) then
+	self.PreviousMomentum = self.PreviousMomentum or AqwamMatrixLibrary:createMatrix(#costFunctionDerivatives, #costFunctionDerivatives[1])
+	
+	self.PreviousVelocity = self.PreviousVelocity or AqwamMatrixLibrary:createMatrix(#costFunctionDerivatives, #costFunctionDerivatives[1])
 
-		self.PreviousSumOfGradientSquaredMatrix = AqwamMatrixLibrary:createMatrix(#ModelParametersDerivatives, #ModelParametersDerivatives[1])
+	local momentumPart1 = AqwamMatrixLibrary:multiply(self.Beta1, self.PreviousMomentum)
+	
+	local momentumPart2 = AqwamMatrixLibrary:multiply((1 - self.Beta1), costFunctionDerivatives)
+	
+	local momentum = AqwamMatrixLibrary:add(momentumPart1, momentumPart2)
 
-	end
-	
-	local MomentumPart1Matrix = AqwamMatrixLibrary:multiply(self.Beta1, self.PreviousSumOfGradientSquaredMatrix)
-	
-	local MomentumPart2Matrix = AqwamMatrixLibrary:multiply((1 - self.Beta1), ModelParametersDerivatives)
-	
-	local MomentumMatrix = AqwamMatrixLibrary:add(MomentumPart1Matrix, MomentumPart2Matrix)
-	
-	local RMSPropPart1Matrix = AqwamMatrixLibrary:multiply(self.Beta2, self.PreviousSumOfGradientSquaredMatrix)
-	
-	local SquaredSumOfGradientMatrix = AqwamMatrixLibrary:power(ModelParametersDerivatives, 2)
-	
-	local RMSPropPart2Matrix = AqwamMatrixLibrary:multiply((1 - self.Beta2), SquaredSumOfGradientMatrix)
-	
-	local RMSPropMatrix = AqwamMatrixLibrary:add(RMSPropPart1Matrix, RMSPropPart2Matrix)
-	
-	local AdamMatrix = AqwamMatrixLibrary:divide(MomentumMatrix, RMSPropMatrix)
+	local squaredModelParameters = AqwamMatrixLibrary:power(costFunctionDerivatives, 2)
 
-	return AdamMatrix
+	local velocityPart1 = AqwamMatrixLibrary:multiply(self.Beta2, self.PreviousVelocity)
+	
+	local velocityPart2 = AqwamMatrixLibrary:multiply((1 - self.Beta2), squaredModelParameters)
+	
+	local velocity = AqwamMatrixLibrary:add(velocityPart1, velocityPart2)
 
+	local meanMomentum = AqwamMatrixLibrary:divide(momentum, (1 - self.Beta1))
+	
+	local meanVelocity = AqwamMatrixLibrary:divide(velocity, (1 - self.Beta2))
+	
+	local squareRootedDivisor = AqwamMatrixLibrary:power(meanVelocity, 0.5)
+
+	local updatedParameters = AqwamMatrixLibrary:divide(meanMomentum, squareRootedDivisor)
+
+	self.PreviousMomentum = momentum
+	
+	self.PreviousVelocity = velocity
+
+	return costFunctionDerivatives
 end
 
 function AdaptiveMomentEstimationOptimizer:reset()
 
-	self.PreviousSumOfGradientSquaredMatrix = nil
+	self.PreviousMomentum = nil
+
+	self.PreviousVelocity = nil
 
 end
 
 return AdaptiveMomentEstimationOptimizer
-

5fc3bee2d7a731695109f797d6db74ec9c523cfb
AqwamCreates <67371914+AqwamCreates@users.noreply.github.com>
2023-06-21 03:00:33 +0800

    Update RecurrentNeuralNetwork.lua

--- ModuleScripts/Models/RecurrentNeuralNetwork.lua
+++ ModuleScripts/Models/RecurrentNeuralNetwork.lua
@@ -148,7 +148,7 @@ function RecurrentNeuralNetworkModel:convertTokenToLogisticVector(token)
 
 	local logisticMatrix = AqwamMatrixLibrary:createMatrix(self.outputSize, 1)
 
-	if (type(token) == "number") then
+	if (token ~= nil) then
 
 		logisticMatrix[token][1] = 1
 

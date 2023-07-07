cf4d8e1ab3db0aece2fe7a110aa80a26b4d94cd7
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-07-08 00:55:52 +0800

    Update SupportVectorMachine2OneVsAll.lua

--- ModuleScripts/Models/SupportVectorMachine2OneVsAll.lua
+++ ModuleScripts/Models/SupportVectorMachine2OneVsAll.lua
@@ -1,4 +1,8 @@
-local SupportVectorMachine = require(script.Parent.SupportVectorMachine)
+local DataPredictLibrary = script.Parent.Parent
+
+local SupportVectorMachine = require(DataPredictLibrary.Models.SupportVectorMachine)
+
+local Optimizers = DataPredictLibrary.Optimizers
 
 local AqwamMatrixLibrary = require(script.Parent.Parent.AqwamRobloxMatrixLibraryLinker.Value)
 
@@ -39,33 +43,33 @@ local function getClassesList(labelVector)
 end
 
 local function convertToBinaryLabelVector(labelVector, selectedClass)
-	
+
 	local numberOfRows = #labelVector
-	
+
 	local newLabelVector = AqwamMatrixLibrary:createMatrix(numberOfRows, 1)
-	
+
 	for row = 1, numberOfRows, 1 do
-		
+
 		if (labelVector[row][1] == selectedClass) then
-			
+
 			newLabelVector[row][1] = 1
-			
+
 		else
-			
+
 			newLabelVector[row][1] = -1
-			
+
 		end
-		
+
 	end
-	
+
 	return newLabelVector
-	
+
 end
 
 function SupportVectorMachineOneVsAllModel.new(maxNumberOfIterations, learningRate, cValue, targetCost, kernelFunction, kernelParameters)
 
 	local NewSupportVectorMachineOneVsAll = {}
-	
+
 	setmetatable(NewSupportVectorMachineOneVsAll, SupportVectorMachineOneVsAllModel)
 
 	NewSupportVectorMachineOneVsAll.maxNumberOfIterations = maxNumberOfIterations or defaultMaxNumberOfIterations
@@ -85,7 +89,7 @@ function SupportVectorMachineOneVsAllModel.new(maxNumberOfIterations, learningRa
 	NewSupportVectorMachineOneVsAll.validationLabelVector = nil
 
 	NewSupportVectorMachineOneVsAll.Optimizer = nil
-	
+
 	NewSupportVectorMachineOneVsAll.IsOutputPrinted = false
 
 	return NewSupportVectorMachineOneVsAll
@@ -108,146 +112,138 @@ function SupportVectorMachineOneVsAllModel:setParameters(maxNumberOfIterations, 
 
 end
 
-function SupportVectorMachineOneVsAllModel:setOptimizer(Optimizer)
-
-	self.Optimizer = Optimizer
-
-end
-
 function SupportVectorMachineOneVsAllModel:train(featureMatrix, labelVector)
-	
+
 	local classesList = getClassesList(labelVector)
-	
+
 	table.sort(classesList, function(a,b) return a < b end)
-	
+
 	local total
-	
+
 	local totalCost
-	
+
 	local cost
-	
+
 	local costArray = {}
 	
+	local internalCostArray = {}
+
 	local ModelParameters = {}
-	
+
 	local SupportVectorMachineModel
-	
+
 	local SupportVectorMachineModelsArray = {}
-	
+
 	local binaryLabelVector
-	
+
 	local binaryLabelVectorTable = {}
-	
+
 	local ModelParametersVectorColumn
-	
+
 	local ModelParametersVectorRow
-	
+
 	local numberOfIterations = 0
-	
+
 	self.ClassesList = classesList
-	
+
 	for i, class in ipairs(classesList) do
-		
+
 		SupportVectorMachineModel = SupportVectorMachine.new(1, self.learningRate, self.cValue, self.targetCost, self.kernelFunction, self.kernelParameters)
-		
-		SupportVectorMachineModel:setOptimizer(self.Optimizer)
-		
-		SupportVectorMachineModel:setCValue(self.Regularization)
-		
+
 		SupportVectorMachineModel:setPrintOutput(false) 
-		
+
 		binaryLabelVector = convertToBinaryLabelVector(labelVector, class)
-		
+
 		table.insert(SupportVectorMachineModelsArray, SupportVectorMachineModel)
-		
+
 		table.insert(binaryLabelVectorTable, binaryLabelVector)
-		
+
 	end
-	
+
 	repeat
 
 		numberOfIterations += 1
-		
+
 		totalCost = 0
-		
+
 		for i, class in ipairs(classesList) do
-			
+
 			binaryLabelVector = binaryLabelVectorTable[i]
-			
+
 			SupportVectorMachineModel = SupportVectorMachineModelsArray[i]
 
-			cost = SupportVectorMachineModel:train(featureMatrix, binaryLabelVector)
-			
-			cost = cost[1]
-			
+			internalCostArray = SupportVectorMachineModel:train(featureMatrix, binaryLabelVector)
+
+			cost = internalCostArray[1]
+
 			totalCost += cost
-			
+
 		end
-		
+
 		if self.IsOutputPrinted then print("Iteration: " .. numberOfIterations .. "\t\tCost: " .. cost) end
-		
+
 		table.insert(costArray, totalCost)
 
 	until (numberOfIterations == self.maxNumberOfIterations) or (math.abs(totalCost) <= self.targetCost)
-	
+
 	if (cost == math.huge) then warn("The model diverged! Please repeat the experiment again or change the argument values.") end
-	
+
 	for i, class in ipairs(classesList) do
-		
+
 		SupportVectorMachineModel = SupportVectorMachineModelsArray[i]
-		
+
 		ModelParametersVectorColumn = SupportVectorMachineModel:getModelParameters()
 
 		ModelParametersVectorRow = AqwamMatrixLibrary:transpose(ModelParametersVectorColumn)
 
 		table.insert(ModelParameters, ModelParametersVectorRow[1])
-		
+
 	end
-	
+
 	self.ModelParameters = AqwamMatrixLibrary:transpose(ModelParameters)
-	
+
 	return costArray
-	
+
 end
 
 function SupportVectorMachineOneVsAllModel:predict(featureMatrix)
-	
+
 	local hypothesis
-	
+
 	local highestClass
-	
+
 	local longestDistance = -math.huge
-	
+
 	local hypothesisVector = AqwamMatrixLibrary:dotProduct(featureMatrix, self.ModelParameters)
-	
+
 	for column = 1, #hypothesisVector[1], 1 do
-		
+
 		hypothesis = hypothesisVector[1][column]
-		
+
 		if (hypothesis > 0) and (hypothesis > longestDistance) then
-			
+
 			highestClass = self.ClassesList[column]
-			
+
 			longestDistance = hypothesis
-			
+
 		end
-		
+
 	end
-	
+
 	return highestClass, longestDistance
-	
+
 end
 
 function SupportVectorMachineOneVsAllModel:getModelParameters()
-	
+
 	return self.ModelParameters
-	
+
 end
 
 function SupportVectorMachineOneVsAllModel:getClassesList()
-	
+
 	return self.ClassesList
-	
+
 end
 
 function SupportVectorMachineOneVsAllModel:setModelParameters(ModelParameters)

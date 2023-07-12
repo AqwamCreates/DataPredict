9191a77723f9b834ca2244071c168ea6f623ab66
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-07-12 08:36:50 +0800

    Update LogisticRegressionOneVsAll.lua

--- ModuleScripts/Models/LogisticRegressionOneVsAll.lua
+++ ModuleScripts/Models/LogisticRegressionOneVsAll.lua
@@ -1,4 +1,10 @@
-local LogisticRegression = require(script.Parent.LogisticRegression)
+local BaseModel = require(script.Parent.BaseModel)
+
+LogisticRegressionModel = {}
+
+LogisticRegressionModel.__index = LogisticRegressionModel
+
+setmetatable(LogisticRegressionModel, BaseModel)
 
 local AqwamMatrixLibrary = require(script.Parent.Parent.AqwamRobloxMatrixLibraryLinker.Value)
 
@@ -10,109 +16,92 @@ local defaultSigmoidFunction = "sigmoid"
 
 local defaultTargetCost = 0
 
-local defaultLambda = 0
-
 local sigmoidFunctionList = {
 
 	["sigmoid"] = function (z) return 1/(1+math.exp(-1 * z)) end,
 
 }
 
-local LogisticRegressionOneVsAllModel = {}
-
-LogisticRegressionOneVsAllModel.__index = LogisticRegressionOneVsAllModel
-
-local function getClassesList(labelVector)
-
-	local classesList = {}
-
-	local value
-
-	for i = 1, #labelVector, 1 do
-
-		value = labelVector[i][1]
-
-		if not table.find(classesList, value) then
-
-			table.insert(classesList, value)
-
-		end
-
-	end
-
-	return classesList
-
-end
-
-local function convertToBinaryLabelVector(labelVector, selectedClass)
+local lossFunctionList = {
 	
-	local numberOfRows = #labelVector
+	["sigmoid"] = function (y, h) return -(y * math.log10(h) + (1 - y) * math.log10(1 - h)) end
 	
-	local newLabelVector = AqwamMatrixLibrary:createMatrix(numberOfRows, 1)
 	
-	for row = 1, numberOfRows, 1 do
+}
+
+local function calculateHypothesisVector(featureMatrix, modelParameters, sigmoidFunction)
+	
+	local numberOfData = #featureMatrix
+	
+	local zVector = AqwamMatrixLibrary:dotProduct(featureMatrix, modelParameters)
+	
+	if (type(zVector) == "number") then zVector = {{zVector}} end
 		
-		if (labelVector[row][1] == selectedClass) then
-			
-			newLabelVector[row][1] = 1
-			
-		else
-			
-			newLabelVector[row][1] = 0
-			
-		end
-		
-	end
-	
-	return newLabelVector
-	
-end
-
-local function softMax(matrix)
-	
-	local e = AqwamMatrixLibrary:applyFunction(math.exp, matrix)
-
-	local eSum = AqwamMatrixLibrary:sum(e)
-
-	local result = AqwamMatrixLibrary:divide(e, eSum)
+	local result = AqwamMatrixLibrary:applyFunction(sigmoidFunctionList[sigmoidFunction], zVector)
 	
 	return result
 	
 end
 
-function LogisticRegressionOneVsAllModel.new(maxNumberOfIterations, learningRate, sigmoidFunction, targetCost)
-
-	local NewLogisticRegressionOneVsAllModel = {}
-
-	setmetatable(NewLogisticRegressionOneVsAllModel, LogisticRegressionOneVsAllModel)
-
-	NewLogisticRegressionOneVsAllModel.maxNumberOfIterations = maxNumberOfIterations or defaultMaxNumberOfIterations
-
-	NewLogisticRegressionOneVsAllModel.learningRate = learningRate or defaultLearningRate
-
-	NewLogisticRegressionOneVsAllModel.sigmoidFunction = sigmoidFunction or defaultSigmoidFunction
-
-	NewLogisticRegressionOneVsAllModel.targetCost = targetCost or defaultTargetCost
-
-	NewLogisticRegressionOneVsAllModel.validationFeatureMatrix = nil
-
-	NewLogisticRegressionOneVsAllModel.validationLabelVector = nil
-
-	NewLogisticRegressionOneVsAllModel.Optimizer = nil
-
-	NewLogisticRegressionOneVsAllModel.Regularization = nil
+local function calculateCost(modelParameters, featureMatrix, labelVector, sigmoidFunction)
 	
-	NewLogisticRegressionOneVsAllModel.ModelParameters = nil
+	local numberOfData = #featureMatrix
 	
-	NewLogisticRegressionOneVsAllModel.ClassesList = {}
+	local hypothesisVector = calculateHypothesisVector(featureMatrix, modelParameters, sigmoidFunction)
 	
-	NewLogisticRegressionOneVsAllModel.IsOutputPrinted = true
+	local costVector = AqwamMatrixLibrary:applyFunction(lossFunctionList[sigmoidFunction], labelVector, hypothesisVector)
 
-	return NewLogisticRegressionOneVsAllModel
-
+	local totalCost = AqwamMatrixLibrary:sum(costVector)
+	
+	local averageCost = totalCost / numberOfData
+	
+	return averageCost
+	
 end
 
-function LogisticRegressionOneVsAllModel:setParameters(maxNumberOfIterations, learningRate, sigmoidFunction, targetCost)
+local function gradientDescent(modelParameters, featureMatrix, labelVector, sigmoidFunction)
+	
+	local numberOfData = #featureMatrix
+
+	local hypothesisVector = calculateHypothesisVector(featureMatrix, modelParameters, sigmoidFunction)
+
+	local calculatedError = AqwamMatrixLibrary:subtract(hypothesisVector, labelVector)
+	
+	local calculatedErrorWithFeatureMatrix = AqwamMatrixLibrary:dotProduct(AqwamMatrixLibrary:transpose(featureMatrix), calculatedError)
+
+	local costFunctionDerivatives = AqwamMatrixLibrary:multiply((1/numberOfData),  calculatedErrorWithFeatureMatrix)
+	
+	return costFunctionDerivatives
+	
+end
+
+function LogisticRegressionModel.new(maxNumberOfIterations, learningRate, sigmoidFunction, targetCost)
+	
+	local NewLogisticRegressionModel = BaseModel.new()
+
+	setmetatable(NewLogisticRegressionModel, LogisticRegressionModel)
+
+	NewLogisticRegressionModel.maxNumberOfIterations = maxNumberOfIterations or defaultMaxNumberOfIterations
+
+	NewLogisticRegressionModel.learningRate = learningRate or defaultLearningRate
+
+	NewLogisticRegressionModel.sigmoidFunction = sigmoidFunction or defaultSigmoidFunction
+
+	NewLogisticRegressionModel.targetCost = targetCost or defaultTargetCost
+
+	NewLogisticRegressionModel.validationFeatureMatrix = nil
+
+	NewLogisticRegressionModel.validationLabelVector = nil
+	
+	NewLogisticRegressionModel.Optimizer = nil
+	
+	NewLogisticRegressionModel.Regularization = nil
+	
+	return NewLogisticRegressionModel
+	
+end
+
+function LogisticRegressionModel:setParameters(maxNumberOfIterations, learningRate, sigmoidFunction, targetCost)
 
 	self.maxNumberOfIterations = maxNumberOfIterations or self.maxNumberOfIterations
 
@@ -124,176 +113,118 @@ function LogisticRegressionOneVsAllModel:setParameters(maxNumberOfIterations, le
 
 end
 
-function LogisticRegressionOneVsAllModel:setRegularization(Regularization)
+function LogisticRegressionModel:setOptimizer(Optimizer)
+
+	self.Optimizer = Optimizer
+
+end
+
+function LogisticRegressionModel:setRegularization(Regularization)
 
 	self.Regularization = Regularization
 
 end
 
-function LogisticRegressionOneVsAllModel:train(featureMatrix, labelVector)
-	
-	local classesList = getClassesList(labelVector)
-	
-	table.sort(classesList, function(a,b) return a < b end)
-	
-	local total
-	
-	local totalCost
-	
+function LogisticRegressionModel:train(featureMatrix, labelVector)
+
 	local cost
 	
 	local costArray = {}
 	
-	local internalCostArray = {}
-	
-	local ModelParameters = {}
-	
-	local LogisticRegressionModel
-	
-	local LogisticRegressionModelsArray = {}
-	
-	local binaryLabelVector
-	
-	local binaryLabelVectorTable = {}
-	
-	local ModelParametersVectorColumn
-	
-	local ModelParametersVectorRow
-	
 	local numberOfIterations = 0
 	
-	self.ClassesList = classesList
+	local costFunctionDerivatives
 	
-	for i, class in ipairs(classesList) do
-		
-		LogisticRegressionModel = LogisticRegression.new(1, self.learningRate, self.sigmoidFunction, self.targetCost)
-		
-		LogisticRegressionModel:setRegularization(self.Regularization)
-		
-		LogisticRegressionModel:setPrintOutput(false) 
-		
-		binaryLabelVector = convertToBinaryLabelVector(labelVector, class)
-		
-		table.insert(LogisticRegressionModelsArray, LogisticRegressionModel)
-		
-		table.insert(binaryLabelVectorTable, binaryLabelVector)
-		
+	local numberOfData = #featureMatrix[1]
+	
+	local previousCostFunctionDerivatives
+	
+	local lambda
+	
+	local regularizationCost
+	
+	local RegularizationDerivatives
+	
+	if (#featureMatrix ~= #labelVector) then error("The feature matrix and the label vector does not contain the same number of rows!") end
+
+	if (self.ModelParameters) then
+
+		if (#featureMatrix[1] ~= #self.ModelParameters) then error("The number of features are not the same as the model parameters!") end
+
+	else
+
+		self.ModelParameters = self:initializeMatrixBasedOnMode(#featureMatrix[1], 1)
+
 	end
 	
 	repeat
-
+		
+		self:iterationWait()
+		
 		numberOfIterations += 1
 		
-		totalCost = 0
+		costFunctionDerivatives = gradientDescent(self.ModelParameters, featureMatrix, labelVector, self.sigmoidFunction)
 		
-		for i, class in ipairs(classesList) do
-			
-			binaryLabelVector = binaryLabelVectorTable[i]
-			
-			LogisticRegressionModel = LogisticRegressionModelsArray[i]
+		costFunctionDerivatives = AqwamMatrixLibrary:multiply(self.learningRate, costFunctionDerivatives)
+		
+		if (self.Optimizer) then 
 
-			internalCostArray = LogisticRegressionModel:train(featureMatrix, binaryLabelVector)
-			
-			cost = internalCostArray[1]
-			
-			totalCost += cost
-			
+			costFunctionDerivatives = self.Optimizer:calculate(costFunctionDerivatives, previousCostFunctionDerivatives) 
+
 		end
 		
-		if self.IsOutputPrinted then print("Iteration: " .. numberOfIterations .. "\t\tCost: " .. cost) end
-		
-		table.insert(costArray, totalCost)
+		if (self.Regularization) then
 
-	until (numberOfIterations == self.maxNumberOfIterations) or (math.abs(totalCost) <= self.targetCost)
+			RegularizationDerivatives = self.Regularization:calculateCostFunctionDerivativeRegularization(self.ModelParameters, numberOfData)
+
+			costFunctionDerivatives = AqwamMatrixLibrary:add(costFunctionDerivatives, RegularizationDerivatives)
+
+		end
+		
+		previousCostFunctionDerivatives = costFunctionDerivatives
+
+		self.ModelParameters = AqwamMatrixLibrary:subtract(self.ModelParameters, costFunctionDerivatives)
+		
+		cost = calculateCost(self.ModelParameters, featureMatrix, labelVector, self.sigmoidFunction)
+		
+		if (self.Regularization) then 
+
+			regularizationCost = self.Regularization:calculateCostFunctionRegularization(self.ModelParameters, numberOfData)
+
+			cost += regularizationCost
+
+		end
+		
+		table.insert(costArray, cost)
+		
+		self:printCostAndNumberOfIterations(cost, numberOfIterations)
+		
+	until (numberOfIterations == self.maxNumberOfIterations) or (math.abs(cost) <= self.targetCost)
 	
 	if (cost == math.huge) then warn("The model diverged! Please repeat the experiment again or change the argument values.") end
 	
-	for i, class in ipairs(classesList) do
-		
-		LogisticRegressionModel = LogisticRegressionModelsArray[i]
-		
-		ModelParametersVectorColumn = LogisticRegressionModel:getModelParameters()
-
-		ModelParametersVectorRow = AqwamMatrixLibrary:transpose(ModelParametersVectorColumn)
-
-		table.insert(ModelParameters, ModelParametersVectorRow[1])
-		
-	end
-	
-	self.ModelParameters = AqwamMatrixLibrary:transpose(ModelParameters)
+	if self.Optimizer then self.Optimizer:reset() end
 	
 	return costArray
 	
 end
 
-function LogisticRegressionOneVsAllModel:predict(featureMatrix)
+function LogisticRegressionModel:predict(featureMatrix)
 	
-	local highestClass
+	local z = AqwamMatrixLibrary:dotProduct(featureMatrix, self.ModelParameters)
 	
-	local probability
-
-	local highestProbability = -math.huge
+	local hypothesis = sigmoidFunctionList[self.sigmoidFunction](z)
 	
-	local zVector = AqwamMatrixLibrary:dotProduct(featureMatrix, self.ModelParameters)
-	
-	local zNormalVector = AqwamMatrixLibrary:normalizeMatrix(zVector)
-	
-	local softMaxVector = softMax(zNormalVector)
-	
-	for column = 1, #softMaxVector[1], 1 do
+	if (hypothesis >= 0.5) then
 		
-		probability = softMaxVector[1][column]
+		return 1
 		
-		if (probability > highestProbability) then
-			
-			highestClass = self.ClassesList[column]
-			
-			highestProbability = probability
-			
-		end
-		
-	end
-	
-	return highestClass, highestProbability
-	
-end
-
-function LogisticRegressionOneVsAllModel:getModelParameters()
-	
-	return self.ModelParameters
-	
-end
-
-function LogisticRegressionOneVsAllModel:getClassesList()
-	
-	return self.ClassesList
-	
-end
-
-function LogisticRegressionOneVsAllModel:setModelParameters(ModelParameters)
-
-	self.ModelParameters = ModelParameters
-
-end
-
-function LogisticRegressionOneVsAllModel:setClassesList(ClassesList)
-
-	self.ClassesList = ClassesList
-
-end
-
-function LogisticRegressionOneVsAllModel:setPrintOutput(option) 
-
-	if (option == false) then
-
-		self.IsOutputPrinted = false
-
 	else
-
-		self.IsOutputPrinted = true
-
+		
+		return 0
+		
 	end
 
 end
 
-return LogisticRegressionOneVsAllModel
+return LogisticRegressionModel

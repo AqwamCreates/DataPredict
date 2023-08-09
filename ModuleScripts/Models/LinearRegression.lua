ec44502ccea85adea2457325858f37c9c0bcf714
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-08-09 10:38:03 +0800

    Update LinearRegression.lua

--- ModuleScripts/Models/LinearRegression.lua
+++ ModuleScripts/Models/LinearRegression.lua
@@ -205,7 +205,7 @@ function LinearRegressionModel:train(featureMatrix, labelVector)
 	
 	if (cost == math.huge) then warn("The model diverged! Please repeat the experiment again or change the argument values") end
 	
-	if self.Optimizer then self.Optimizer:reset() end
+	if (self.Optimizer) and (self.AutoResetOptimizers) then self.Optimizer:reset() end
 	
 	return costArray
 	

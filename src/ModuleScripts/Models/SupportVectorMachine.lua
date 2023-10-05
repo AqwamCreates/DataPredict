87a678c19b4a57710067a2b338839feb321e0822
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-10-06 02:39:39 +0800

    Update SupportVectorMachine.lua

--- src/ModuleScripts/Models/SupportVectorMachine.lua
+++ src/ModuleScripts/Models/SupportVectorMachine.lua
@@ -266,7 +266,7 @@ local function calculateCost(modelParameters, individualKernelMatrix, kernelMatr
 	
 	costPart1 = AqwamMatrixLibrary:dotProduct(costPart1, costVector)
 	
-	costPart1 *= 0.5
+	costPart1 /= 2
 	
 	local costPart2 = AqwamMatrixLibrary:dotProduct(transposedCostVector, kernelMatrix)
 	
@@ -274,13 +274,13 @@ local function calculateCost(modelParameters, individualKernelMatrix, kernelMatr
 	
 	local costPart3 = AqwamMatrixLibrary:dotProduct(transposedLabelVector, labelVector)
 	
-	costPart3 *= 0.5
+	costPart3 /= 2
 	
 	local costPart4 = AqwamMatrixLibrary:dotProduct(transposedCostVector, kernelMatrix)
 	
 	costPart4 = AqwamMatrixLibrary:dotProduct(costPart4, costVector)
 	
-	costPart4 *= (1 / (2 * cValue))
+	costPart4 /= (2 * cValue)
 	
 	local cost = costPart1 - costPart2 + costPart3 + costPart4
 	

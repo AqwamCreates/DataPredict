34fef66c20b02803bef0349862c5a85e251f084c
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-10-29 07:34:43 +0800

    Update QLearningNeuralNetwork.lua

--- src/RobloxVersion/Models/QLearningNeuralNetwork.lua
+++ src/RobloxVersion/Models/QLearningNeuralNetwork.lua
@@ -1,81 +1,37 @@
-local NeuralNetworkModel = require(script.Parent.NeuralNetwork)
+local ReinforcementLearningNeuralNetworkBaseModel = require(script.Parent.ReinforcementLearningNeuralNetworkBaseModel)
 
 QLearningNeuralNetworkModel = {}
 
 QLearningNeuralNetworkModel.__index = QLearningNeuralNetworkModel
 
-local AqwamMatrixLibrary = require(script.Parent.Parent.AqwamRobloxMatrixLibraryLinker.Value)
-
-local ExperienceReplayComponent = require(script.Parent.Parent.Components.ExperienceReplay)
-
-setmetatable(QLearningNeuralNetworkModel, NeuralNetworkModel)
-
-local defaultMaxNumberOfEpisode = 500
-
-local defaultEpsilon = 0.5
-
-local defaultEpsilonDecayFactor = 0.999
-
-local defaultDiscountFactor = 0.95
-
-local defaultMaxNumberOfIterations = 1
+setmetatable(QLearningNeuralNetworkModel, ReinforcementLearningNeuralNetworkBaseModel)
 
 function QLearningNeuralNetworkModel.new(maxNumberOfIterations, learningRate, targetCost, maxNumberOfEpisodes, epsilon, epsilonDecayFactor, discountFactor)
-	
-	maxNumberOfIterations = maxNumberOfIterations or defaultMaxNumberOfIterations
 
-	local NewQLearningNeuralNetworkModel = NeuralNetworkModel.new(maxNumberOfIterations, learningRate, targetCost)
+	local NewQLearningNeuralNetworkModel = ReinforcementLearningNeuralNetworkBaseModel.new(maxNumberOfIterations, learningRate, targetCost, maxNumberOfEpisodes, epsilon, epsilonDecayFactor, discountFactor)
 	
-	NewQLearningNeuralNetworkModel:setPrintOutput(false)
-
 	setmetatable(NewQLearningNeuralNetworkModel, QLearningNeuralNetworkModel)
-
-	NewQLearningNeuralNetworkModel.maxNumberOfEpisodes = maxNumberOfEpisodes or defaultMaxNumberOfEpisode
-
-	NewQLearningNeuralNetworkModel.epsilon = epsilon or defaultEpsilon
-
-	NewQLearningNeuralNetworkModel.epsilonDecayFactor =  epsilonDecayFactor or defaultEpsilonDecayFactor
-
-	NewQLearningNeuralNetworkModel.discountFactor =  discountFactor or defaultDiscountFactor
-
-	NewQLearningNeuralNetworkModel.currentNumberOfEpisodes = 0
-
-	NewQLearningNeuralNetworkModel.currentEpsilon = epsilon or defaultEpsilon
-
-	NewQLearningNeuralNetworkModel.previousFeatureVector = nil
 	
-	NewQLearningNeuralNetworkModel.printReinforcementOutput = true
-	
-	NewQLearningNeuralNetworkModel.replayBufferArray = {}
-	
-	NewQLearningNeuralNetworkModel.useExperienceReplay = false
-	
-	NewQLearningNeuralNetworkModel.ExperienceReplayComponent = nil
+	NewQLearningNeuralNetworkModel:setUpdateFunction(function(previousFeatureVector, action, rewardValue, currentFeatureVector)
+
+		if (NewQLearningNeuralNetworkModel.ModelParameters == nil) then NewQLearningNeuralNetworkModel:generateLayers() end
+
+		local predictedValue, maxQValue = NewQLearningNeuralNetworkModel:predict(currentFeatureVector)
+
+		local target = rewardValue + (NewQLearningNeuralNetworkModel.discountFactor * maxQValue[1][1])
+
+		local targetVector = NewQLearningNeuralNetworkModel:predict(previousFeatureVector, true)
+
+		local actionIndex = table.find(NewQLearningNeuralNetworkModel.ClassesList, action)
+
+		targetVector[1][actionIndex] = target
+
+		NewQLearningNeuralNetworkModel:train(previousFeatureVector, targetVector)
+
+	end)
 
 	return NewQLearningNeuralNetworkModel
 
-end
-
-function QLearningNeuralNetworkModel:setExperienceReplay(useExperienceReplay, experienceReplayBatchSize, numberOfReinforcementsForExperienceReplayUpdate, maxExperienceReplayBufferSize)
-
-	self.useExperienceReplay = self:getBooleanOrDefaultOption(useExperienceReplay, self.useExperienceReplay)
-
-	if (self.useExperienceReplay) then
-
-		self.ExperienceReplayComponent = ExperienceReplayComponent.new(experienceReplayBatchSize, numberOfReinforcementsForExperienceReplayUpdate, maxExperienceReplayBufferSize)
-
-	else
-
-		self.ExperienceReplayComponent = nil
-
-	end
-	
-end
-
-function QLearningNeuralNetworkModel:setPrintReinforcementOutput(option)
-	
-	self.printReinforcementOutput = self:getBooleanOrDefaultOption(option, self.printReinforcementOutput)
-	
 end
 
 function QLearningNeuralNetworkModel:setParameters(maxNumberOfIterations, learningRate, targetCost, maxNumberOfEpisodes, epsilon, epsilonDecayFactor, discountFactor)
@@ -95,112 +51,6 @@ function QLearningNeuralNetworkModel:setParameters(maxNumberOfIterations, learni
 	self.discountFactor =  discountFactor or self.discountFactor
 
 	self.currentEpsilon = epsilon or self.currentEpsilon
-
-end
-
-function QLearningNeuralNetworkModel:update(previousFeatureVector, action, rewardValue, currentFeatureVector)
-
-	if (self.ModelParameters == nil) then self:generateLayers() end
-
-	local predictedValue, maxQValue = self:predict(currentFeatureVector)
-
-	local target = rewardValue + (self.discountFactor * maxQValue[1][1])
-
-	local targetVector = self:predict(previousFeatureVector, true)
-
-	local actionIndex = table.find(self.ClassesList, action)
-
-	targetVector[1][actionIndex] = target
-
-	self:train(previousFeatureVector, targetVector)
-	
-end
-
-function QLearningNeuralNetworkModel:reset()
-	
-	self.currentNumberOfEpisodes = 0
-
-	self.previousFeatureVector = nil
-
-	self.currentEpsilon = self.epsilon
-
-	for i, Optimizer in ipairs(self.OptimizerTable) do
-
-		if Optimizer then Optimizer:reset() end
-
-	end
-
-	if (self.useExperienceReplay) then self.ExperienceReplayComponent:reset() end
-
-end
-
-function QLearningNeuralNetworkModel:reinforce(currentFeatureVector, rewardValue, returnOriginalOutput)
-	
-	if (self.ModelParameters == nil) then self:generateLayers() end
-	
-	self.currentNumberOfEpisodes = (self.currentNumberOfEpisodes + 1) % self.maxNumberOfEpisodes
-
-	if (self.currentNumberOfEpisodes == 0) then
-
-		self.currentEpsilon *= self.epsilonDecayFactor
-
-	end
-
-	local action
-
-	local actionVector
-
-	local highestValue
-
-	local highestValueVector
-
-	local allOutputsMatrix
-
-	local randomProbability = Random.new():NextNumber()
-
-	if (randomProbability < self.currentEpsilon) then
-
-		local randomNumber = Random.new():NextInteger(1, #self.ClassesList)
-
-		action = self.ClassesList[randomNumber]
-		
-		allOutputsMatrix = AqwamMatrixLibrary:createMatrix(1, #self.ClassesList)
-
-		allOutputsMatrix[1][randomNumber] = randomProbability
-
-	else
-
-		allOutputsMatrix = self:predict(currentFeatureVector, true)
-
-		actionVector, highestValueVector = self:getLabelFromOutputMatrix(allOutputsMatrix)
-
-		action = actionVector[1][1]
-
-		highestValue = highestValueVector[1][1]
-
-	end
-
-	if (self.previousFeatureVector) then self:update(self.previousFeatureVector, action, rewardValue, currentFeatureVector) end
-
-	if (self.useExperienceReplay) and (self.previousFeatureVector) then 
-
-		self.ExperienceReplayComponent:addExperience(self.previousFeatureVector, action, rewardValue, currentFeatureVector)
-
-		self.ExperienceReplayComponent:run(function(storedPreviousFeatureVector, storedAction, storedRewardValue, storedCurrentFeatureVector)
-
-			self:update(storedPreviousFeatureVector, storedAction, storedRewardValue, storedCurrentFeatureVector)
-
-		end)
-
-	end
-
-	self.previousFeatureVector = currentFeatureVector
-
-	if (self.printReinforcementOutput == true) then print("Current Number Of Episodes: " .. self.currentNumberOfEpisodes .. "\t\tCurrent Epsilon: " .. self.currentEpsilon) end
-
-	if (returnOriginalOutput == true) then return allOutputsMatrix end
-
-	return action, highestValue
 
 end
 

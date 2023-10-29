d2b90de2d438eb299a307db1095f79b467545a0c
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-10-29 08:29:26 +0800

    Create PrioritizedExperienceReplay.lua

---
+++ src/RobloxVersion/ExperienceReplays/PrioritizedExperienceReplay.lua
@@ -0,0 +1,73 @@
+local ExperienceReplayBase = require(script.Parent.ExperienceReplayBase)
+
+PrioritizedExperienceReplay = {}
+
+PrioritizedExperienceReplay.__index = PrioritizedExperienceReplay
+
+setmetatable(PrioritizedExperienceReplay, ExperienceReplayBase)
+
+function PrioritizedExperienceReplay.new(batchSize, numberOfExperienceToUpdate, maxBufferSize)
+	
+	local NewPrioritizedExperienceReplay = ExperienceReplayBase.new(batchSize, numberOfExperienceToUpdate, maxBufferSize)
+	
+	setmetatable(NewPrioritizedExperienceReplay, PrioritizedExperienceReplay)
+	
+	NewPrioritizedExperienceReplay.priorities = {} -- Store priorities
+	
+	NewPrioritizedExperienceReplay:setSampleFunction(function()
+		
+		local batchArray = {}
+		
+		local prioritySum = 0
+
+		for i, priority in ipairs(NewPrioritizedExperienceReplay.priorities) do prioritySum += priority end
+
+		for _ = 1, NewPrioritizedExperienceReplay.batchSize, 1 do
+			
+			local threshold = prioritySum * math.random()
+			
+			local cumulativePriority = 0
+
+			for i, priority in ipairs(NewPrioritizedExperienceReplay.priorities) do
+				
+				cumulativePriority += priority
+
+				if cumulativePriority < threshold then continue end
+				
+				table.insert(batchArray, NewPrioritizedExperienceReplay.replayBufferArray[i])
+				
+				break
+				
+			end
+			
+		end
+		
+		return batchArray
+		
+	end)
+	
+	NewPrioritizedExperienceReplay:setResetFunction(function()
+		
+		NewPrioritizedExperienceReplay.numberOfExperience = 0
+
+		NewPrioritizedExperienceReplay.replayBufferArray = {}
+		
+		NewPrioritizedExperienceReplay.priorities = {}
+		
+	end)
+	
+	return NewPrioritizedExperienceReplay
+	
+end
+
+function PrioritizedExperienceReplay:setParameters(batchSize, numberOfExperienceToUpdate, maxBufferSize)
+	
+	self.batchSize = batchSize or self.batchSize
+
+	self.numberOfExperienceToUpdate = numberOfExperienceToUpdate or self.numberOfExperienceToUpdate
+
+	self.maxBufferSize = maxBufferSize or self.maxBufferSize
+	
+end
+
+return PrioritizedExperienceReplay

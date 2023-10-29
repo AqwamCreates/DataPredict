04fe6ec0a37959ad526bb063331cb6fa4231218f
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-10-29 08:43:21 +0800

    Update and rename ExperienceReplay.lua to UniformExperienceReplay.lua

---
+++ src/RobloxVersion/ExperienceReplays/UniformExperienceReplay.lua
@@ -0,0 +1,55 @@
+local BaseExperienceReplay = require(script.Parent.BaseExperienceReplay)
+
+UniformExperienceReplay = {}
+
+UniformExperienceReplay.__index = UniformExperienceReplay
+
+setmetatable(UniformExperienceReplay, BaseExperienceReplay)
+
+function UniformExperienceReplay.new(batchSize, numberOfExperienceToUpdate, maxBufferSize)
+	
+	local NewUniformExperienceReplay = BaseExperienceReplay.new(batchSize, numberOfExperienceToUpdate, maxBufferSize)
+	
+	setmetatable(NewUniformExperienceReplay, UniformExperienceReplay)
+	
+	NewUniformExperienceReplay:setSampleFunction(function()
+		
+		local batchArray = {}
+
+		local lowestNumberOfBatchSize = math.min(NewUniformExperienceReplay.batchSize, #NewUniformExperienceReplay.replayBufferArray)
+
+		for i = 1, lowestNumberOfBatchSize, 1 do
+
+			local index = Random.new():NextInteger(1, #NewUniformExperienceReplay.replayBufferArray)
+
+			table.insert(batchArray, NewUniformExperienceReplay.replayBufferArray[index])
+
+		end
+
+		return batchArray
+		
+	end)
+	
+	NewUniformExperienceReplay:setResetFunction(function()
+		
+		NewUniformExperienceReplay.numberOfExperience = 0
+
+		NewUniformExperienceReplay.replayBufferArray = {}
+		
+	end)
+	
+	return NewUniformExperienceReplay
+	
+end
+
+function UniformExperienceReplay:setParameters(batchSize, numberOfExperienceToUpdate, maxBufferSize)
+	
+	self.batchSize = batchSize or self.batchSize
+
+	self.numberOfExperienceToUpdate = numberOfExperienceToUpdate or self.numberOfExperienceToUpdate
+
+	self.maxBufferSize = maxBufferSize or self.maxBufferSize
+	
+end
+
+return UniformExperienceReplay

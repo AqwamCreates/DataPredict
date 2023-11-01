15488c667fcdcb6289ef1fde8018aea76ab1290b
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-11-01 23:09:32 +0800

    Update Model_ReinforcementLearningNeuralNetworkBaseModel.lua

--- src/LuaVersion/Model_ReinforcementLearningNeuralNetworkBaseModel.lua
+++ src/LuaVersion/Model_ReinforcementLearningNeuralNetworkBaseModel.lua
@@ -83,7 +83,7 @@ function ReinforcementLearningNeuralNetworkBaseModel.new(maxNumberOfIterations, 
 	
 end
 
-function ReinforcementLearningNeuralNetworkBaseModel:setParameters(maxNumberOfIterations, learningRate, targetCost, maxNumberOfEpisodes, epsilon, epsilonDecayFactor, discountFactor)
+function ReinforcementLearningNeuralNetworkBaseModel:setParameters(maxNumberOfIterations, learningRate, targetCost, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
 
 	self.maxNumberOfIterations = maxNumberOfIterations or self.maxNumberOfIterations
 
@@ -91,7 +91,7 @@ function ReinforcementLearningNeuralNetworkBaseModel:setParameters(maxNumberOfIt
 
 	self.targetCost = targetCost or self.targetCost
 
-	self.maxNumberOfEpisodes = maxNumberOfEpisodes or self.maxNumberOfEpisodes
+	self.numberOfReinforcementsPerEpisode = numberOfReinforcementsPerEpisode or self.numberOfReinforcementsPerEpisode
 
 	self.epsilon = epsilon or self.epsilon
 

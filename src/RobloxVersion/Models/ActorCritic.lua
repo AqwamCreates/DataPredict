7f89e757b330350760ad1d85e19cd062a2cb3203
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-11-02 06:30:26 +0800

    Update ActorCritic.lua

--- src/RobloxVersion/Models/ActorCritic.lua
+++ src/RobloxVersion/Models/ActorCritic.lua
@@ -1,8 +1,12 @@
 local AqwamMatrixLibrary = require(script.Parent.Parent.AqwamRobloxMatrixLibraryLinker.Value)
 
-ActorCriticModel = {}
+local NeuralNetworkModel = require(script.Parent.NeuralNetwork)
 
-ActorCriticModel.__index = ActorCriticModel
+ReinforcementLearningNeuralNetworkBaseModel = {}
+
+ReinforcementLearningNeuralNetworkBaseModel.__index = ReinforcementLearningNeuralNetworkBaseModel
+
+setmetatable(ReinforcementLearningNeuralNetworkBaseModel, NeuralNetworkModel)
 
 local defaultNumberOfReinforcementsPerEpisode = 500
 
@@ -12,88 +16,108 @@ local defaultEpsilonDecayFactor = 0.999
 
 local defaultDiscountFactor = 0.95
 
-function ActorCriticModel.new(numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
+local defaultMaxNumberOfIterations = 1
+
+function ReinforcementLearningNeuralNetworkBaseModel.new(maxNumberOfIterations, learningRate, targetCost, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
 	
-	local NewActorCriticModel = {}
+	maxNumberOfIterations = maxNumberOfIterations or defaultMaxNumberOfIterations
+
+	local NewReinforcementLearningNeuralNetworkBaseModel = NeuralNetworkModel.new(maxNumberOfIterations, learningRate, targetCost)
+
+	NewReinforcementLearningNeuralNetworkBaseModel:setPrintOutput(false)
+
+	setmetatable(NewReinforcementLearningNeuralNetworkBaseModel, ReinforcementLearningNeuralNetworkBaseModel)
+
+	NewReinforcementLearningNeuralNetworkBaseModel.numberOfReinforcementsPerEpisode = numberOfReinforcementsPerEpisode or defaultNumberOfReinforcementsPerEpisode
+
+	NewReinforcementLearningNeuralNetworkBaseModel.epsilon = epsilon or defaultEpsilon
+
+	NewReinforcementLearningNeuralNetworkBaseModel.epsilonDecayFactor =  epsilonDecayFactor or defaultEpsilonDecayFactor
+
+	NewReinforcementLearningNeuralNetworkBaseModel.discountFactor =  discountFactor or defaultDiscountFactor
+
+	NewReinforcementLearningNeuralNetworkBaseModel.currentEpsilon = epsilon or defaultEpsilon
+
+	NewReinforcementLearningNeuralNetworkBaseModel.previousFeatureVector = nil
+
+	NewReinforcementLearningNeuralNetworkBaseModel.printReinforcementOutput = true
 	
-	setmetatable(NewActorCriticModel, ActorCriticModel)
+	NewReinforcementLearningNeuralNetworkBaseModel.currentNumberOfReinforcements = 0
 	
-	NewActorCriticModel.numberOfReinforcementsPerEpisode = numberOfReinforcementsPerEpisode or defaultNumberOfReinforcementsPerEpisode
+	NewReinforcementLearningNeuralNetworkBaseModel.currentNumberOfEpisodes = 0
 
-	NewActorCriticModel.epsilon = epsilon or defaultEpsilon
-
-	NewActorCriticModel.epsilonDecayFactor =  epsilonDecayFactor or defaultEpsilonDecayFactor
-
-	NewActorCriticModel.discountFactor =  discountFactor or defaultDiscountFactor
-
-	NewActorCriticModel.currentEpsilon = epsilon or defaultEpsilon
-
-	NewActorCriticModel.previousFeatureVector = nil
-
-	NewActorCriticModel.printReinforcementOutput = true
-
-	NewActorCriticModel.currentNumberOfReinforcements = 0
-
-	NewActorCriticModel.currentNumberOfEpisodes = 0
-	
-	return NewActorCriticModel
-	
-end
-
-function ActorCriticModel:setActorModel(Model)
-	
-	self.ActorModel = Model
-	
-end
-
-function ActorCriticModel:setCriticModel(Model)
-
-	self.CriticModel = Model
-
-end
-
-function ActorCriticModel:setClassesList(classesList)
-	
-	self.ClassesList = classesList
+	return NewReinforcementLearningNeuralNetworkBaseModel
 	
 end
 
-function ActorCriticModel:update(previousFeatureVector, action, rewardValue, currentFeatureVector)
+function ReinforcementLearningNeuralNetworkBaseModel:setParameters(maxNumberOfIterations, learningRate, targetCost, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
 
+	self.maxNumberOfIterations = maxNumberOfIterations or self.maxNumberOfIterations
 
+	self.learningRate = learningRate or self.learningRate
+
+	self.targetCost = targetCost or self.targetCost
+
+	self.numberOfReinforcementsPerEpisode = numberOfReinforcementsPerEpisode or self.numberOfReinforcementsPerEpisode
+
+	self.epsilon = epsilon or self.epsilon
+
+	self.epsilonDecayFactor =  epsilonDecayFactor or self.epsilonDecayFactor
+
+	self.discountFactor =  discountFactor or self.discountFactor
+
+	self.currentEpsilon = epsilon or self.currentEpsilon
 
 end
 
-function ActorCriticModel:setExperienceReplay(ExperienceReplay)
+function ReinforcementLearningNeuralNetworkBaseModel:setUpdateFunction(updateFunction)
+	
+	self.updateFunction = updateFunction
+	
+end
+
+function ReinforcementLearningNeuralNetworkBaseModel:update(previousFeatureVector, action, rewardValue, currentFeatureVector)
+	
+	self.updateFunction(previousFeatureVector, action, rewardValue, currentFeatureVector)
+	
+end
+
+function ReinforcementLearningNeuralNetworkBaseModel:setExperienceReplay(ExperienceReplay)
 
 	self.ExperienceReplay = ExperienceReplay
 
 end
 
-function ActorCriticModel:setPrintReinforcementOutput(option)
+local function getBooleanOrDefaultOption(boolean, defaultBoolean)
 
-	self.printReinforcementOutput = self:getBooleanOrDefaultOption(option, self.printReinforcementOutput)
+	if (type(boolean) == "nil") then return defaultBoolean end
+
+	return boolean
 
 end
 
-function ActorCriticModel:reinforce(currentFeatureVector, rewardValue, returnOriginalOutput)
-	
-	if (self.ActorModel == nil) then error("No actor model!") end
-	
-	if (self.CriticModel == nil) then error("No critic model!") end
+function ReinforcementLearningNeuralNetworkBaseModel:setPrintReinforcementOutput(option)
 
+	self.printReinforcementOutput = getBooleanOrDefaultOption(option, self.printReinforcementOutput)
+
+end
+
+function ReinforcementLearningNeuralNetworkBaseModel:reinforce(currentFeatureVector, rewardValue, returnOriginalOutput)
+
+	if (self.ModelParameters == nil) then self:generateLayers() end
+	
 	if (self.currentNumberOfReinforcements >= self.numberOfReinforcementsPerEpisode) then
-
+		
 		self.currentNumberOfReinforcements = 0
-
+		
 		self.currentNumberOfEpisodes += 1
-
+		
 		self.currentEpsilon *= self.epsilonDecayFactor
-
+		
 	end
-
-	self.currentNumberOfReinforcements += 1
 	
+	self.currentNumberOfReinforcements += 1
+
 	local action
 
 	local actionVector
@@ -107,12 +131,14 @@ function ActorCriticModel:reinforce(currentFeatureVector, rewardValue, returnOri
 	local randomProbability = Random.new():NextNumber()
 
 	if (randomProbability < self.currentEpsilon) then
+		
+		local numberOfClasses = #self.ClassesList
 
-		local randomNumber = Random.new():NextInteger(1, #self.ClassesList)
+		local randomNumber = Random.new():NextInteger(1, numberOfClasses)
 
 		action = self.ClassesList[randomNumber]
 
-		allOutputsMatrix = AqwamMatrixLibrary:createMatrix(1, #self.ClassesList)
+		allOutputsMatrix = AqwamMatrixLibrary:createMatrix(1, numberOfClasses)
 
 		allOutputsMatrix[1][randomNumber] = randomProbability
 
@@ -127,7 +153,7 @@ function ActorCriticModel:reinforce(currentFeatureVector, rewardValue, returnOri
 		highestValue = highestValueVector[1][1]
 
 	end
-	
+
 	if (self.previousFeatureVector) then self:update(self.previousFeatureVector, action, rewardValue, currentFeatureVector) end
 
 	if (self.ExperienceReplay) and (self.previousFeatureVector) then 
@@ -149,33 +175,45 @@ function ActorCriticModel:reinforce(currentFeatureVector, rewardValue, returnOri
 	if (returnOriginalOutput) then return allOutputsMatrix end
 
 	return action, highestValue
+
+end
+
+function ReinforcementLearningNeuralNetworkBaseModel:getCurrentNumberOfEpisodes()
+	
+	return self.currentNumberOfEpisodes
 	
 end
 
-function ActorCriticModel:getCurrentNumberOfEpisodes()
-
-	return self.currentNumberOfEpisodes
-
-end
-
-function ActorCriticModel:getCurrentNumberOfReinforcements()
-
+function ReinforcementLearningNeuralNetworkBaseModel:getCurrentNumberOfReinforcements()
+	
 	return self.currentNumberOfReinforcements
-
+	
 end
 
-function ActorCriticModel:reset()
+function ReinforcementLearningNeuralNetworkBaseModel:getCurrentEpsilon()
+	
+	return self.currentEpsilon
+	
+end
 
+function ReinforcementLearningNeuralNetworkBaseModel:reset()
+	
 	self.currentNumberOfReinforcements = 0
-
+	
 	self.currentNumberOfEpisodes = 0
 
 	self.previousFeatureVector = nil
 
 	self.currentEpsilon = self.epsilon
 
+	for i, Optimizer in ipairs(self.OptimizerTable) do
+
+		if Optimizer then Optimizer:reset() end
+
+	end
+
 	if (self.ExperienceReplay) then self.ExperienceReplay:reset() end
 
 end
 
-return ActorCriticModel
+return ReinforcementLearningNeuralNetworkBaseModel

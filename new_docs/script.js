f94259977e84f29cdd763408632c90ef049d331c
AqwamCreates <aqwam.harish.aiman@gmail.com>
2023-11-13 20:03:37 +0800

    Creation of new documentation.

---
+++ new_docs/script.js
@@ -0,0 +1,8 @@
+function openSidebar() {
+  document.getElementById("sidebar").style.width = "300px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeSideBar() {
+  document.getElementById("sidebar").style.width = "0";
+}

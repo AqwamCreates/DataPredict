03ce352b721df11a4ec5c8f1bcbe92d17e5ab6ab
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-12-03 16:56:12 +0800

    Create Model_REINFORCENeuralNetwork.lua

---
+++ src/LuaVersion/Model_REINFORCENeuralNetwork.lua
@@ -0,0 +1,81 @@
+local ReinforcementLearningNeuralNetworkBaseModel = require("Model_ReinforcementLearningNeuralNetworkBaseModel")
+
+local AqwamMatrixLibrary = require("AqwamMatrixLibrary")
+
+REINFORCENeuralNetworkModel = {}
+
+REINFORCENeuralNetworkModel.__index = REINFORCENeuralNetworkModel
+
+setmetatable(REINFORCENeuralNetworkModel, ReinforcementLearningNeuralNetworkBaseModel)
+
+function REINFORCENeuralNetworkModel.new(maxNumberOfIterations, learningRate, targetCost, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
+
+	local NewREINFORCENeuralNetworkModel = ReinforcementLearningNeuralNetworkBaseModel.new(maxNumberOfIterations, learningRate, targetCost, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
+	
+	setmetatable(NewREINFORCENeuralNetworkModel, REINFORCENeuralNetworkModel)
+	
+	local logProbilitiesMatrix = {}
+	
+	local rewardVector = {}
+	
+	NewREINFORCENeuralNetworkModel:setUpdateFunction(function(previousFeatureVector, action, rewardValue, currentFeatureVector)
+
+		local predictedValue, maxQValue = NewREINFORCENeuralNetworkModel:predict(currentFeatureVector)
+
+		local predictedVector = NewREINFORCENeuralNetworkModel:predict(previousFeatureVector, true)
+		
+		local logPredictedVector = AqwamMatrixLibrary:applyFunction(math.log, predictedVector)
+
+		table.insert(logProbilitiesMatrix, logPredictedVector[1])
+		
+		table.insert(rewardVector, {rewardValue})
+
+	end)
+	
+	NewREINFORCENeuralNetworkModel:setEpisodeUpdateFunction(function()
+		
+		local meanLogProbabilitiesVector = AqwamMatrixLibrary:verticalMean(logProbilitiesMatrix)
+		
+		local sumReward = AqwamMatrixLibrary:sum(rewardVector)
+		
+		local negativeMeanLogProbabilitiesVector = AqwamMatrixLibrary:multiply(-1, meanLogProbabilitiesVector)
+		
+		local targetVector = AqwamMatrixLibrary:multiply(negativeMeanLogProbabilitiesVector, sumReward)
+		
+		local numberOfNeurons = NewREINFORCENeuralNetworkModel.numberOfNeuronsTable[1] + NewREINFORCENeuralNetworkModel.hasBiasNeuronTable[1]
+		
+		local inputVector = {table.create(numberOfNeurons, 1)}
+		
+		NewREINFORCENeuralNetworkModel:train(inputVector, targetVector)
+		
+		table.clear(rewardVector)
+		
+		table.clear(logProbilitiesMatrix)
+		
+	end)
+
+	return NewREINFORCENeuralNetworkModel
+
+end
+
+function REINFORCENeuralNetworkModel:setParameters(maxNumberOfIterations, learningRate, targetCost, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
+	
+	self.maxNumberOfIterations = maxNumberOfIterations or self.maxNumberOfIterations
+
+	self.learningRate = learningRate or self.learningRate
+
+	self.targetCost = targetCost or self.targetCost
+	
+	self.numberOfReinforcementsPerEpisode = numberOfReinforcementsPerEpisode or self.numberOfReinforcementsPerEpisode
+
+	self.epsilon = epsilon or self.epsilon
+
+	self.epsilonDecayFactor =  epsilonDecayFactor or self.epsilonDecayFactor
+
+	self.discountFactor =  discountFactor or self.discountFactor
+
+	self.currentEpsilon = epsilon or self.currentEpsilon
+
+end
+
+return REINFORCENeuralNetworkModel

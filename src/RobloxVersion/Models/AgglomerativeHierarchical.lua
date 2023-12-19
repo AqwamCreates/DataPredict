fe3ed6b65cea104067d7602a3a4d5a51ef316718
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2023-12-20 00:36:49 +0800

    Update AgglomerativeHierarchical.lua

--- src/RobloxVersion/Models/AgglomerativeHierarchical.lua
+++ src/RobloxVersion/Models/AgglomerativeHierarchical.lua
@@ -193,7 +193,7 @@ end
 
 local function wardLinkage(centroids, centroidDistanceMatrix, centroidIndex1, centroidIndex2)
 
-	local newCentroidDistanceMatrix = createCentroidDistanceMatrix(centroids, "euclidean")
+	local newCentroidDistanceMatrix = createCentroidDistanceMatrix(centroids, "Euclidean")
 
 	for i = 2, #newCentroidDistanceMatrix,1 do
 

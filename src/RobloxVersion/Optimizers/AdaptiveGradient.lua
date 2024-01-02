0d55344f11edfa29ebb163a2cf2612acf8a42f66
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-01-03 01:42:34 +0800

    Update AdaptiveGradient.lua

--- src/RobloxVersion/Optimizers/AdaptiveGradient.lua
+++ src/RobloxVersion/Optimizers/AdaptiveGradient.lua
@@ -1,44 +1,52 @@
+local BaseOptimizer = require(script.Parent.BaseOptimizer)
+
+local AqwamMatrixLibrary = require(script.Parent.Parent.AqwamMatrixLibraryLinker.Value)
+
 AdaptiveGradientOptimizer = {}
 
 AdaptiveGradientOptimizer.__index = AdaptiveGradientOptimizer
 
-local AqwamMatrixLibrary = require(script.Parent.Parent.AqwamMatrixLibraryLinker.Value)
+setmetatable(AdaptiveGradientOptimizer, BaseOptimizer)
 
 function AdaptiveGradientOptimizer.new()
 	
-	local NewAdaptiveGradientOptimizer = {}
+	local NewAdaptiveGradientOptimizer = BaseOptimizer.new("AdaptiveGradient")
 	
 	setmetatable(NewAdaptiveGradientOptimizer, AdaptiveGradientOptimizer)
 	
 	NewAdaptiveGradientOptimizer.PreviousSumOfGradientSquaredMatrix = nil
 	
+	--------------------------------------------------------------------------------
+	
+	NewAdaptiveGradientOptimizer:setCalculationFunction(function(learningRate, costFunctionDerivatives)
+		
+		NewAdaptiveGradientOptimizer.PreviousSumOfGradientSquaredMatrix = NewAdaptiveGradientOptimizer.PreviousSumOfGradientSquaredMatrix or AqwamMatrixLibrary:createMatrix(#costFunctionDerivatives, #costFunctionDerivatives[1])
+
+		local GradientSquaredMatrix = AqwamMatrixLibrary:power(costFunctionDerivatives, 2)
+
+		local CurrentSumOfGradientSquaredMatrix = AqwamMatrixLibrary:add(NewAdaptiveGradientOptimizer.PreviousSumOfGradientSquaredMatrix, GradientSquaredMatrix)
+
+		local SquareRootSumOfGradientSquared = AqwamMatrixLibrary:power(CurrentSumOfGradientSquaredMatrix, 0.5)
+
+		local costFunctionDerivativesPart1 = AqwamMatrixLibrary:divide(costFunctionDerivatives, SquareRootSumOfGradientSquared)
+
+		costFunctionDerivatives = AqwamMatrixLibrary:multiply(learningRate, costFunctionDerivativesPart1)
+
+		NewAdaptiveGradientOptimizer.PreviousSumOfGradientSquaredMatrix = CurrentSumOfGradientSquaredMatrix
+
+		return costFunctionDerivatives
+		
+	end)
+	
+	--------------------------------------------------------------------------------
+	
+	NewAdaptiveGradientOptimizer:setResetFunction(function()
+		
+		NewAdaptiveGradientOptimizer.PreviousSumOfGradientSquaredMatrix = nil
+		
+	end)
+	
 	return NewAdaptiveGradientOptimizer
-	
-end
-
-function AdaptiveGradientOptimizer:calculate(learningRate, costFunctionDerivatives)
-	
-	self.PreviousSumOfGradientSquaredMatrix = self.PreviousSumOfGradientSquaredMatrix or AqwamMatrixLibrary:createMatrix(#costFunctionDerivatives, #costFunctionDerivatives[1])
-
-	local GradientSquaredMatrix = AqwamMatrixLibrary:power(costFunctionDerivatives, 2)
-
-	local CurrentSumOfGradientSquaredMatrix = AqwamMatrixLibrary:add(self.PreviousSumOfGradientSquaredMatrix, GradientSquaredMatrix)
-
-	local SquareRootSumOfGradientSquared = AqwamMatrixLibrary:power(CurrentSumOfGradientSquaredMatrix, 0.5)
-
-	local costFunctionDerivativesPart1 = AqwamMatrixLibrary:divide(costFunctionDerivatives, SquareRootSumOfGradientSquared)
-	
-	costFunctionDerivatives = AqwamMatrixLibrary:multiply(learningRate, costFunctionDerivativesPart1)
-	
-	self.PreviousSumOfGradientSquaredMatrix = CurrentSumOfGradientSquaredMatrix
-
-	return costFunctionDerivatives
-	
-end
-
-function AdaptiveGradientOptimizer:reset()
-	
-	self.PreviousSumOfGradientSquaredMatrix = nil
 	
 end
 

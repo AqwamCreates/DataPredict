ada5ed16248454c64dc37324cf9b4e05c2a65f6a
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-01-06 02:50:49 +0800

    Create Optimizer_LearningRateTimeDecay.lua

---
+++ src/LuaVersion/Optimizer_LearningRateTimeDecay.lua
@@ -0,0 +1,73 @@
+local BaseOptimizer = require("Optimizer_BaseOptimizer")
+
+local AqwamMatrixLibrary = require("AqwamMatrixLibrary")
+
+LearningRateTimeDecayOptimizer = {}
+
+LearningRateTimeDecayOptimizer.__index = LearningRateTimeDecayOptimizer
+
+setmetatable(LearningRateTimeDecayOptimizer, BaseOptimizer)
+
+local defaultDecayRate = 0.5
+
+local defaultTimeStepToDecay = 100
+
+function LearningRateTimeDecayOptimizer.new(decayRate, timeStepToDecay)
+	
+	local NewLearningRateTimeDecayOptimizer = BaseOptimizer.new("LearningRateDecay")
+	
+	setmetatable(NewLearningRateTimeDecayOptimizer, LearningRateTimeDecayOptimizer)
+	
+	NewLearningRateTimeDecayOptimizer.decayRate = decayRate or defaultDecayRate
+	
+	NewLearningRateTimeDecayOptimizer.timeStepToDecay = timeStepToDecay or defaultTimeStepToDecay
+	
+	NewLearningRateTimeDecayOptimizer.currentLearningRate = nil
+	
+	NewLearningRateTimeDecayOptimizer.currentTimeStep = 0
+	
+	--------------------------------------------------------------------------------
+	
+	NewLearningRateTimeDecayOptimizer:setCalculateFunction(function(learningRate, costFunctionDerivatives)
+		
+		NewLearningRateTimeDecayOptimizer.currentTimeStep += 1
+		
+		local currentLearningRate = NewLearningRateTimeDecayOptimizer.currentLearningRate or learningRate
+			
+		currentLearningRate /= (NewLearningRateTimeDecayOptimizer.decayRate * NewLearningRateTimeDecayOptimizer.currentTimeStep)
+		
+		costFunctionDerivatives = AqwamMatrixLibrary:multiply(currentLearningRate, costFunctionDerivatives)
+		
+		NewLearningRateTimeDecayOptimizer.currentLearningRate = currentLearningRate
+
+		return costFunctionDerivatives
+		
+	end)
+	
+	--------------------------------------------------------------------------------
+	
+	NewLearningRateTimeDecayOptimizer:setResetFunction(function()
+		
+		NewLearningRateTimeDecayOptimizer.currentLearningRate = nil
+		
+		NewLearningRateTimeDecayOptimizer.currentTimeStep = 0
+		
+	end)
+	
+	return NewLearningRateTimeDecayOptimizer
+	
+end
+
+function LearningRateTimeDecayOptimizer:setDecayRate(decayRate)
+	
+	self.decayRate = decayRate
+	
+end
+
+function LearningRateTimeDecayOptimizer:setTimeStepToDecay(timeStepToDecay)
+	
+	self.timeStepToDecay = timeStepToDecay
+	
+end
+
+return LearningRateTimeDecayOptimizer

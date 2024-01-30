dd2d155ece58cb12519b4815d008b44f23789ab6
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-01-30 19:59:49 +0800

    Update ClippedDoubleQLearningNeuralNetwork.lua

--- src/RobloxVersion/Models/ClippedDoubleQLearningNeuralNetwork.lua
+++ src/RobloxVersion/Models/ClippedDoubleQLearningNeuralNetwork.lua
@@ -6,9 +6,9 @@ ClippedDoubleQLearningNeuralNetworkModel.__index = ClippedDoubleQLearningNeuralN
 
 setmetatable(ClippedDoubleQLearningNeuralNetworkModel, ReinforcementLearningNeuralNetworkBaseModel)
 
-function ClippedDoubleQLearningNeuralNetworkModel.new(maxNumberOfIterations, learningRate, targetCost, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
+function ClippedDoubleQLearningNeuralNetworkModel.new(maxNumberOfIterations, learningRate, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
 
-	local NewClippedDoubleQLearningNeuralNetworkModel = ReinforcementLearningNeuralNetworkBaseModel.new(maxNumberOfIterations, learningRate, targetCost, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
+	local NewClippedDoubleQLearningNeuralNetworkModel = ReinforcementLearningNeuralNetworkBaseModel.new(maxNumberOfIterations, learningRate, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
 
 	NewClippedDoubleQLearningNeuralNetworkModel.ModelParametersArray = {}
 	
@@ -50,13 +50,11 @@ function ClippedDoubleQLearningNeuralNetworkModel.new(maxNumberOfIterations, lea
 
 end
 
-function ClippedDoubleQLearningNeuralNetworkModel:setParameters(maxNumberOfIterations, learningRate, targetCost, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
+function ClippedDoubleQLearningNeuralNetworkModel:setParameters(maxNumberOfIterations, learningRate, numberOfReinforcementsPerEpisode, epsilon, epsilonDecayFactor, discountFactor)
 
 	self.maxNumberOfIterations = maxNumberOfIterations or self.maxNumberOfIterations
 
 	self.learningRate = learningRate or self.learningRate
-
-	self.targetCost = targetCost or self.targetCost
 
 	self.numberOfReinforcementsPerEpisode = numberOfReinforcementsPerEpisode or self.numberOfReinforcementsPerEpisode
 

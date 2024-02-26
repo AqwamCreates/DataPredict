9d2a9f5ea7a599dab9ac18a83d5a085392bd14bc
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-02-26 20:42:41 +0800

    Update ReinforcementLearningNeuralNetworkBaseModel.lua

--- src/RobloxVersion/Models/ReinforcementLearningNeuralNetworkBaseModel.lua
+++ src/RobloxVersion/Models/ReinforcementLearningNeuralNetworkBaseModel.lua
@@ -90,6 +90,12 @@ function ReinforcementLearningNeuralNetworkBaseModel:episodeUpdate()
 	
 	local episodeUpdateFunction = self.episodeUpdateFunction
 	
+	self.currentNumberOfReinforcements = 0
+
+	self.currentNumberOfEpisodes += 1
+
+	self.currentEpsilon *= self.epsilonDecayFactor
+	
 	if not episodeUpdateFunction then return end
 	
 	episodeUpdateFunction()
@@ -161,12 +167,6 @@ function ReinforcementLearningNeuralNetworkBaseModel:reinforce(currentFeatureVec
 	if (self.currentNumberOfReinforcements >= self.numberOfReinforcementsPerEpisode) then
 
 		self:episodeUpdate()
-
-		self.currentNumberOfReinforcements = 0
-
-		self.currentNumberOfEpisodes += 1
-
-		self.currentEpsilon *= self.epsilonDecayFactor
 
 	end
 

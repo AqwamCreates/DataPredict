3777ab100981bf5fc7dfccb498e2b7274e561243
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-03-23 01:55:41 +0800

    Update Other_ModelParametersMerger.lua

--- src/LuaVersion/Other_ModelParametersMerger.lua
+++ src/LuaVersion/Other_ModelParametersMerger.lua
@@ -125,9 +125,9 @@ local function checkIfIsTableOfMatrices(array)
 
 	local depth = checkDepth(array)
 
-	local isTable = (depth == 3)
+	local isTableOfMatrices = (depth == 3)
 
-	return isTable
+	return isTableOfMatrices
 
 end
 

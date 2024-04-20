5842ac22bb4328e44e1fe68a0b4e6cffdf48fad1
AqwamCreates <aqwam.harish.aiman@gmail.com>
2024-04-20 15:44:19 +0800

    Update Model_DuelingQLearning.lua

--- src/LuaVersion/Models/Model_DuelingQLearning.lua
+++ src/LuaVersion/Models/Model_DuelingQLearning.lua
@@ -86,13 +86,13 @@ function DuelingQLearningModel:update(previousFeatureVector, action, rewardValue
 
 	local vLoss = AqwamMatrixLibrary:subtract(currentValue, previousValue)
 
-	self.ValueModel:forwardPropagate(previousFeatureVector, true)
-
-	self.ValueModel:backPropagate(qLoss, true)
-
 	self.AdvantageModel:forwardPropagate(previousFeatureVector, true)
 
-	self.AdvantageModel:backPropagate(vLoss, true)
+	self.AdvantageModel:backPropagate(qLoss, true)
+
+	self.ValueModel:forwardPropagate(previousFeatureVector, true)
+
+	self.ValueModel:backPropagate(vLoss, true)
 
 	return expectedQValue
 

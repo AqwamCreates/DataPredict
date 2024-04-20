9550610d2b93676bb819f0f2066f93c95beab9fc
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-04-20 21:46:55 +0800

    Update AdvantageActorCritic.lua

--- src/RobloxVersion/Models/AdvantageActorCritic.lua
+++ src/RobloxVersion/Models/AdvantageActorCritic.lua
@@ -42,11 +42,23 @@ end
 
 local function calculateProbability(outputMatrix)
 
-	local sumVector = AqwamMatrixLibrary:horizontalSum(outputMatrix)
+	local meanVector = AqwamMatrixLibrary:horizontalMean(outputMatrix)
 
-	local result = AqwamMatrixLibrary:divide(outputMatrix, sumVector)
+	local standardDeviationVector = AqwamMatrixLibrary:horizontalStandardDeviation(outputMatrix)
 
-	return result
+	local zScoreVectorPart1 = AqwamMatrixLibrary:subtract(outputMatrix, meanVector)
+
+	local zScoreVector = AqwamMatrixLibrary:divide(zScoreVectorPart1, standardDeviationVector)
+
+	local zScoreSquaredVector = AqwamMatrixLibrary:power(zScoreVector, 2)
+
+	local probabilityVectorPart1 = AqwamMatrixLibrary:multiply(-0.5, zScoreSquaredVector)
+
+	local probabilityVectorPart2 = AqwamMatrixLibrary:multiply(standardDeviationVector, math.sqrt(2 * math.pi))
+
+	local probabilityVector = AqwamMatrixLibrary:divide(probabilityVectorPart1, probabilityVectorPart2)
+
+	return probabilityVector
 
 end
 
@@ -76,7 +88,17 @@ function AdvantageActorCriticModel.new(discountFactor)
 
 		local numberOfActions = #allOutputsMatrix[1]
 
-		local actionIndex = sampleAction(actionProbabilityVector)
+		local actionIndex
+		
+		if action then
+
+			actionIndex = table.find(NewAdvantageActorCriticModel.ActorModel:getClassesList(), action)
+
+		else
+
+			actionIndex = sampleAction(actionProbabilityVector)
+
+		end
 
 		local actionProbability = actionProbabilityVector[1][actionIndex]
 

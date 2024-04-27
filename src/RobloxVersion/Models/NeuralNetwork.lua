59b364d481a0bab1ed37eeec5aad07d3df172236
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-04-28 05:01:23 +0800

    Update NeuralNetwork.lua

--- src/RobloxVersion/Models/NeuralNetwork.lua
+++ src/RobloxVersion/Models/NeuralNetwork.lua
@@ -555,7 +555,7 @@ function NeuralNetworkModel:calculateErrorMatrix(lossMatrix, forwardPropagateTab
 	local numberOfLayers = #self.numberOfNeuronsTable
 
 	local zLayerMatrix
-	
+
 	local layerCostMatrix = lossMatrix
 
 	table.insert(errorMatrixTable, layerCostMatrix)
@@ -567,15 +567,15 @@ function NeuralNetworkModel:calculateErrorMatrix(lossMatrix, forwardPropagateTab
 		local derivativeFunction = derivativeList[activationFunctionName]
 
 		local layerMatrix = self.ModelParameters[layerNumber]
-		
-		local hasBiasNeuron = self.hasBiasNeuronTable[layerNumber + 1]
+
+		local hasBiasNeuron = self.hasBiasNeuronTable[layerNumber]
 
 		local layerMatrix = AqwamMatrixLibrary:transpose(layerMatrix)
 
 		local partialErrorMatrix = AqwamMatrixLibrary:dotProduct(layerCostMatrix, layerMatrix)
 
 		local derivativeMatrix = derivativeFunction(forwardPropagateTable[layerNumber], zTable[layerNumber])
-		
+
 		if (hasBiasNeuron == 1) then
 
 			for data = 1, numberOfData, 1 do derivativeMatrix[data][1] = 0 end -- Derivative of bias is 0.

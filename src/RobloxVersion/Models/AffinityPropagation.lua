49a66fd1738eec4efa9d3bc149a9537f1c71057e
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-05-11 19:29:33 +0800

    Update AffinityPropagation.lua

--- src/RobloxVersion/Models/AffinityPropagation.lua
+++ src/RobloxVersion/Models/AffinityPropagation.lua
@@ -264,9 +264,9 @@ function AffinityPropagationModel:train(featureMatrix)
 
 	local numberOfFeatures = #featureMatrix[1]
 
-	local preferenceVector = initializePreferences(featureMatrix, self.similarityFunction)
+	--local preferenceVector = initializePreferences(featureMatrix, self.similarityFunction)
 
-	local similarityMatrix = AqwamMatrixLibrary:createMatrix(numberOfData, numberOfData)
+	local similarityMatrix = createDistanceMatrix(featureMatrix, featureMatrix, self.similarityFunction)
 
 	local responsibilityMatrix = AqwamMatrixLibrary:createMatrix(numberOfData, numberOfData)
 
@@ -290,7 +290,7 @@ function AffinityPropagationModel:train(featureMatrix)
 		
 		self:iterationWait()
 
-		similarityMatrix = calculateSimilarityMatrix(featureMatrix, similarityMatrix, preferenceVector)
+		--similarityMatrix = calculateSimilarityMatrix(featureMatrix, similarityMatrix, preferenceVector)
 
 		responsibilityMatrix = calculateResponsibilityMatrix(responsibilityMatrix, similarityMatrix, availabilityMatrix)
 

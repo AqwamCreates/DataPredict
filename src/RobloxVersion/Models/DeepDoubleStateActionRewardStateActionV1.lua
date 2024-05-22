4d8252edf5406a4b6706dc60ad951fc92c007ff7
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-05-22 20:42:44 +0800

    Update and rename DoubleStateActionRewardStateActionNeuralNetworkV1.lua to DeepDoubleStateActionRewardStateActionV1.lua

---
+++ src/RobloxVersion/Models/DeepDoubleStateActionRewardStateActionV1.lua
@@ -0,0 +1,131 @@
+local AqwamMatrixLibrary = require(script.Parent.Parent.AqwamMatrixLibraryLinker.Value)
+
+local ReinforcementLearningBaseModel = require(script.Parent.ReinforcementLearningBaseModel)
+
+DeepDoubleStateActionRewardStateActionModel = {}
+
+DeepDoubleStateActionRewardStateActionModel.__index = DeepDoubleStateActionRewardStateActionModel
+
+setmetatable(DeepDoubleStateActionRewardStateActionModel, ReinforcementLearningBaseModel)
+
+function DeepDoubleStateActionRewardStateActionModel.new(discountFactor)
+
+	local NewDeepDoubleStateActionRewardStateActionModel = ReinforcementLearningBaseModel.new(discountFactor)
+
+	setmetatable(NewDeepDoubleStateActionRewardStateActionModel, DeepDoubleStateActionRewardStateActionModel)
+
+	NewDeepDoubleStateActionRewardStateActionModel.ModelParametersArray = {}
+
+	NewDeepDoubleStateActionRewardStateActionModel:setUpdateFunction(function(previousFeatureVector, action, rewardValue, currentFeatureVector)
+		
+		local Model = NewDeepDoubleStateActionRewardStateActionModel.Model
+
+		local randomProbability = math.random()
+
+		local updateSecondModel = (randomProbability >= 0.5)
+
+		local selectedModelNumberForTargetVector = (updateSecondModel and 1) or 2
+
+		local selectedModelNumberForUpdate = (updateSecondModel and 2) or 1
+
+		NewDeepDoubleStateActionRewardStateActionModel:loadModelParametersFromModelParametersArray(selectedModelNumberForTargetVector)
+
+		local lossVector = NewDeepDoubleStateActionRewardStateActionModel:generateLossVector(previousFeatureVector, action, rewardValue, currentFeatureVector)
+
+		NewDeepDoubleStateActionRewardStateActionModel:saveModelParametersFromModelParametersArray(selectedModelNumberForTargetVector)
+
+		NewDeepDoubleStateActionRewardStateActionModel:loadModelParametersFromModelParametersArray(selectedModelNumberForUpdate)
+
+		Model:forwardPropagate(previousFeatureVector, true)
+		
+		Model:backPropagate(lossVector, true)
+
+		NewDeepDoubleStateActionRewardStateActionModel:saveModelParametersFromModelParametersArray(selectedModelNumberForUpdate)
+		
+		return lossVector
+
+	end)
+
+	return NewDeepDoubleStateActionRewardStateActionModel
+
+end
+
+function DeepDoubleStateActionRewardStateActionModel:setParameters(discountFactor)
+
+	self.discountFactor =  discountFactor or self.discountFactor
+
+end
+
+function DeepDoubleStateActionRewardStateActionModel:saveModelParametersFromModelParametersArray(index)
+
+	self.ModelParametersArray[index] = self.Model:getModelParameters()
+
+end
+
+function DeepDoubleStateActionRewardStateActionModel:loadModelParametersFromModelParametersArray(index)
+	
+	local Model = self.Model
+
+	local FirstModelParameters = self.ModelParametersArray[1]
+
+	local SecondModelParameters = self.ModelParametersArray[2]
+
+	if (FirstModelParameters == nil) and (SecondModelParameters == nil) then
+
+		Model:generateLayers()
+
+		self:saveModelParametersFromModelParametersArray(1)
+
+		self:saveModelParametersFromModelParametersArray(2)
+
+	end
+
+	local CurrentModelParameters = self.ModelParametersArray[index]
+
+	Model:setModelParameters(CurrentModelParameters, true)
+
+end
+
+function DeepDoubleStateActionRewardStateActionModel:generateLossVector(previousFeatureVector, action, rewardValue, currentFeatureVector)
+	
+	local Model = self.Model
+
+	local targetVector = Model:predict(currentFeatureVector, true)
+
+	local dicountedVector = AqwamMatrixLibrary:multiply(self.discountFactor, targetVector)
+
+	local newTargetVector = AqwamMatrixLibrary:add(rewardValue, dicountedVector)
+	
+	local previousVector = Model:predict(previousFeatureVector, true)
+	
+	local lossVector = AqwamMatrixLibrary:subtract(newTargetVector, previousVector)
+
+	return lossVector
+
+end
+
+function DeepDoubleStateActionRewardStateActionModel:setModelParameters1(ModelParameters1)
+
+	self.ModelParametersArray[1] = ModelParameters1
+
+end
+
+function DeepDoubleStateActionRewardStateActionModel:setModelParameters2(ModelParameters2)
+
+	self.ModelParametersArray[2] = ModelParameters2
+
+end
+
+function DeepDoubleStateActionRewardStateActionModel:getModelParameters1(ModelParameters1)
+
+	return self.ModelParametersArray[1]
+
+end
+
+function DeepDoubleStateActionRewardStateActionModel:getModelParameters2(ModelParameters2)
+
+	return self.ModelParametersArray[2]
+
+end
+
+return DeepDoubleStateActionRewardStateActionModel

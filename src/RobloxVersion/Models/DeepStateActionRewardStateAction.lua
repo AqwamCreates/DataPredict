3b414e3f066d2c0faf2a1e3ef2d0a9e600915a3a
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-05-22 19:20:31 +0800

    Update and rename StateActionRewardStateActionNeuralNetwork.lua to DeepStateActionRewardStateAction.lua

---
+++ src/RobloxVersion/Models/DeepStateActionRewardStateAction.lua
@@ -0,0 +1,49 @@
+local AqwamMatrixLibrary = require(script.Parent.Parent.AqwamMatrixLibraryLinker.Value)
+
+local ReinforcementLearningBaseModel = require(script.Parent.ReinforcementLearningBaseModel)
+
+DeepStateActionRewardStateActionModel = {}
+
+DeepStateActionRewardStateActionModel.__index = DeepStateActionRewardStateActionModel
+
+setmetatable(DeepStateActionRewardStateActionModel, ReinforcementLearningBaseModel)
+
+function DeepStateActionRewardStateActionModel.new(discountFactor)
+
+	local NewDeepStateActionRewardStateActionModel = ReinforcementLearningBaseModel.new(discountFactor)
+
+	setmetatable(NewDeepStateActionRewardStateActionModel, DeepStateActionRewardStateActionModel)
+
+	NewDeepStateActionRewardStateActionModel:setUpdateFunction(function(previousFeatureVector, action, rewardValue, currentFeatureVector)
+		
+		local Model = NewDeepStateActionRewardStateActionModel.Model
+
+		local qVector = Model:predict(currentFeatureVector, true)
+
+		local discountedQVector = AqwamMatrixLibrary:multiply(NewDeepStateActionRewardStateActionModel.discountFactor, qVector)
+
+		local targetVector = AqwamMatrixLibrary:add(rewardValue, discountedQVector)
+
+		local previousQVector = Model:predict(previousFeatureVector, true)
+
+		local temporalDifferenceVector = AqwamMatrixLibrary:subtract(targetVector, previousQVector)
+		
+		Model:forwardPropagate(previousFeatureVector, true)
+
+		Model:backPropagate(temporalDifferenceVector, true)
+		
+		return temporalDifferenceVector
+
+	end)
+
+	return NewDeepStateActionRewardStateActionModel
+
+end
+
+function DeepStateActionRewardStateActionModel:setParameters(discountFactor)
+
+	self.discountFactor =  discountFactor or self.discountFactor
+
+end
+
+return DeepStateActionRewardStateActionModel

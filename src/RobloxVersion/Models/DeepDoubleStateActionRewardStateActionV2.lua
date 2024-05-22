5292987fb4eada34dcc20bf342ba0ba7d87de5b8
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-05-22 20:54:32 +0800

    Update and rename DoubleStateActionRewardStateActionNeuralNetworkV2.lua to DeepDoubleStateActionRewardStateActionV2.lua

---
+++ src/RobloxVersion/Models/DeepDoubleStateActionRewardStateActionV2.lua
@@ -0,0 +1,83 @@
+local AqwamMatrixLibrary = require(script.Parent.Parent.AqwamMatrixLibraryLinker.Value)
+
+local ReinforcementLearningBaseModel = require(script.Parent.ReinforcementLearningBaseModel)
+
+DeepDoubleStateActionRewardStateActionModel = {}
+
+DeepDoubleStateActionRewardStateActionModel.__index = DeepDoubleStateActionRewardStateActionModel
+
+setmetatable(DeepDoubleStateActionRewardStateActionModel, ReinforcementLearningBaseModel)
+
+local defaultAveragingRate = 0.01
+
+local function rateAverageModelParameters(averagingRate, PrimaryModelParameters, TargetModelParameters)
+
+	local averagingRateComplement = 1 - averagingRate
+
+	for layer = 1, #TargetModelParameters, 1 do
+
+		local PrimaryModelParametersPart = AqwamMatrixLibrary:multiply(averagingRate, PrimaryModelParameters[layer])
+
+		local TargetModelParametersPart = AqwamMatrixLibrary:multiply(averagingRateComplement, TargetModelParameters[layer])
+
+		TargetModelParameters[layer] = AqwamMatrixLibrary:add(PrimaryModelParametersPart, TargetModelParametersPart)
+
+	end
+
+	return TargetModelParameters
+
+end
+
+function DeepDoubleStateActionRewardStateActionModel.new(averagingRate, discountFactor)
+
+	local NewDeepDoubleStateActionRewardStateActionModel = ReinforcementLearningBaseModel.new(discountFactor)
+
+	setmetatable(NewDeepDoubleStateActionRewardStateActionModel, DeepDoubleStateActionRewardStateActionModel)
+
+	NewDeepDoubleStateActionRewardStateActionModel.averagingRate = averagingRate or defaultAveragingRate
+
+	NewDeepDoubleStateActionRewardStateActionModel:setUpdateFunction(function(previousFeatureVector, action, rewardValue, currentFeatureVector)
+		
+		local Model = NewDeepDoubleStateActionRewardStateActionModel.Model
+
+		if (Model:getModelParameters() == nil) then NewDeepDoubleStateActionRewardStateActionModel:generateLayers() end
+
+		local PrimaryModelParameters = Model:getModelParameters()
+
+		local qVector = Model:predict(currentFeatureVector, true)
+
+		local discountedQVector = AqwamMatrixLibrary:multiply(NewDeepDoubleStateActionRewardStateActionModel.discountFactor, qVector)
+
+		local targetVector = AqwamMatrixLibrary:add(rewardValue, discountedQVector)
+
+		local previousQVector = Model:predict(previousFeatureVector, true)
+
+		local temporalDifferenceVector = AqwamMatrixLibrary:subtract(targetVector, previousQVector)
+
+		Model:forwardPropagate(previousFeatureVector, true)
+
+		Model:backPropagate(temporalDifferenceVector, true)
+		
+		local TargetModelParameters = Model:getModelParameters(true)
+
+		TargetModelParameters = rateAverageModelParameters(NewDeepDoubleStateActionRewardStateActionModel.averagingRate, PrimaryModelParameters, TargetModelParameters)
+
+		Model:setModelParameters(TargetModelParameters, true)
+		
+		return temporalDifferenceVector
+
+	end)
+
+	return NewDeepDoubleStateActionRewardStateActionModel
+
+end
+
+function DeepDoubleStateActionRewardStateActionModel:setParameters(averagingRate, discountFactor)
+
+	self.discountFactor =  discountFactor or self.discountFactor
+
+	self.averagingRate = averagingRate or self.averagingRate
+
+end
+
+return DeepDoubleStateActionRewardStateActionModel

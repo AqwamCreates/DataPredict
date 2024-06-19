4c703d1ede57c15098d4d6eda67b964ba4752605
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-06-20 05:19:17 +0800

    Update OneVsAll.lua

--- src/RobloxVersion/Others/OneVsAll.lua
+++ src/RobloxVersion/Others/OneVsAll.lua
@@ -318,7 +318,7 @@ function OneVsAll:getBestPrediction(featureVector)
 		
 		if (typeof(allOutputVector) == "number") then allOutputVector = {{allOutputVector}} end
 
-		local value, maximumValueIndex = AqwamMatrixLibrary:findMaximumValueInMatrix(allOutputVector)
+		local value, maximumValueIndex = AqwamMatrixLibrary:findMaximumValue(allOutputVector)
 
 		if (maximumValueIndex == nil) then continue end
 

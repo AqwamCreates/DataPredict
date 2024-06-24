1634c13cea97927354159a8866a2ff2e493d2f17
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-06-24 20:50:33 +0800

    Update WassersteinGenerativeAdversarialNetwork.lua

--- src/RobloxVersion/Models/WassersteinGenerativeAdversarialNetwork.lua
+++ src/RobloxVersion/Models/WassersteinGenerativeAdversarialNetwork.lua
@@ -68,15 +68,7 @@ end
 
 function WassersteinGenerativeAdversarialNetworkModel:setPrintOutput(option)
 	
-	if (option == false) then
-
-		self.isOutputPrinted = false
-
-	else
-
-		self.isOutputPrinted = true
-
-	end
+	self.isOutputPrinted = option
 	
 end
 

474785f16fa335b643909ffc90127489f9cc6e63
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-06-24 23:55:07 +0800

    Update Regularization.lua

--- src/RobloxVersion/Others/Regularization.lua
+++ src/RobloxVersion/Others/Regularization.lua
@@ -94,7 +94,7 @@ function Regularization:calculateRegularizationDerivatives(ModelParameters)
 
 	else
 
-		error("Regularization Mode Does Not Exist!")
+		error("Regularization mode does not exist!")
 
 	end
 	
@@ -154,10 +154,10 @@ function Regularization:calculateRegularization(ModelParameters)
 		
 	else
 		
-		error("Regularization Mode Does Not Exist!")
+		error("Regularization mode does not exist!")
 		
 	end
-
+	
 	regularizationValue = regularizationValue / 2
 	
 	return regularizationValue

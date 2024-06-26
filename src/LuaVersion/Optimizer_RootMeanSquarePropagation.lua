455904eb010320da55e9046352f7e1ea9275728e
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-06-27 05:35:23 +0800

    Update Optimizer_RootMeanSquarePropagation.lua

--- src/LuaVersion/Optimizer_RootMeanSquarePropagation.lua
+++ src/LuaVersion/Optimizer_RootMeanSquarePropagation.lua
@@ -44,43 +44,33 @@ function RootMeanSquarePropagationOptimizer.new(beta, epsilon)
 	
 	NewRootMeanSquarePropagationOptimizer.epsilon = epsilon or defaultEpsilonValue
 	
-	NewRootMeanSquarePropagationOptimizer.previousVelocityMatrix = nil
-	
 	--------------------------------------------------------------------------------
 	
 	NewRootMeanSquarePropagationOptimizer:setCalculateFunction(function(learningRate, costFunctionDerivatives)
 		
-		NewRootMeanSquarePropagationOptimizer.previousVelocityMatrix = NewRootMeanSquarePropagationOptimizer.previousVelocityMatrix or AqwamMatrixLibrary:createMatrix(#costFunctionDerivatives, #costFunctionDerivatives[1])
+		local previousVelocity = NewRootMeanSquarePropagationOptimizer.optimizerInternalParameters[1] or AqwamMatrixLibrary:createMatrix(#costFunctionDerivatives, #costFunctionDerivatives[1])
 		
 		local beta = NewRootMeanSquarePropagationOptimizer.beta
 
 		local squaredCostFunctionDerivatives = AqwamMatrixLibrary:power(costFunctionDerivatives, 2)
 
-		local vMatrixPart1 = AqwamMatrixLibrary:multiply(beta, NewRootMeanSquarePropagationOptimizer.previousVelocityMatrix)
+		local vPart1 = AqwamMatrixLibrary:multiply(beta, NewRootMeanSquarePropagationOptimizer.previousVelocity)
 
-		local vMatrixPart2 = AqwamMatrixLibrary:multiply((1 - beta), squaredCostFunctionDerivatives)
+		local vPart2 = AqwamMatrixLibrary:multiply((1 - beta), squaredCostFunctionDerivatives)
 
-		local currentVelocityMatrix = AqwamMatrixLibrary:add(vMatrixPart1, vMatrixPart2)
+		local velocity = AqwamMatrixLibrary:add(vPart1, vPart2)
 
-		local nonZeroDivisorMatrix = AqwamMatrixLibrary:add(currentVelocityMatrix, NewRootMeanSquarePropagationOptimizer.epsilon)
+		local velocityNonZeroDivisor = AqwamMatrixLibrary:add(velocity, NewRootMeanSquarePropagationOptimizer.epsilon)
 
-		local squaredRootVelocityMatrix = AqwamMatrixLibrary:power(nonZeroDivisorMatrix, 0.5)
+		local squaredRootVelocityMatrix = AqwamMatrixLibrary:power(velocityNonZeroDivisor, 0.5)
 
 		local costFunctionDerivativesPart1 = AqwamMatrixLibrary:divide(costFunctionDerivatives, squaredRootVelocityMatrix)
 
 		local costFunctionDerivatives = AqwamMatrixLibrary:multiply(learningRate, costFunctionDerivativesPart1)
 
-		NewRootMeanSquarePropagationOptimizer.previousVelocityMatrix = currentVelocityMatrix
+		NewRootMeanSquarePropagationOptimizer.optimizerInternalParameters = {velocity}
 
 		return costFunctionDerivatives
-		
-	end)
-	
-	--------------------------------------------------------------------------------
-	
-	NewRootMeanSquarePropagationOptimizer:setResetFunction(function()
-		
-		NewRootMeanSquarePropagationOptimizer.previousVelocityMatrix = nil
 		
 	end)
 	

6fc643b8e2ae9e3651d9758de457f9d80702f01c
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-06-30 12:06:29 +0800

    Update NeuralNetwork.lua

--- src/RobloxVersion/Models/NeuralNetwork.lua
+++ src/RobloxVersion/Models/NeuralNetwork.lua
@@ -632,7 +632,7 @@ function NeuralNetworkModel:calculateCostFunctionDerivativeMatrixTable(lossMatri
 
 end
 
-function NeuralNetworkModel:gradientDescent(costFunctionDerivativeMatrixTable, numberOfData)
+function NeuralNetworkModel:gradientDescent(costFunctionDerivativeMatrixTable)
 	
 	local NewModelParameters = {}
 	
@@ -669,8 +669,6 @@ function NeuralNetworkModel:gradientDescent(costFunctionDerivativeMatrixTable, n
 			costFunctionDerivativeMatrix = AqwamMatrixLibrary:add(costFunctionDerivativeMatrix, regularizationDerivativeMatrix)
 
 		end
-		
-		costFunctionDerivativeMatrix = AqwamMatrixLibrary:divide(costFunctionDerivativeMatrix, numberOfData)
 
 		if (Optimizer ~= 0) then
 

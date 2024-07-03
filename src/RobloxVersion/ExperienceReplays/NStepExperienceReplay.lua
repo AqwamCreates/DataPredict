9ec5af66d543bc581aaf4baf9f160ccc39c3fc62
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-07-04 07:21:04 +0800

    Update NStepExperienceReplay.lua

--- src/RobloxVersion/ExperienceReplays/NStepExperienceReplay.lua
+++ src/RobloxVersion/ExperienceReplays/NStepExperienceReplay.lua
@@ -9,33 +9,67 @@ setmetatable(NStepExperienceReplay, BaseExperienceReplay)
 local defaultNStep = 3
 
 function NStepExperienceReplay.new(batchSize, numberOfExperienceToUpdate, maxBufferSize, nStep)
-	
+
 	local NewNStepExperienceReplay = BaseExperienceReplay.new(batchSize, numberOfExperienceToUpdate, maxBufferSize)
-	
+
 	setmetatable(NewNStepExperienceReplay, NStepExperienceReplay)
-	
+
 	NewNStepExperienceReplay.nStep = nStep or defaultNStep
-	
+
 	NewNStepExperienceReplay:setSampleFunction(function()
-		
+
 		local batchArray = {}
 
-		local lowestNumberOfBatchSize = math.min(NewNStepExperienceReplay.batchSize, #NewNStepExperienceReplay.replayBufferArray)
+		local replayBufferArray = NewNStepExperienceReplay.replayBufferArray
+
+		local replayBufferArraySize = #replayBufferArray
+
+		local lowestNumberOfBatchSize = math.min(NewNStepExperienceReplay.batchSize, replayBufferArraySize)
 
 		for i = 1, lowestNumberOfBatchSize, 1 do
 
-			local index = Random.new():NextInteger(1, #NewNStepExperienceReplay.replayBufferArray)
+			local index = Random.new():NextInteger(1, replayBufferArraySize)
 
-			table.insert(batchArray, NewNStepExperienceReplay.replayBufferArray[index])
+			table.insert(batchArray, replayBufferArray[index])
 
 		end
 
 		return batchArray
-		
+
 	end)
-	
+
+	NewNStepExperienceReplay:setRunFunction(function(updateFunction)
+
+		local experienceReplayBatchArray = NewNStepExperienceReplay:sample()
+		
+		local nStep = NewNStepExperienceReplay.nStep
+
+		for _, experience in ipairs(experienceReplayBatchArray) do
+
+			local nStepRewards = 0
+
+			local previousState = experience[1]
+
+			local action = experience[2]
+			
+			local currentState = experience[4]
+
+			for i = 1, nStep do
+
+				if (not experienceReplayBatchArray[i]) then continue end
+
+				nStepRewards += experienceReplayBatchArray[i][3]
+
+			end
+
+			updateFunction(previousState, action, nStepRewards, currentState)
+
+		end
+
+	end)
+
 	return NewNStepExperienceReplay
-	
+
 end
 
 function NStepExperienceReplay:setParameters(batchSize, numberOfExperienceToUpdate, maxBufferSize, nStep)
@@ -45,43 +79,9 @@ function NStepExperienceReplay:setParameters(batchSize, numberOfExperienceToUpda
 	self.numberOfExperienceToUpdate = numberOfExperienceToUpdate or self.numberOfExperienceToUpdate
 
 	self.maxBufferSize = maxBufferSize or self.maxBufferSize
-	
+
 	self.nStep = nStep or self.nStep
 
-end
-
-function NStepExperienceReplay:run(updateFunction)
-	
-	if self.numberOfExperience < self.numberOfExperienceToUpdate then return end
-
-	self.numberOfExperience = 0
-
-	local experienceReplayBatchArray = self:sample()
-
-	for _, experience in ipairs(experienceReplayBatchArray) do
-		
-		local nStepRewards = 0
-		
-		local currentState = experience[4]
-		
-		local previousState = experience[1]
-		
-		local action = experience[2]
-		
-		local nStep = self.nStep
-		
-		for i = 1, nStep do
-			
-			if not experienceReplayBatchArray[i] then break end
-			
-			nStepRewards += experienceReplayBatchArray[i][3]
-			
-		end
-
-		updateFunction(previousState, action, nStepRewards, currentState)
-		
-	end
-	
 end
 
 return NStepExperienceReplay

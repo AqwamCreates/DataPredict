84b6054912bbe56510faf0d356ae765848ea231b
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-07-04 18:10:36 +0800

    Update ExperienceReplay_NStepExperienceReplay.lua

--- src/LuaVersion/ExperienceReplay_NStepExperienceReplay.lua
+++ src/LuaVersion/ExperienceReplay_NStepExperienceReplay.lua
@@ -30,8 +30,6 @@ setmetatable(NStepExperienceReplay, BaseExperienceReplay)
 
 local defaultNStep = 3
 
-local defaultDiscountFactor = 0.95
-
 local function sample(replayBufferArray, batchSize)
 
 	local batchArray = {}

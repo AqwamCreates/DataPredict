3c5619ac9b62600701a43868ff236bc1778abc36
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-07-08 20:14:19 +0800

    Update Other_ReinforcementLearningQuickSetup.lua

--- src/LuaVersion/Other_ReinforcementLearningQuickSetup.lua
+++ src/LuaVersion/Other_ReinforcementLearningQuickSetup.lua
@@ -226,15 +226,13 @@ function ReinforcementLearningQuickSetup:getLabelFromOutputMatrix(outputMatrix)
 
 end
 
-function ReinforcementLearningQuickSetup:selectAction(currentFeatureVector, ClassesList, childModelNumber)
-	
-	local allOutputsMatrix = self.Model:predict(currentFeatureVector, true, childModelNumber)
-	
-	local actionSelectionFunction = self.actionSelectionFunction
+function ReinforcementLearningQuickSetup:selectAction(allOutputsMatrix, ClassesList)
 	
 	local action
 	
 	local selectedValue
+	
+	local actionSelectionFunction = self.actionSelectionFunction
 	
 	if (actionSelectionFunction == "Maximum") then
 		
@@ -256,7 +254,7 @@ function ReinforcementLearningQuickSetup:selectAction(currentFeatureVector, Clas
 		
 	end
 	
-	return action, selectedValue, allOutputsMatrix
+	return action, selectedValue
 	
 end
 
@@ -299,8 +297,10 @@ function ReinforcementLearningQuickSetup:reinforce(currentFeatureVector, rewardV
 		allOutputsMatrix[1][randomNumber] = randomProbability
 
 	else
+		
+		allOutputsMatrix = self.Model:predict(currentFeatureVector, true, childModelNumber)
 
-		action, selectedValue, allOutputsMatrix = self:selectAction(currentFeatureVector, ClassesList, childModelNumber)
+		action, selectedValue = self:selectAction(allOutputsMatrix, ClassesList)
 
 	end
 

ce24cc1228915b22d5277392a91b051c8e7c26cc
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-08-04 19:22:38 +0800

    Update DeepDoubleQLearningV2.lua

--- src/RobloxVersion/Models/DeepDoubleQLearningV2.lua
+++ src/RobloxVersion/Models/DeepDoubleQLearningV2.lua
@@ -28,13 +28,13 @@
 
 local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
 
-local ReinforcementLearningBaseModel = require(script.Parent.ReinforcementLearningBaseModel)
+local DeepReinforcementLearningBaseModel = require(script.Parent.DeepReinforcementLearningBaseModel)
 
 DeepDoubleQLearningModel = {}
 
 DeepDoubleQLearningModel.__index = DeepDoubleQLearningModel
 
-setmetatable(DeepDoubleQLearningModel, ReinforcementLearningBaseModel)
+setmetatable(DeepDoubleQLearningModel, DeepReinforcementLearningBaseModel)
 
 local defaultAveragingRate = 0.995
 
@@ -62,7 +62,7 @@ function DeepDoubleQLearningModel.new(parameterDictionary)
 	
 	parameterDictionary = parameterDictionary or {}
 
-	local NewDeepDoubleQLearningModel = ReinforcementLearningBaseModel.new(parameterDictionary)
+	local NewDeepDoubleQLearningModel = DeepReinforcementLearningBaseModel.new(parameterDictionary)
 
 	setmetatable(NewDeepDoubleQLearningModel, DeepDoubleQLearningModel)
 	

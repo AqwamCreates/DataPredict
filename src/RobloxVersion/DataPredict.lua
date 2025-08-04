69c8b2f5bb84935a5b9fe88a346cf8aef4a1f79f
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-08-04 20:25:35 +0800

    Update DataPredict.lua

--- src/RobloxVersion/DataPredict.lua
+++ src/RobloxVersion/DataPredict.lua
@@ -1,3 +1,31 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
 local AqwamMachineAndDeepLearningLibrary = {}
 
 local Models = script.Models

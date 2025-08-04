80305128b47248c3dc32aa40a7866f3248815a8b
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-08-04 23:31:01 +0800

    Update Model_DeepReinforcementLearningBaseModel.lua

--- src/LuaVersion/Model_DeepReinforcementLearningBaseModel.lua
+++ src/LuaVersion/Model_DeepReinforcementLearningBaseModel.lua
@@ -26,7 +26,7 @@
 
 --]]
 
-local BaseInstance = require("Cores_BaseInstance")
+local BaseInstance = require("Core_BaseInstance")
 
 DeepReinforcementLearningBaseModel = {}
 
@@ -86,7 +86,7 @@ function DeepReinforcementLearningBaseModel:predict(featureVector, returnOrigina
 
 end
 
-function DeepReinforcementLearningBaseModel:getClassesList()
+function DeepReinforcementLearningBaseModel:getActionsList()
 	
 	return self.Model:getClassesList()
 	

d1c3487f0b0087da2f7f0b68c090c4d8e5f2fa6e
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-08-05 13:12:37 +0800

    Create Regularizer_ElasticNet.lua

---
+++ src/LuaVersion/Regularizer_ElasticNet.lua
@@ -0,0 +1,63 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+
+local BaseRegularizer = require(script.Parent.BaseRegularizer)
+
+ElasticNet = {}
+
+ElasticNet.__index = ElasticNet
+
+setmetatable(ElasticNet, BaseRegularizer)
+
+function ElasticNet.new(parameterDictionary)
+	
+	local NewElasticNet = BaseRegularizer.new(parameterDictionary)
+	
+	setmetatable(NewElasticNet, ElasticNet)
+	
+	NewElasticNet:setName("ElasticNet")
+	
+	NewElasticNet:setCalculateFunction(function(ModelParameters)
+		
+		local signMatrix = AqwamTensorLibrary:applyFunction(math.sign, ModelParameters)
+
+		local regularizationMatrixPart1 = AqwamTensorLibrary:multiply(NewElasticNet.lambda, signMatrix)
+
+		local regularizationMatrixPart2 = AqwamTensorLibrary:multiply(2, NewElasticNet.lambda, ModelParameters)
+
+		return AqwamTensorLibrary:add(regularizationMatrixPart1, regularizationMatrixPart2)
+		
+	end)
+	
+	return NewElasticNet
+	
+end
+
+return ElasticNet

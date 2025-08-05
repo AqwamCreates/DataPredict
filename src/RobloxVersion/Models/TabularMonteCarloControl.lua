8c95191065486e61b00f13a5cbf564f833eab79d
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-08-05 21:59:09 +0800

    Create TabularMonteCarloControl.lua

---
+++ src/RobloxVersion/Models/TabularMonteCarloControl.lua
@@ -0,0 +1,111 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+
+local TabularReinforcementLearningBaseModel = require(script.Parent.TabularReinforcementLearningBaseModel)
+
+TabularMonteCarloControlModel = {}
+
+TabularMonteCarloControlModel.__index = TabularMonteCarloControlModel
+
+setmetatable(TabularMonteCarloControlModel, TabularReinforcementLearningBaseModel)
+
+local function calculateRewardToGo(rewardValueHistory, discountFactor)
+
+	local rewardToGoArray = {}
+
+	local discountedReward = 0
+
+	for h = #rewardValueHistory, 1, -1 do
+
+		discountedReward = rewardValueHistory[h] + (discountFactor * discountedReward)
+
+		table.insert(rewardToGoArray, 1, discountedReward)
+
+	end
+
+	return rewardToGoArray
+
+end
+
+function TabularMonteCarloControlModel.new(parameterDictionary)
+
+	local NewTabularMonteCarloControlModel = TabularReinforcementLearningBaseModel.new(parameterDictionary)
+	
+	setmetatable(NewTabularMonteCarloControlModel, TabularMonteCarloControlModel)
+	
+	NewTabularMonteCarloControlModel:setName("TabularMonteCarloControl")
+	
+	local featureVectorHistory = {}
+	
+	local rewardValueHistory = {}
+	
+	NewTabularMonteCarloControlModel:setCategoricalUpdateFunction(function(previousState, action, rewardValue, currentState, terminalStateValue)
+		
+		table.insert(featureVectorHistory, previousState)
+		
+		table.insert(rewardValueHistory, rewardValue)
+
+	end)
+	
+	NewTabularMonteCarloControlModel:setEpisodeUpdateFunction(function(terminalStateValue)
+		
+		local Model = NewTabularMonteCarloControlModel.Model
+		
+		local rewardToGoArray = calculateRewardToGo(rewardValueHistory, NewTabularMonteCarloControlModel.discountFactor)
+		
+		for h, featureVector in ipairs(featureVectorHistory) do
+			
+			local averageRewardToGo = rewardToGoArray[h] / h
+			
+			Model:forwardPropagate(featureVector, true)
+
+			Model:update(averageRewardToGo, true)
+			
+		end
+		
+		table.clear(featureVectorHistory)
+		
+		table.clear(rewardValueHistory)
+		
+	end)
+	
+	NewTabularMonteCarloControlModel:setResetFunction(function()
+		
+		table.clear(featureVectorHistory)
+		
+		table.clear(rewardValueHistory)
+		
+	end)
+	
+	return NewTabularMonteCarloControlModel
+
+end
+
+return TabularMonteCarloControlModel

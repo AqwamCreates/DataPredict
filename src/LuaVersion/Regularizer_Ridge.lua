179060d570bf8018039ac31e2ffd68148f60e509
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-08-05 13:13:14 +0800

    Create Regularizer_Ridge.lua

---
+++ src/LuaVersion/Regularizer_Ridge.lua
@@ -0,0 +1,57 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+
+local BaseRegularizer = require(script.Parent.BaseRegularizer)
+
+Ridge = {}
+
+Ridge.__index = Ridge
+
+setmetatable(Ridge, BaseRegularizer)
+
+function Ridge.new(parameterDictionary)
+	
+	local NewRidge = BaseRegularizer.new(parameterDictionary)
+	
+	setmetatable(NewRidge, Ridge)
+	
+	NewRidge:setName("Ridge")
+	
+	NewRidge:setCalculateFunction(function(ModelParameters)
+		
+		return AqwamTensorLibrary:multiply(2, NewRidge.lambda, ModelParameters)
+		
+	end)
+	
+	return NewRidge
+	
+end
+
+return Ridge

69d0d9f1704ee2b139e004ea9a40261bb8793467
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-08-06 17:55:43 +0800

    Create Model_TabularQLearning.lua

---
+++ src/LuaVersion/Model_TabularQLearning.lua
@@ -0,0 +1,123 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require("AqwamTensorLibrary")
+
+local TabularReinforcementLearningBaseModel = require("Model_TabularReinforcementLearningBaseModel")
+
+TabularQLearningModel = {}
+
+TabularQLearningModel.__index = TabularQLearningModel
+
+setmetatable(TabularQLearningModel, TabularReinforcementLearningBaseModel)
+
+local defaultLearningRate = 0.1
+
+local defaultLambda = 0
+
+function TabularQLearningModel.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+
+	local NewTabularQLearning = TabularReinforcementLearningBaseModel.new(parameterDictionary)
+	
+	setmetatable(NewTabularQLearning, TabularQLearningModel)
+	
+	NewTabularQLearning:setName("TabularQLearning")
+	
+	NewTabularQLearning.learningRate = parameterDictionary.learningRate or defaultLearningRate
+	
+	NewTabularQLearning.lambda = parameterDictionary.lambda or defaultLambda
+	
+	NewTabularQLearning.eligibilityTraceMatrix = parameterDictionary.eligibilityTraceMatrix
+	
+	NewTabularQLearning:setCategoricalUpdateFunction(function(previousStateValue, action, rewardValue, currentStateValue, terminalStateValue)
+		
+		local discountFactor = NewTabularQLearning.discountFactor
+		
+		local lambda = NewTabularQLearning.lambda
+		
+		local StatesList = NewTabularQLearning:getStatesList()
+
+		local ActionsList = NewTabularQLearning:getActionsList()
+
+		local ModelParameters = NewTabularQLearning.ModelParameters
+
+		local _, maxQValue = NewTabularQLearning:predict({{currentStateValue}})
+
+		local targetValue = rewardValue + (discountFactor * (1 - terminalStateValue) * maxQValue[1][1])
+		
+		local stateIndex = table.find(StatesList, previousStateValue)
+
+		local actionIndex = table.find(ActionsList, action)
+
+		local lastValue = ModelParameters[stateIndex][actionIndex]
+
+		local temporalDifferenceError = targetValue - lastValue
+		
+		if (lambda ~= 0) then
+			
+			local eligibilityTraceMatrix = NewTabularQLearning.eligibilityTraceMatrix
+			
+			if (not eligibilityTraceMatrix) then eligibilityTraceMatrix = AqwamTensorLibrary:createTensor({#StatesList, #ActionsList}, 0) end
+			
+			eligibilityTraceMatrix = AqwamTensorLibrary:multiply(eligibilityTraceMatrix, discountFactor * lambda)
+			
+			local eligibilityTraceValue = eligibilityTraceMatrix[stateIndex][actionIndex] + 1
+			
+			eligibilityTraceMatrix[stateIndex][actionIndex] = eligibilityTraceValue
+			
+			temporalDifferenceError = temporalDifferenceError * eligibilityTraceValue
+			
+			NewTabularQLearning.eligibilityTraceMatrix = eligibilityTraceMatrix
+			
+		end
+		
+		ModelParameters[stateIndex][actionIndex] = ModelParameters[stateIndex][actionIndex] + (NewTabularQLearning.learningRate * temporalDifferenceError)
+		
+		return temporalDifferenceError
+
+	end)
+	
+	NewTabularQLearning:setEpisodeUpdateFunction(function(terminalStateValue)
+		
+		NewTabularQLearning.eligibilityTraceMatrix = nil
+		
+	end)
+
+	NewTabularQLearning:setResetFunction(function()
+		
+		NewTabularQLearning.eligibilityTraceMatrix = nil
+		
+	end)
+
+	return NewTabularQLearning
+
+end
+
+return TabularQLearningModel

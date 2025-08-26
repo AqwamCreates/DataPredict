1abae7a754ecbccb6df4f2676d5aeea8a45f3330
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-08-26 09:29:57 +0800

    Create EligibilityTrace_AccumulatingTrace.lua

---
+++ src/LuaVersion/EligibilityTrace_AccumulatingTrace.lua
@@ -0,0 +1,59 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require("AqwamTensorLibrary")
+
+local BaseEligibilityTrace = require("EligibilityTrace_BaseEligibilityTrace")
+
+AccumulatingTrace = {}
+
+AccumulatingTrace.__index = AccumulatingTrace
+
+setmetatable(AccumulatingTrace, BaseEligibilityTrace)
+
+function AccumulatingTrace.new(parameterDictionary)
+	
+	local NewAccumulatingTrace = BaseEligibilityTrace.new(parameterDictionary)
+	
+	setmetatable(NewAccumulatingTrace, AccumulatingTrace)
+	
+	NewAccumulatingTrace:setName("AccumulatingTrace")
+	
+	NewAccumulatingTrace:setIncrementFunction(function(eligibilityTraceMatrix, actionIndex)
+
+		eligibilityTraceMatrix[1][actionIndex] = eligibilityTraceMatrix[1][actionIndex] + 1
+		
+		return eligibilityTraceMatrix
+		
+	end)
+	
+	return NewAccumulatingTrace
+	
+end
+
+return AccumulatingTrace

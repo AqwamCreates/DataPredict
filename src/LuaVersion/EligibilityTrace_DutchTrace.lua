8e938cf3be8b4709cbb8912c6a5e4367fdb18c18
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-08-26 09:31:36 +0800

    Create EligibilityTrace_DutchTrace.lua

---
+++ src/LuaVersion/EligibilityTrace_DutchTrace.lua
@@ -0,0 +1,65 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require("AqwamTensorLibrary")
+
+local BaseEligibilityTrace = require("EligibilityTrace_BaseEligibilityTrace")
+
+DutchTrace = {}
+
+DutchTrace.__index = DutchTrace
+
+setmetatable(DutchTrace, BaseEligibilityTrace)
+
+local defaultAlpha = 0.5
+
+function DutchTrace.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local NewDutchTrace = BaseEligibilityTrace.new(parameterDictionary)
+	
+	setmetatable(NewDutchTrace, DutchTrace)
+	
+	NewDutchTrace:setName("DutchTrace")
+	
+	NewDutchTrace.alpha = parameterDictionary.alpha or defaultAlpha
+	
+	NewDutchTrace:setIncrementFunction(function(eligibilityTraceMatrix, actionIndex)
+
+		eligibilityTraceMatrix[1][actionIndex] = ((1 - NewDutchTrace.alpha) * eligibilityTraceMatrix[1][actionIndex]) + 1
+		
+		return eligibilityTraceMatrix
+		
+	end)
+	
+	return NewDutchTrace
+	
+end
+
+return DutchTrace

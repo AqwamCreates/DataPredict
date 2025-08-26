e07f51081eda9ecf1c5fadc7aaf2902c71dd45da
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-08-26 09:30:48 +0800

    Create EligibilityTrace_ReplacingTrace.lua

---
+++ src/LuaVersion/EligibilityTrace_ReplacingTrace.lua
@@ -0,0 +1,59 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require("AqwamTensorLibrary")
+
+local BaseEligibilityTrace = require("EligibilityTrace_BaseEligibilityTrace")
+
+ReplacingTrace = {}
+
+ReplacingTrace.__index = ReplacingTrace
+
+setmetatable(ReplacingTrace, BaseEligibilityTrace)
+
+function ReplacingTrace.new(parameterDictionary)
+	
+	local NewReplacingTrace = BaseEligibilityTrace.new(parameterDictionary)
+	
+	setmetatable(NewReplacingTrace, ReplacingTrace)
+	
+	NewReplacingTrace:setName("ReplacingTrace")
+	
+	NewReplacingTrace:setIncrementFunction(function(eligibilityTraceMatrix, actionIndex)
+		
+		eligibilityTraceMatrix[1][actionIndex] = 1
+		
+		return eligibilityTraceMatrix
+		
+	end)
+	
+	return NewReplacingTrace
+	
+end
+
+return ReplacingTrace

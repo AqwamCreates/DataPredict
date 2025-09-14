68162c1b16951e97e5d0bc9c23eadc9aaccce391
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-09-14 09:01:19 +0800

    Update Model_ExpectationMaximization.lua

--- src/LuaVersion/Model_ExpectationMaximization.lua
+++ src/LuaVersion/Model_ExpectationMaximization.lua
@@ -26,7 +26,7 @@
 
 --]]
 
-local IterativeMethodBaseModel = require("Model_IterativeMethodBaseModel")
+local IterativeMethodBaseModel = require(script.Parent.IterativeMethodBaseModel)
 
 local ExpectationMaximizationModel = {}
 
@@ -34,7 +34,7 @@ ExpectationMaximizationModel.__index = ExpectationMaximizationModel
 
 setmetatable(ExpectationMaximizationModel, IterativeMethodBaseModel)
 
-local AqwamTensorLibrary = require("AqwamTensorLibrary")
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
 
 local defaultMaximumNumberOfIterations = 10
 
@@ -170,6 +170,8 @@ end
 
 function ExpectationMaximizationModel:getBayesianInformationCriterion(featureMatrix, numberOfClusters, epsilon)
 	
+	local numberOfData = #featureMatrix
+	
 	local numberOfFeatures = #featureMatrix[1]
 	
 	local piMatrix, meanMatrix, varianceMatrix = self:initializeParameters(numberOfClusters, numberOfFeatures)
@@ -183,8 +185,6 @@ function ExpectationMaximizationModel:getBayesianInformationCriterion(featureMat
 	local logLikelihood = AqwamTensorLibrary:logarithm(gaussianMatrix)
 
 	local sumLogLikelihood = AqwamTensorLibrary:sum(logLikelihood)
-	
-	local numberOfData = #featureMatrix
 	
 	local k = (numberOfClusters - 1) + numberOfClusters * numberOfFeatures * 2
 	

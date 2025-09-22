00997227083ea6cd85562c477ae0a7353492a79e
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-09-22 19:29:46 +0800

    Create Other_ModelParametersSafeguardWrapper.lua

---
+++ src/LuaVersion/Other_ModelParametersSafeguardWrapper.lua
@@ -0,0 +1,385 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require("AqwamTensorLibrary")
+
+local BaseInstance = require("Core_BaseInstance")
+
+local ModelParametersSafeguardWrapper = {}
+
+ModelParametersSafeguardWrapper.__index = ModelParametersSafeguardWrapper
+
+setmetatable(ModelParametersSafeguardWrapper, BaseInstance)
+
+local defaultIgnoreUpdateOnDefect = false
+
+local defaultRemoveDefectiveDataOnDefect = true
+
+local defaultStoreDefectiveUpdateInformation = false
+
+local defaultMaximumAcceptableCostMultiplier = 1
+
+local function getMaximumAcceptableCost(featureMatrix, labelMatrix)
+	
+	local absoluteFeatureMatrix = AqwamTensorLibrary:applyFunction(math.abs, featureMatrix)
+	
+	local sum = AqwamTensorLibrary:sum(absoluteFeatureMatrix)
+	
+	if (labelMatrix) then
+		
+		local absoluteLabelMatrix = AqwamTensorLibrary:applyFunction(math.abs, labelMatrix)
+		
+		sum = sum + AqwamTensorLibrary:sum(absoluteLabelMatrix)
+		
+	end
+	
+	return sum
+	
+end
+
+local function checkIfIsAcceptableValue(value, minimumValue, maximumValue)
+	
+	local isValidValue = (value == value) and (value ~= math.huge) and (value ~= -math.huge) and (type(value) == "number")
+	
+	if (not isValidValue) then return false end
+	
+	if (minimumValue) then
+		
+		if (value < minimumValue) then return false end
+		
+	end
+	
+	if (maximumValue) then
+
+		if (value > maximumValue) then return false end
+
+	end
+
+	return true
+
+end
+
+local function checkIfModelParametersAreAcceptable(ModelParameters, minimumValue, maximumValue)
+	
+	local isAcceptable = true
+	
+	if (type(ModelParameters) == "table") then
+		
+		for _, value in ipairs(ModelParameters) do
+			
+			isAcceptable = checkIfModelParametersAreAcceptable(value)
+			
+			if (not isAcceptable) then return false end
+			
+		end
+		
+	else
+		
+		isAcceptable = checkIfIsAcceptableValue(ModelParameters, minimumValue, maximumValue)
+		
+	end
+	
+	return isAcceptable
+	
+end
+
+local function removeDefectiveData(featureMatrix, labelMatrix, ClassesList) -- If even a single column contains a defective value, remove the whole row.
+	
+	local numberOfData = #featureMatrix
+
+	local rowToDeleteArray = {}
+	
+	local numberOfClasses
+	
+	if (labelMatrix) then
+		
+		numberOfClasses = #labelMatrix[1]
+		
+	end
+	
+	for i, featureVector in ipairs(featureMatrix) do
+		
+		local isAcceptableData = true
+
+		for f, featureValue in ipairs(featureVector) do
+
+			if (not checkIfIsAcceptableValue(featureValue)) then
+				
+				rowToDeleteArray[i] = true
+				
+				isAcceptableData = false
+
+				break
+				
+			end
+
+		end
+		
+		if (labelMatrix) and (isAcceptableData) then
+			
+			local labelVector = labelMatrix[i]
+			
+			if (ClassesList) then
+				
+				if (not table.find(ClassesList, labelVector[1])) then
+					
+					rowToDeleteArray[i] = true
+
+					isAcceptableData = false
+					
+					break
+					
+				end
+				
+			else
+				
+				for l = 1, numberOfClasses, 1 do
+
+					if (not checkIfIsAcceptableValue(labelVector[l])) then
+
+						rowToDeleteArray[i] = true
+
+						isAcceptableData = false
+
+						break
+
+					end
+
+				end
+				
+			end
+			
+		end
+		
+	end
+
+	local filteredFeatureMatrix = {}
+	
+	local filteredLabelMatrix
+	
+	if (labelMatrix) then filteredLabelMatrix = {} end
+
+	for i = 1, numberOfData, 1 do
+
+		if (not rowToDeleteArray[i]) then
+
+			table.insert(filteredFeatureMatrix, featureMatrix[i])
+
+			if (labelMatrix) then table.insert(filteredLabelMatrix, labelMatrix[i]) end
+
+		end
+
+	end
+	
+	return filteredFeatureMatrix, filteredLabelMatrix
+
+end
+
+function ModelParametersSafeguardWrapper.new(parameterDictionary)
+	
+	local NewModelParametersSafeguardWrapper = BaseInstance.new(parameterDictionary)
+	
+	setmetatable(NewModelParametersSafeguardWrapper, ModelParametersSafeguardWrapper)
+	
+	NewModelParametersSafeguardWrapper:setName("ModelParametersSafeguardWrapper")
+	
+	NewModelParametersSafeguardWrapper:setClassName("ModelParametersSafeguardWrapper")
+	
+	NewModelParametersSafeguardWrapper.Model = parameterDictionary.Model
+	
+	NewModelParametersSafeguardWrapper.ignoreUpdateOnDefect = NewModelParametersSafeguardWrapper:getValueOrDefaultValue(parameterDictionary.ignoreUpdateOnDefect, defaultIgnoreUpdateOnDefect)
+	
+	NewModelParametersSafeguardWrapper.removeDefectiveDataOnDefect = NewModelParametersSafeguardWrapper:getValueOrDefaultValue(parameterDictionary.removeDefectiveDataOnDefect, defaultRemoveDefectiveDataOnDefect)
+	
+	NewModelParametersSafeguardWrapper.storeDefectiveUpdateInformation = NewModelParametersSafeguardWrapper:getValueOrDefaultValue(parameterDictionary.storeDefectiveUpdateInformation, defaultStoreDefectiveUpdateInformation)
+	
+	NewModelParametersSafeguardWrapper.maximumAcceptableCostMultiplier = NewModelParametersSafeguardWrapper:getValueOrDefaultValue(parameterDictionary.maximumAcceptableCostMultiplier, defaultMaximumAcceptableCostMultiplier)
+	
+	NewModelParametersSafeguardWrapper.canUseModel = true
+	
+	NewModelParametersSafeguardWrapper.defectiveUpdateInformationDictionary = {}
+	
+	return NewModelParametersSafeguardWrapper
+	
+end
+
+function ModelParametersSafeguardWrapper:runSandboxedEnvironment(eventName, functionToRun, removeDefectFunction)
+	
+	self.canUseModel = false
+
+	local Model = self.Model
+
+	local ignoreUpdateOnDefect = self.ignoreUpdateOnDefect
+
+	local removeDefectiveDataOnDefect = self.removeDefectiveDataOnDefect
+
+	local storeDefectiveUpdateInformation = self.storeDefectiveUpdateInformation
+	
+	local defectiveUpdateInformationDictionary = self.defectiveUpdateInformationDictionary
+
+	local OriginalModelParameters = Model:getModelParameters()
+	
+	local isAcceptable = false
+	
+	local valueArray
+	
+	local isSuccessful = pcall(function()
+		
+		isAcceptable, valueArray = functionToRun(Model)
+		
+	end)
+	
+	if (isAcceptable) and (isSuccessful) then
+		
+		self.canUseModel = true
+		
+		return table.unpack(valueArray or {})
+		
+	end
+	
+	if (storeDefectiveUpdateInformation) then
+
+		local currentTimeString = tostring(os.time())
+
+		defectiveUpdateInformationDictionary[currentTimeString] = eventName
+
+	end
+	
+	Model:setModelParameters(OriginalModelParameters)
+
+	if (ignoreUpdateOnDefect) then 
+
+		self.canUseModel = true
+
+		return table.unpack(valueArray or {}) 
+
+	end
+
+	if (removeDefectiveDataOnDefect) and (removeDefectFunction) then
+
+		removeDefectFunction()
+
+	end
+	
+	isAcceptable, valueArray = functionToRun(Model) -- Once defective data is removed, run the function again to use the model with the updated data.
+	
+	return table.unpack(valueArray or {})
+	
+end
+
+function ModelParametersSafeguardWrapper:train(featureMatrix, labelMatrix)
+	
+	local costArray
+	
+	local finalCostValue
+	
+	local isAcceptableValue
+	
+	local maximumAcceptableCost
+	
+	local ClassesList = self.Model.ClassesList
+	
+	local maximumAcceptableCostMultiplier = self.maximumAcceptableCostMultiplier
+	
+	self:runSandboxedEnvironment("train", function(Model)
+		
+		costArray = Model:train(featureMatrix, labelMatrix)
+		
+		maximumAcceptableCost = maximumAcceptableCostMultiplier * getMaximumAcceptableCost(featureMatrix, labelMatrix)
+
+		finalCostValue = costArray[#costArray]
+		
+		isAcceptableValue = checkIfIsAcceptableValue(finalCostValue, -maximumAcceptableCost, maximumAcceptableCost)
+		
+		return isAcceptableValue, {costArray}
+		
+	end, function()
+		
+		featureMatrix, labelMatrix = removeDefectiveData(featureMatrix, labelMatrix, ClassesList)
+		
+	end)
+	
+	return costArray or {}
+	
+end
+
+function ModelParametersSafeguardWrapper:update(...)
+	
+	local valueArray = {...}
+	
+	local UpdatedModelParameters
+
+	self:runSandboxedEnvironment("update", function(Model)
+
+		Model:update(table.unpack(valueArray))
+		
+		UpdatedModelParameters = Model:getModelParameters()
+
+		return checkIfModelParametersAreAcceptable(UpdatedModelParameters)
+
+	end)
+	
+end
+
+function ModelParametersSafeguardWrapper:predict(...)
+	
+	return self.Model:predict(...)
+	
+end
+
+function ModelParametersSafeguardWrapper:setModel(Model)
+	
+	self.Model = Model
+	
+end
+
+function ModelParametersSafeguardWrapper:getModel()
+
+	return self.Model
+
+end
+
+function ModelParametersSafeguardWrapper:getModelParameters(...)
+
+	return self.Model:getModelParameters(...)
+
+end
+
+function ModelParametersSafeguardWrapper:setModelParameters(...)
+
+	self.Model:setModelParameters(...)
+
+end
+
+function ModelParametersSafeguardWrapper:getCanUseModel()
+	
+	return self.canUseModel
+	
+end
+
+return ModelParametersSafeguardWrapper

de138efaf0edf6211300f2b87101862359cd0838
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-09-24 18:20:37 +0800

    Update and rename Regularizer_BaseRegularize.rlua to Regularizer_BaseRegularizer.lua

--- src/LuaVersion/Regularizer_BaseRegularizer.lua
+++ src/LuaVersion/Regularizer_BaseRegularizer.lua
@@ -54,13 +54,44 @@ function BaseRegularizer.new(parameterDictionary)
 	
 end
 
-function BaseRegularizer:calculate(weightTensor)
+function BaseRegularizer:makeLambdaAtBiasZero(ModelParameters)
+	
+	local NewModelParameters = self:deepCopyTable(ModelParameters)
+	
+	local firstRowModelParameters = NewModelParameters[1]
+	
+	for i, _ in ipairs(firstRowModelParameters) do
+		
+		firstRowModelParameters[i] = 0
+		
+	end
+
+	return NewModelParameters
+
+end
+
+function BaseRegularizer:calculateCost(ModelParameters)
+	
+	local CalculateCostFunction = self.CalculateCostFunction
+	
+	if (CalculateCostFunction) then return CalculateCostFunction(ModelParameters) end
+	
+end
+
+function BaseRegularizer:setCalculateCostFunction(CalculateCostFunction)
+
+	self.CalculateCostFunction = CalculateCostFunction
+
+end
+
+function BaseRegularizer:calculate(ModelParameters)
 
 	local CalculateFunction = self.CalculateFunction
 
-	if (CalculateFunction) then return CalculateFunction(weightTensor) end
+	if (CalculateFunction) then return CalculateFunction(ModelParameters) end
 
 end
+
 function BaseRegularizer:setCalculateFunction(CalculateFunction)
 	
 	self.CalculateFunction = CalculateFunction

02a896bdd87040b7fcc6cae5f4f2b68f2a93848c
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-09-28 11:33:21 +0800

    Update NaiveBayesBaseModel.lua

--- src/RobloxVersion/Models/NaiveBayesBaseModel.lua
+++ src/RobloxVersion/Models/NaiveBayesBaseModel.lua
@@ -259,22 +259,15 @@ function NaiveBayesBaseModel:predict(featureMatrix, returnOriginalOutput)
 
 end
 
-
 function NaiveBayesBaseModel:setPredictFunction(predictFunction)
 
 	self.predictFunction = predictFunction
 
 end
 
-function NaiveBayesBaseModel:generate(labelVector, noiseMatrix)
+function NaiveBayesBaseModel:generate(labelVector, ...)
 	
-	if (noiseMatrix) then
-		
-		if (#labelVector ~= #noiseMatrix) then error("The label vector and the label matrix does not contain the same number of rows.") end
-		
-	end
-	
-	return self.generateFunction(labelVector, noiseMatrix)
+	return self.generateFunction(labelVector, ...)
 	
 end
 

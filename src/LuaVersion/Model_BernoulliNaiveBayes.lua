5a7511cddc31666dc444a2641e3737fad7a1772d
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-09-28 19:43:33 +0800

    Update Model_BernoulliNaiveBayes.lua

--- src/LuaVersion/Model_BernoulliNaiveBayes.lua
+++ src/LuaVersion/Model_BernoulliNaiveBayes.lua
@@ -28,7 +28,7 @@
 
 local AqwamTensorLibrary = require("AqwamTensorLibrary")
 
-local NaiveBayesBaseModel = require("Model_NaiveBayesBaseModel")
+local NaiveBayesBaseModel = require("Model_NaiveBayesBaseModel')
 
 BernoulliNaiveBayesModel = {}
 
@@ -385,8 +385,6 @@ function BernoulliNaiveBayesModel.new(parameterDictionary)
 		local numberOfFeatures = #featureProbabilityMatrix[1]
 		
 		local selectedFeatureProbabiltyMatrix = {}
-
-		local generatedFeatureMatrix = {}
 		
 		if (useLogProbabilities) then
 

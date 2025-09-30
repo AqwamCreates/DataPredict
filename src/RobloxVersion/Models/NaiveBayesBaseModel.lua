df89d4ca89198ae745c3ba84491100db141232d7
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-09-30 12:01:19 +0800

    Update NaiveBayesBaseModel.lua

--- src/RobloxVersion/Models/NaiveBayesBaseModel.lua
+++ src/RobloxVersion/Models/NaiveBayesBaseModel.lua
@@ -149,16 +149,24 @@ function NaiveBayesBaseModel:separateFeatureMatrixByClass(featureMatrix, labelVe
 	local extractedFeatureMatrixTable = {}
 
 	local extractedFeatureMatrix
+	
+	local numberOfFeatures
 
 	for classIndex, class in ipairs(ClassesList) do
 
 		extractedFeatureMatrix = extractFeatureMatrixFromPosition(featureMatrix, classesPositionTable[classIndex])
 
 		table.insert(extractedFeatureMatrixTable, extractedFeatureMatrix)
+		
+		if (not numberOfFeatures) then
+			
+			if (#extractedFeatureMatrix >= 1) then numberOfFeatures = #extractedFeatureMatrix[1] end
+			
+		end
 
 	end
 
-	return extractedFeatureMatrixTable
+	return extractedFeatureMatrixTable, numberOfFeatures
 
 end
 

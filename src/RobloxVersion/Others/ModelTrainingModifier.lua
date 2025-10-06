a1fe4c2bef600e9d9a26743b7612306cea735b7a
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-06 14:24:50 +0800

    Update ModelTrainingModifier.lua

--- src/RobloxVersion/Others/ModelTrainingModifier.lua
+++ src/RobloxVersion/Others/ModelTrainingModifier.lua
@@ -26,460 +26,248 @@
 
 --]]
 
-local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+local BaseIntstance = require(script.Parent.Parent.Cores.BaseInstance)
 
-local BaseInstance = require(script.Parent.Parent.Cores.BaseInstance)
+local ModelTrainingModifier = {}
 
-local ModelTrainingModifier = require(script.Parent.ModelTrainingModifier)
+ModelTrainingModifier.__index = ModelTrainingModifier
 
-local ModelSafeguardWrapper = {}
+setmetatable(ModelTrainingModifier, BaseIntstance)
 
-ModelSafeguardWrapper.__index = ModelSafeguardWrapper
+local defaultMode = "Stochastic"
 
-setmetatable(ModelSafeguardWrapper, BaseInstance)
+local defaultBatchSize = 2
 
-local defaultIgnoreUpdateOnDefect = false
+local defaultIsOutputPrinted = true
 
-local defaultRemoveDefectiveDataOnDefect = true
-
-local defaultReplaceValuesOnDefect = true
-
-local defaultModifyModelOnDefect = true
-
-local defaultStoreDefectiveUpdateInformation = false
-
-local defaultMaximumAcceptableCostMultiplier = 1
-
-local function checkIfAllAreNumbers(matrix)
+function ModelTrainingModifier.new(parameterDictionary)
 	
-	local numberOfRows = #matrix
+	local NewModelTrainingModifier = BaseIntstance.new(parameterDictionary)
 	
-	local numberOfColumns = #matrix[1]
+	setmetatable(NewModelTrainingModifier, ModelTrainingModifier)
 	
-	for row = 1, numberOfRows, 1 do
+	NewModelTrainingModifier:setName("ModelTrainingModifier")
+	
+	NewModelTrainingModifier:setClassName("ModelTrainingModifier")
+	
+	NewModelTrainingModifier.mode = parameterDictionary.mode or defaultMode
+
+	NewModelTrainingModifier.batchSize = parameterDictionary.batchSize or defaultBatchSize
+	
+	NewModelTrainingModifier.isOutputPrinted = NewModelTrainingModifier:getValueOrDefaultValue(parameterDictionary.isOutputPrinted, defaultIsOutputPrinted)
+	
+	NewModelTrainingModifier.Model = parameterDictionary.Model
+	
+	return NewModelTrainingModifier
+	
+end
+
+local function breakMatrixToMultipleSmallerMatrices(matrix, batchSize)
+	
+	local numberOfBatches = math.ceil(#matrix/batchSize)
+	
+	local matrixBatchesTable = {}
+	
+	local batchPositions = {}
+	
+	local batchFeatureMatrix
+	
+	local batchLabelVector 
+	
+	for batch = 1, numberOfBatches, 1 do
 		
-		for column = 1, numberOfColumns, 1 do
+		local startIndex = (batch - 1) * batchSize + 1
+		
+		local endIndex = math.min(batch * batchSize, #matrix)
+		
+		local batchFeatureMatrix = {}
+		
+		for i = startIndex, endIndex do table.insert(batchFeatureMatrix, matrix[i]) end
+		
+		table.insert(matrixBatchesTable, batchFeatureMatrix)
+		
+	end
+	
+	return matrixBatchesTable
+	
+end
+
+function ModelTrainingModifier:batchTrain(...)
+	
+	return self.Model:train(...)
+	
+end
+
+function ModelTrainingModifier:miniBatchTrain(...)
+	
+	local batchSize = self.batchSize
+	
+	if (batchSize < 0) then error("Batch size cannot be negative!") end
+	
+	local matrixArray = {...}
+	
+	local numberOfMatrices = #matrixArray
+
+	local numberOfData = #matrixArray[1]
+
+	for matrixIndex = 1, numberOfMatrices, 1 do
+
+		if (numberOfData ~= #matrixArray[matrixIndex]) then error("All matrices or vectors must contain same number of data") end
+
+	end
+	
+	if (batchSize > numberOfData) then error("Batch size is greater than the number of data!") end
+	
+	local numberOfBatches = math.ceil(numberOfData / batchSize)
+	
+	local miniBatchMatrixArray = {}
+	
+	for matrixIndex = 1, numberOfMatrices, 1 do
+		
+		local matrices = breakMatrixToMultipleSmallerMatrices(matrixArray[matrixIndex], batchSize)
+		
+		table.insert(miniBatchMatrixArray, matrices)
+		
+	end
+	
+	local costArray = {}
+	
+	local Model = self.Model
+	
+	local isOutputPrinted = self.isOutputPrinted
+	
+	for currentBatchNumber = 1, numberOfBatches, 1 do
+		
+		local currentMatrixBatchArray = {}
+		
+		for matrixIndex = 1, numberOfMatrices, 1 do
 			
-			if (type(matrix[row][column]) ~= "number") then return false end
+			table.insert(currentMatrixBatchArray, miniBatchMatrixArray[matrixIndex][currentBatchNumber])
+			
+		end
+
+		local miniBatchCostArray = Model:train(table.unpack(currentMatrixBatchArray))
+		
+		local cost = miniBatchCostArray[#miniBatchCostArray]
+		
+		table.insert(costArray, cost)
+		
+		if (isOutputPrinted) then print("Batch: " .. currentBatchNumber .. "\t\t\tFinal cost: " .. cost) end
+		
+	end
+	
+	return costArray
+
+end
+
+function ModelTrainingModifier:stochasticTrain(...)
+	
+	local matrixArray = {...}
+
+	local numberOfMatrices = #matrixArray
+
+	local numberOfData = #matrixArray[1]
+
+	for matrixIndex = 1, numberOfMatrices, 1 do
+
+		if (numberOfData ~= #matrixArray[matrixIndex]) then error("All matrices or vectors must contain same number of data") end
+
+	end
+	
+	local costArray = {}
+	
+	local Model = self.Model
+	
+	local isOutputPrinted = self.isOutputPrinted
+	
+	local originalMaximumNumberOfIterations = Model.maximumNumberOfIterations
+	
+	Model.maximumNumberOfIterations = 1
+	
+	for dataIndex = 1, numberOfData, 1 do
+		
+		local currentMatrixBatchArray = {}
+
+		for matrixIndex = 1, numberOfMatrices, 1 do
+
+			table.insert(currentMatrixBatchArray, {matrixArray[matrixIndex][dataIndex]})
 			
 		end
 		
-	end
-	
-	return true
-	
-end
-
-local function getMaximumAcceptableCost(featureMatrix, labelMatrix, ClassesList)
-	
-	local absoluteFeatureMatrix = AqwamTensorLibrary:applyFunction(math.abs, featureMatrix)
-	
-	local sum = AqwamTensorLibrary:sum(absoluteFeatureMatrix)
-	
-	if (labelMatrix) and (ClassesList) then
+		local stochasticCostArray = Model:train(table.unpack(currentMatrixBatchArray))
 		
-		sum = sum + #labelMatrix
+		local cost = stochasticCostArray[#stochasticCostArray]
 		
-	elseif (labelMatrix) and (not ClassesList) then
+		table.insert(costArray, cost)
 		
-		local absoluteLabelMatrix = AqwamTensorLibrary:applyFunction(math.abs, labelMatrix)
-
-		sum = sum + AqwamTensorLibrary:sum(absoluteLabelMatrix)
+		if (isOutputPrinted) then print("Data number: " .. dataIndex .. "\t\tFinal cost: " .. cost) end
 		
 	end
 	
-	return sum
+	Model.maximumNumberOfIterations = originalMaximumNumberOfIterations
 	
-end
-
-local function checkIfIsAcceptableValue(value, minimumValue, maximumValue)
-	
-	local isValidValue = (value == value) and (value ~= math.huge) and (value ~= -math.huge) and (type(value) == "number")
-	
-	if (not isValidValue) then return false end
-	
-	if (minimumValue) then
-		
-		if (value < minimumValue) then return false end
-		
-	end
-	
-	if (maximumValue) then
-
-		if (value > maximumValue) then return false end
-
-	end
-
-	return true
+	return costArray
 
 end
 
-local function checkIfModelParametersAreAcceptable(ModelParameters, minimumValue, maximumValue)
+function ModelTrainingModifier:train(...)
 	
-	local isAcceptable = true
+	local trainingMode = self.mode
 	
-	if (type(ModelParameters) == "table") then
+	if (trainingMode == "Batch") then
 		
-		for _, value in ipairs(ModelParameters) do
-			
-			isAcceptable = checkIfModelParametersAreAcceptable(value)
-			
-			if (not isAcceptable) then return false end
-			
-		end
+		return self:batchTrain(...)
+		
+	elseif (trainingMode == "MiniBatch") then
+		
+		return self:miniBatchTrain(...)
+		
+	elseif (trainingMode == "Stochastic") then
+		
+		return self:stochasticTrain(...)
 		
 	else
 		
-		isAcceptable = checkIfIsAcceptableValue(ModelParameters, minimumValue, maximumValue)
+		error("The selected gradient descent method cannot be found.")
 		
 	end
-	
-	return isAcceptable
 	
 end
 
-local function removeDefectiveData(featureMatrix, labelMatrix, ClassesList) -- If even a single column contains a defective value, remove the whole row.
-	
-	local numberOfData = #featureMatrix
+function ModelTrainingModifier:update(...)
 
-	local rowToDeleteArray = {}
-	
-	local numberOfClasses
-	
-	if (labelMatrix) then
-		
-		numberOfClasses = #labelMatrix[1]
-		
-	end
-	
-	for i, featureVector in ipairs(featureMatrix) do
-		
-		local isAcceptableData = true
-
-		for f, featureValue in ipairs(featureVector) do
-
-			if (not checkIfIsAcceptableValue(featureValue)) then
-				
-				rowToDeleteArray[i] = true
-				
-				isAcceptableData = false
-
-				break
-				
-			end
-
-		end
-		
-		if (labelMatrix) and (isAcceptableData) then
-			
-			local labelVector = labelMatrix[i]
-			
-			if (ClassesList) then
-				
-				if (not table.find(ClassesList, labelVector[1])) then
-					
-					rowToDeleteArray[i] = true
-
-					isAcceptableData = false
-					
-					break
-					
-				end
-				
-			else
-				
-				for l = 1, numberOfClasses, 1 do
-
-					if (not checkIfIsAcceptableValue(labelVector[l])) then
-
-						rowToDeleteArray[i] = true
-
-						isAcceptableData = false
-
-						break
-
-					end
-
-				end
-				
-			end
-			
-		end
-		
-	end
-
-	local filteredFeatureMatrix = {}
-	
-	local filteredLabelMatrix
-	
-	if (labelMatrix) then filteredLabelMatrix = {} end
-
-	for i = 1, numberOfData, 1 do
-
-		if (not rowToDeleteArray[i]) then
-
-			table.insert(filteredFeatureMatrix, featureMatrix[i])
-
-			if (labelMatrix) then table.insert(filteredLabelMatrix, labelMatrix[i]) end
-
-		end
-
-	end
-	
-	return filteredFeatureMatrix, filteredLabelMatrix
+	return self.Model:update(...)
 
 end
 
-function ModelSafeguardWrapper.new(parameterDictionary)
-	
-	local NewModelSafeguardWrapper = BaseInstance.new(parameterDictionary)
-	
-	setmetatable(NewModelSafeguardWrapper, ModelSafeguardWrapper)
-	
-	NewModelSafeguardWrapper:setName("ModelSafeguardWrapper")
-	
-	NewModelSafeguardWrapper:setClassName("ModelSafeguardWrapper")
-	
-	local Model = parameterDictionary.Model
-	
-	NewModelSafeguardWrapper.Model = Model
-	
-	NewModelSafeguardWrapper.ignoreUpdateOnDefect = NewModelSafeguardWrapper:getValueOrDefaultValue(parameterDictionary.ignoreUpdateOnDefect, defaultIgnoreUpdateOnDefect)
-	
-	NewModelSafeguardWrapper.removeDefectiveDataOnDefect = NewModelSafeguardWrapper:getValueOrDefaultValue(parameterDictionary.removeDefectiveDataOnDefect, defaultRemoveDefectiveDataOnDefect)
-	
-	NewModelSafeguardWrapper.replaceValuesOnDefect = NewModelSafeguardWrapper:getValueOrDefaultValue(parameterDictionary.replaceValuesOnDefect, defaultReplaceValuesOnDefect)
-	
-	NewModelSafeguardWrapper.modifyModelOnDefect = NewModelSafeguardWrapper:getValueOrDefaultValue(parameterDictionary.modifyModelOnDefect, defaultModifyModelOnDefect)
-	
-	NewModelSafeguardWrapper.storeDefectiveUpdateInformation = NewModelSafeguardWrapper:getValueOrDefaultValue(parameterDictionary.storeDefectiveUpdateInformation, defaultStoreDefectiveUpdateInformation)
-	
-	NewModelSafeguardWrapper.maximumAcceptableCostMultiplier = NewModelSafeguardWrapper:getValueOrDefaultValue(parameterDictionary.maximumAcceptableCostMultiplier, defaultMaximumAcceptableCostMultiplier)
-	
-	NewModelSafeguardWrapper.ModifiedModel = parameterDictionary.ModifiedModel or ModelTrainingModifier.new({Model = Model, trainingMode = "Stochastic"})
-	
-	NewModelSafeguardWrapper.canUseModel = true
-	
-	NewModelSafeguardWrapper.defectiveUpdateInformationDictionary = {}
-	
-	return NewModelSafeguardWrapper
-	
-end
-
-function ModelSafeguardWrapper:runSandboxedEnvironment(eventName, Model, functionToRun, onDefectFunctionToRunDictionary)
-	
-	self.canUseModel = false
-
-	local ignoreUpdateOnDefect = self.ignoreUpdateOnDefect
-
-	local storeDefectiveUpdateInformation = self.storeDefectiveUpdateInformation
-	
-	local defectiveUpdateInformationDictionary = self.defectiveUpdateInformationDictionary
-
-	local OriginalModelParameters = Model:getModelParameters()
-	
-	local isAcceptable = false
-	
-	local valueArray
-	
-	local isSuccessful = pcall(function()
-		
-		isAcceptable, valueArray = functionToRun()
-		
-	end)
-	
-	if (isAcceptable) and (isSuccessful) then
-		
-		self.canUseModel = true
-		
-		return table.unpack(valueArray or {})
-		
-	end
-	
-	if (storeDefectiveUpdateInformation) then
-
-		local currentTimeString = tostring(os.time())
-
-		defectiveUpdateInformationDictionary[currentTimeString] = eventName
-
-	end
-	
-	Model:setModelParameters(OriginalModelParameters)
-
-	if (ignoreUpdateOnDefect) or ((not ignoreUpdateOnDefect) and (not onDefectFunctionToRunDictionary)) then 
-
-		self.canUseModel = true
-
-		return table.unpack(valueArray or {}) 
-
-	end
-	
-	local onDefectSettingArray = {self.removeDefectiveDataOnDefect,  self.replaceValuesOnDefect, self.modifyModelOnDefect}
-
-	local onDefectFunctionNameArray = {"removeDefectFunction", "replaceValueFunction", "modifyModelFunction"}
-	
-	local onDefectFunctionName
-
-	local onDefectFunctionToRun
-	
-	for i, value in ipairs(onDefectSettingArray) do
-		
-		if (value) then
-			
-			onDefectFunctionName = onDefectFunctionNameArray[i]
-			
-			onDefectFunctionToRun = onDefectFunctionToRunDictionary[onDefectFunctionName]
-			
-			if (onDefectFunctionToRun) then 
-				
-				onDefectFunctionToRun() 
-				
-				Model:setModelParameters(OriginalModelParameters)
-				
-				isSuccessful = pcall(function()
-
-					isAcceptable, valueArray = functionToRun()
-
-				end)
-
-				if (isAcceptable) and (isSuccessful) then
-
-					self.canUseModel = true
-
-					return table.unpack(valueArray or {})
-
-				end
-				
-			end
-			
-		end
-		
-	end
-	
-	Model:setModelParameters(OriginalModelParameters)
-	
-	self.canUseModel = true
-	
-	return table.unpack(valueArray or {})
-	
-end
-
-function ModelSafeguardWrapper:train(featureMatrix, labelMatrix)
-	
-	local Model = self.Model
-
-	local ClassesList = Model.ClassesList
-	
-	local maximumAcceptableCostMultiplier = self.maximumAcceptableCostMultiplier
-	
-	local costArray
-	
-	local finalCostValue
-	
-	local isAcceptableValue
-	
-	local maximumAcceptableCost
-	
-	local onDefectFunctionToRunDictionary = {
-		
-		["removeDefectFunction"] = function()
-			
-			featureMatrix, labelMatrix = removeDefectiveData(featureMatrix, labelMatrix, ClassesList)
-			
-		end,
-		
-		["replaceValueFunction"] = function()
-			
-			if (not checkIfAllAreNumbers(featureMatrix)) then return end
-
-			featureMatrix = AqwamTensorLibrary:zScoreNormalization(featureMatrix, 2)
-			
-		end,
-		
-		["modifyModelFunction"] = function()
-			
-			local ModifiedModel = self.ModifiedModel
-			
-			if (not ModifiedModel) then return end
-			
-			Model = ModifiedModel
-			
-		end,
-		
-	}
-	
-	self:runSandboxedEnvironment("train", Model, function()
-		
-		costArray = Model:train(featureMatrix, labelMatrix)
-		
-		maximumAcceptableCost = maximumAcceptableCostMultiplier * getMaximumAcceptableCost(featureMatrix, labelMatrix)
-
-		finalCostValue = costArray[#costArray]
-		
-		isAcceptableValue = checkIfIsAcceptableValue(finalCostValue, -maximumAcceptableCost, maximumAcceptableCost)
-		
-		return isAcceptableValue, {costArray}
-		
-	end, onDefectFunctionToRunDictionary)
-	
-	return costArray or {}
-	
-end
-
-function ModelSafeguardWrapper:update(...)
-	
-	local Model = self.Model
-	
-	local valueArray = {...}
-	
-	local UpdatedModelParameters
-
-	self:runSandboxedEnvironment("update", Model, function()
-
-		Model:update(table.unpack(valueArray))
-		
-		UpdatedModelParameters = Model:getModelParameters()
-
-		return checkIfModelParametersAreAcceptable(UpdatedModelParameters)
-
-	end)
-	
-end
-
-function ModelSafeguardWrapper:predict(...)
+function ModelTrainingModifier:predict(...)
 	
 	return self.Model:predict(...)
 	
 end
 
-function ModelSafeguardWrapper:setModel(Model)
+function ModelTrainingModifier:setModel(Model)
 	
 	self.Model = Model
 	
 end
 
-function ModelSafeguardWrapper:getModel()
+function ModelTrainingModifier:getModel()
 
 	return self.Model
 
 end
 
-function ModelSafeguardWrapper:getModelParameters(...)
+function ModelTrainingModifier:getModelParameters(...)
 
 	return self.Model:getModelParameters(...)
 
 end
 
-function ModelSafeguardWrapper:setModelParameters(...)
+function ModelTrainingModifier:setModelParameters(...)
 
 	self.Model:setModelParameters(...)
 
 end
 
-function ModelSafeguardWrapper:getCanUseModel()
-	
-	return self.canUseModel
-	
-end
-
-return ModelSafeguardWrapper
+return ModelTrainingModifier

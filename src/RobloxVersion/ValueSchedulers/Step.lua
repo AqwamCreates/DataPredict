3b7782bcc3e2ca6b904e8f41aeb05521048d3d6a
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-11 05:15:31 +0800

    Update and rename StepDecay.lua to Step.lua

---
+++ src/RobloxVersion/ValueSchedulers/Step.lua
@@ -0,0 +1,79 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local BaseValueScheduler = require(script.Parent.BaseValueScheduler)
+
+StepValueScheduler = {}
+
+StepValueScheduler.__index = StepValueScheduler
+
+setmetatable(StepValueScheduler, BaseValueScheduler)
+
+local defaultTimeStepToDecay = 100
+
+local defaultDecayRate = 0.5
+
+function StepValueScheduler.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local NewStepValueScheduler = BaseValueScheduler.new(parameterDictionary)
+	
+	setmetatable(NewStepValueScheduler, StepValueScheduler)
+	
+	NewStepValueScheduler:setName("Step")
+	
+	NewStepValueScheduler.timeValueToDecay = parameterDictionary.timeValueToDecay or defaultTimeStepToDecay
+	
+	NewStepValueScheduler.decayRate = parameterDictionary.decayRate or defaultDecayRate
+	
+	--------------------------------------------------------------------------------
+	
+	NewStepValueScheduler:setCalculateFunction(function(value, timeValue)
+
+		return (value * math.pow(NewStepValueScheduler.decayRate, (math.floor(timeValue / NewStepValueScheduler.timeValueToDecay))))
+		
+	end)
+	
+	return NewStepValueScheduler
+	
+end
+
+function StepValueScheduler:setDecayRate(decayRate)
+	
+	self.decayRate = decayRate
+	
+end
+
+function StepValueScheduler:setTimeValueToDecay(timeValueToDecay)
+	
+	self.timeValueToDecay = timeValueToDecay
+	
+end
+
+return StepValueScheduler

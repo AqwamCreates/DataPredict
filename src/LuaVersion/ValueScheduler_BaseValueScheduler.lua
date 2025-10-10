0eede4d6c944c1b7b57c129122ced361a2d7965c
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-10 12:47:12 +0800

    Update ValueScheduler_BaseValueScheduler.lua

--- src/LuaVersion/ValueScheduler_BaseValueScheduler.lua
+++ src/LuaVersion/ValueScheduler_BaseValueScheduler.lua
@@ -26,6 +26,8 @@
 
 --]]
 
+local AqwamTensorLibrary = require("AqwamTensorLibrary")
+
 local BaseInstance = require("Core_BaseInstance")
 
 BaseValueScheduler = {}
@@ -54,9 +56,13 @@ function BaseValueScheduler.new(parameterDictionary)
 	
 end
 
-function BaseValueScheduler:calculate(epsilon)
+function BaseValueScheduler:calculate(valueToSchedule, valueToScale)
 	
-	if (self.calculateFunction) then return self.calculateFunction(epsilon) end
+	valueToSchedule = self.calculateFunction(valueToSchedule) 
+	
+	if (not valueToScale) then return valueToSchedule end
+	
+	return AqwamTensorLibrary:multiply(valueToSchedule, valueToScale)
 	
 end
 

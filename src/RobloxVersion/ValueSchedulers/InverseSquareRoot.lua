12588957ca5bd304373d811320326f150ea8f344
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-11 08:28:18 +0800

    Create InverseSquareRoot.lua

---
+++ src/RobloxVersion/ValueSchedulers/InverseSquareRoot.lua
@@ -0,0 +1,59 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local BaseValueScheduler = require(script.Parent.BaseValueScheduler)
+
+InverseSquareRootValueScheduler = {}
+
+InverseSquareRootValueScheduler.__index = InverseSquareRootValueScheduler
+
+setmetatable(InverseSquareRootValueScheduler, BaseValueScheduler)
+
+function InverseSquareRootValueScheduler.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local NewInverseSquareRootValueScheduler = BaseValueScheduler.new(parameterDictionary)
+	
+	setmetatable(NewInverseSquareRootValueScheduler, InverseSquareRootValueScheduler)
+	
+	NewInverseSquareRootValueScheduler:setName("InverseSquareRoot")
+	
+	--------------------------------------------------------------------------------
+	
+	NewInverseSquareRootValueScheduler:setCalculateFunction(function(value, timeValue)
+
+		return (value / math.pow(timeValue, 0.5))
+		
+	end)
+	
+	return NewInverseSquareRootValueScheduler
+	
+end
+
+return InverseSquareRootValueScheduler

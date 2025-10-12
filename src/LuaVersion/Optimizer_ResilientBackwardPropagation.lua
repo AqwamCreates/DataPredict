bd44d6507dd32a75b91224789e330095686973f9
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-12 12:46:05 +0800

    Create Optimizer_ResilientBackwardPropagation.lua

---
+++ src/LuaVersion/Optimizer_ResilientBackwardPropagation.lua
@@ -0,0 +1,143 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require("AqwamTensorLibrary")
+
+local BaseOptimizer = require("Optimizer_BaseOptimizer")
+
+ResilientBackwardPropagationOptimizer = {}
+
+ResilientBackwardPropagationOptimizer.__index = ResilientBackwardPropagationOptimizer
+
+setmetatable(ResilientBackwardPropagationOptimizer, BaseOptimizer)
+
+local defaultEtaPlus = 0.5
+
+local defaultEtaMinus = 1.2
+
+local defaultMinimumStepSize = 1e-6
+
+local defaultMaximumStepSize = 50
+
+function ResilientBackwardPropagationOptimizer.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local NewResilientBackwardPropagationOptimizer = BaseOptimizer.new(parameterDictionary)
+	
+	setmetatable(NewResilientBackwardPropagationOptimizer, ResilientBackwardPropagationOptimizer)
+	
+	NewResilientBackwardPropagationOptimizer:setName("ResilientBackwardPropagation")
+	
+	NewResilientBackwardPropagationOptimizer.etaPlus = parameterDictionary.etaPlus or defaultEtaPlus
+	
+	NewResilientBackwardPropagationOptimizer.etaMinus = parameterDictionary.etaMinus or defaultEtaMinus
+	
+	NewResilientBackwardPropagationOptimizer.maximumStepSize = parameterDictionary.maximumStepSize or defaultMaximumStepSize
+	
+	NewResilientBackwardPropagationOptimizer.minimumStepSize = parameterDictionary.minimumStepSize or defaultMinimumStepSize
+	
+	--------------------------------------------------------------------------------
+	
+	NewResilientBackwardPropagationOptimizer:setCalculateFunction(function(learningRate, costFunctionDerivativeMatrix, weightMatrix)
+		
+		local previousGradientMatrix = NewResilientBackwardPropagationOptimizer.optimizerInternalParameterArray[1] or AqwamTensorLibrary:createTensor(AqwamTensorLibrary:getDimensionSizeArray(costFunctionDerivativeMatrix), 0)
+		
+		local learningRateMatrix = NewResilientBackwardPropagationOptimizer.optimizerInternalParameterArray[2] or AqwamTensorLibrary:createTensor(AqwamTensorLibrary:getDimensionSizeArray(costFunctionDerivativeMatrix), learningRate)
+		
+		local etaPlus = NewResilientBackwardPropagationOptimizer.etaPlus
+		
+		local etaMinus = NewResilientBackwardPropagationOptimizer.etaMinus
+		
+		local maximumStepSize = NewResilientBackwardPropagationOptimizer.maximumStepSize
+		
+		local minimumStepSize = NewResilientBackwardPropagationOptimizer.minimumStepSize
+		
+		local gradientMatrix = costFunctionDerivativeMatrix
+		
+		local multipliedGradientMatrix = AqwamTensorLibrary:multiply(gradientMatrix, previousGradientMatrix)
+		
+		for i, unwrappedMultipliedGradientVector in ipairs(multipliedGradientMatrix) do
+			
+			for j, unwrappedGradientValue in ipairs(unwrappedMultipliedGradientVector) do
+				
+				if (unwrappedGradientValue > 0) then
+					
+					learningRateMatrix[i][j] = math.min(learningRateMatrix[i][j] * etaPlus, maximumStepSize)
+					
+				elseif (unwrappedGradientValue < 0) then
+					
+					learningRateMatrix[i][j] = math.max(learningRateMatrix[i][j] * etaMinus, minimumStepSize)
+					
+					gradientMatrix[i][j] = 0
+					
+				end
+				
+			end
+			
+		end
+		
+		local signMatrix = AqwamTensorLibrary:applyFunction(math.sign, gradientMatrix)
+		
+		costFunctionDerivativeMatrix = AqwamTensorLibrary:multiply(learningRateMatrix, signMatrix)
+
+		NewResilientBackwardPropagationOptimizer.optimizerInternalParameterArray = {gradientMatrix, learningRateMatrix}
+
+		return costFunctionDerivativeMatrix
+		
+	end)
+	
+	return NewResilientBackwardPropagationOptimizer
+	
+end
+
+function ResilientBackwardPropagationOptimizer:setEtaPlus(etaPlus)
+	
+	self.etaPlus = etaPlus
+	
+end
+
+function ResilientBackwardPropagationOptimizer:setEtaMinus(etaMinus)
+
+	self.etaMinus = etaMinus
+
+end
+
+function ResilientBackwardPropagationOptimizer:setMaximumStepSize(maximumStepSize)
+
+	self.maximumStepSize = maximumStepSize
+
+end
+
+function ResilientBackwardPropagationOptimizer:setMinimumStepSize(minimumStepSize)
+
+	self.minimumStepSize = minimumStepSize
+
+end
+
+return ResilientBackwardPropagationOptimizer

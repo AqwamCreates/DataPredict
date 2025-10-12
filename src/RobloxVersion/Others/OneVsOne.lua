b6301d0ab85029bac93b54659de40f5773a13ab2
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-12 08:46:14 +0800

    Update OneVsOne.lua

--- src/RobloxVersion/Others/OneVsOne.lua
+++ src/RobloxVersion/Others/OneVsOne.lua
@@ -34,6 +34,8 @@ local Optimizers = DataPredictLibrary.Optimizers
 
 local Regularizers = DataPredictLibrary.Regularizers
 
+local ValueSchedulers = DataPredictLibrary.ValueSchedulers
+
 local AqwamTensorLibrary = require(DataPredictLibrary.AqwamTensorLibraryLinker.Value)
 
 local IterativeMethodBaseModel = require(Models.IterativeMethodBaseModel)
@@ -155,11 +157,19 @@ function OneVsOne:setOptimizer(parameterDictionary)
 	
 	if (not optimizerName) then error("No optimizer name.") end
 	
-	local SelectedOptimizer = require(Optimizers[optimizerName])
+	local SelectedOptimizer = require(Optimizers[optimizerName] or ValueSchedulers[optimizerName])
+	
+	local valueSchedulerName = parameterDictionary.valueSchedulerName
+	
+	local SelectedValueScheduler
+	
+	if (valueSchedulerName) then SelectedValueScheduler = require(ValueSchedulers[valueSchedulerName]) end
 		
 	for classArray, Model in pairs(ModelArray) do 
 
-		local success = pcall(function() 
+		local success = pcall(function()
+			
+			if (SelectedValueScheduler) then parameterDictionary.LearningRateValueScheduler = SelectedValueScheduler.new(parameterDictionary) end
 				
 			local OptimizerObject = SelectedOptimizer.new(parameterDictionary)
 

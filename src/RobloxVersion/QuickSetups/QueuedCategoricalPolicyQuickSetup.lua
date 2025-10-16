e1f59fc8c973c699865679f5b5ecf3e79c722118
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-17 04:39:10 +0800

    Update QueuedCategoricalPolicyQuickSetup.lua

--- src/RobloxVersion/QuickSetups/QueuedCategoricalPolicyQuickSetup.lua
+++ src/RobloxVersion/QuickSetups/QueuedCategoricalPolicyQuickSetup.lua
@@ -230,6 +230,8 @@ function QueuedCategoricalPolicyQuickSetup:start()
 	
 	local currentFeatureVector
 	
+	local terminalStateValue
+	
 	local selectedActionCountVector
 	
 	local isOriginalValueNotAVector

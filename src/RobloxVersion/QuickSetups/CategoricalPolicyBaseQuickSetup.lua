9a338f7909418a503d69bf6884286ef02c54812e
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-17 01:09:02 +0800

    Create CategoricalPolicyBaseQuickSetup.lua

---
+++ src/RobloxVersion/QuickSetups/CategoricalPolicyBaseQuickSetup.lua
@@ -0,0 +1,221 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+
+local ReinforcementLearningBaseQuickSetup = require(script.Parent.ReinforcementLearningBaseQuickSetup)
+
+CategoricalPolicyBaseQuickSetup = {}
+
+CategoricalPolicyBaseQuickSetup.__index = CategoricalPolicyBaseQuickSetup
+
+setmetatable(CategoricalPolicyBaseQuickSetup, ReinforcementLearningBaseQuickSetup)
+
+local defaultActionSelectionFunction = "Maximum"
+
+local defaultTemperature = 1
+
+local defaultCValue = 1
+
+local RandomObject = Random.new()
+
+local function selectIndexWithHighestValue(valueVector)
+	
+	local selectedIndex = 1
+	
+	local highestValue = -math.huge
+	
+	for index, value in ipairs(valueVector[1]) do
+
+		if (value > highestValue) then
+
+			highestValue = value
+
+			selectedIndex = index
+
+		end
+
+	end
+	
+	return selectedIndex
+	
+end
+
+local function calculateStableProbability(valueVector, temperature)
+
+	local maximumValue = AqwamTensorLibrary:findMaximumValue(valueVector)
+
+	local zValueVector = AqwamTensorLibrary:subtract(valueVector, maximumValue)
+	
+	local temperatureZValueVector = AqwamTensorLibrary:divide(zValueVector, temperature)
+
+	local exponentVector = AqwamTensorLibrary:exponent(temperatureZValueVector)
+
+	local sumExponentValue = AqwamTensorLibrary:sum(exponentVector)
+
+	local probabilityVector = AqwamTensorLibrary:divide(exponentVector, sumExponentValue)
+
+	return probabilityVector
+
+end
+
+local function calculateProbability(valueVector, temperature)
+
+	local temperatureZValueVector = AqwamTensorLibrary:divide(valueVector, temperature)
+
+	local exponentVector = AqwamTensorLibrary:exponent(temperatureZValueVector)
+
+	local sumExponentValue = AqwamTensorLibrary:sum(exponentVector)
+
+	local probabilityVector = AqwamTensorLibrary:divide(exponentVector, sumExponentValue)
+
+	return probabilityVector
+
+end
+
+local function sample(probabilityVector)
+
+	local totalProbability = 0
+	
+	local unwrappedProbabilityVector = probabilityVector[1]
+
+	for _, probability in ipairs(unwrappedProbabilityVector) do
+
+		totalProbability = totalProbability + probability
+
+	end
+
+	local randomValue = math.random() * totalProbability
+
+	local cumulativeProbability = 0
+
+	for index, probability in ipairs(unwrappedProbabilityVector) do
+
+		cumulativeProbability = cumulativeProbability + probability
+
+		if (randomValue <= cumulativeProbability) then return index end
+
+	end
+
+	return unwrappedProbabilityVector[#unwrappedProbabilityVector]
+
+end
+
+local function calculateUpperConfidenceBound(actionVector, cValue, totalNumberOfReinforcements, selectedActionCountVector)
+	
+	local naturalLogTotalNumberOfReinforcements = math.log(totalNumberOfReinforcements)
+	
+	local upperConfidenceBoundVector1 = AqwamTensorLibrary:divide(naturalLogTotalNumberOfReinforcements, selectedActionCountVector)
+	
+	local upperConfidenceBoundVector2 = AqwamTensorLibrary:multiply(cValue, upperConfidenceBoundVector1)
+	
+	local upperConfidenceBoundVector = AqwamTensorLibrary:add(actionVector, upperConfidenceBoundVector2)
+	
+	return upperConfidenceBoundVector
+	
+end
+
+function CategoricalPolicyBaseQuickSetup:selectAction(actionVector, selectedActionCountVector)
+	
+	local actionSelectionFunction = self.actionSelectionFunction
+	
+	local currentEpsilon = self.currentEpsilon or self.epsilon
+	
+	local EpsilonValueScheduler = self.EpsilonValueScheduler
+	
+	local randomProbability = RandomObject:NextNumber()
+	
+	local actionIndex
+	
+	if (not selectedActionCountVector) then selectedActionCountVector = {table.create(#actionVector[1], 0)} end
+	
+	if (randomProbability <= currentEpsilon) then
+		
+		actionIndex = RandomObject:NextInteger(1, #actionVector[1])
+	
+	elseif (actionSelectionFunction == "Maximum") then
+		
+		actionIndex = selectIndexWithHighestValue(actionVector)
+	
+	elseif (actionSelectionFunction == "StableSoftmaxSampling") or (actionSelectionFunction == "StableBoltzmannSampling") then
+		
+		local stableActionProbabilityVector = calculateStableProbability(actionVector, self.temperature)
+		
+		actionIndex = sample(stableActionProbabilityVector)
+		
+	elseif (actionSelectionFunction == "SoftmaxSampling") or (actionSelectionFunction == "BoltzmannSampling") then
+
+		local actionProbabilityVector = calculateProbability(actionVector, self.temperature)
+
+		actionIndex = sample(actionProbabilityVector)
+		
+	elseif (actionSelectionFunction == "UpperConfidenceBound") then
+		
+		local actionUpperConfidenceBoundVector = calculateUpperConfidenceBound(actionVector, self.cValue, self.currentNumberOfReinforcements, selectedActionCountVector)
+		
+		actionIndex = selectIndexWithHighestValue(actionUpperConfidenceBoundVector)
+		
+	else
+		
+		error("Invalid action selection function.")
+		
+	end
+	
+	if (EpsilonValueScheduler) then
+
+		self.currentEpsilon = EpsilonValueScheduler:calculate(currentEpsilon)
+
+	end
+	
+	selectedActionCountVector[1][actionIndex] = selectedActionCountVector[1][actionIndex] + 1
+	
+	return actionIndex, selectedActionCountVector
+	
+end
+
+function CategoricalPolicyBaseQuickSetup.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local NewCategoricalPolicyBaseQuickSetup = ReinforcementLearningBaseQuickSetup.new(parameterDictionary)
+	
+	setmetatable(NewCategoricalPolicyBaseQuickSetup, CategoricalPolicyBaseQuickSetup)
+	
+	NewCategoricalPolicyBaseQuickSetup:setClassName("CategoricalPolicyQuickSetup")
+	
+	NewCategoricalPolicyBaseQuickSetup.actionSelectionFunction = parameterDictionary.actionSelectionFunction or defaultActionSelectionFunction
+	
+	NewCategoricalPolicyBaseQuickSetup.temperature = parameterDictionary.temperature or defaultTemperature
+	
+	NewCategoricalPolicyBaseQuickSetup.cValue = parameterDictionary.cValue or defaultCValue
+	
+	return NewCategoricalPolicyBaseQuickSetup
+	
+end
+
+return CategoricalPolicyBaseQuickSetup

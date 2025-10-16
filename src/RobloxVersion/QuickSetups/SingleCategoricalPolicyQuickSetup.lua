1803f0fd4f7acd72c037797f604c7d7e96ae1642
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-17 01:27:21 +0800

    Create SingleCategoricalPolicyQuickSetup.lua

---
+++ src/RobloxVersion/QuickSetups/SingleCategoricalPolicyQuickSetup.lua
@@ -0,0 +1,157 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+
+local CategoricalPolicyBaseQuickSetup = require(script.Parent.CategoricalPolicyBaseQuickSetup)
+
+SingleCategoricalPolicyQuickSetup = {}
+
+SingleCategoricalPolicyQuickSetup.__index = SingleCategoricalPolicyQuickSetup
+
+setmetatable(SingleCategoricalPolicyQuickSetup, CategoricalPolicyBaseQuickSetup)
+
+function SingleCategoricalPolicyQuickSetup.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local NewSingleCategoricalPolicyQuickSetup = CategoricalPolicyBaseQuickSetup.new(parameterDictionary)
+	
+	setmetatable(NewSingleCategoricalPolicyQuickSetup, SingleCategoricalPolicyQuickSetup)
+	
+	NewSingleCategoricalPolicyQuickSetup:setName("SingleCategoricalPolicyQuickSetup")
+	
+	NewSingleCategoricalPolicyQuickSetup.previousAction = parameterDictionary.previousAction
+	
+	NewSingleCategoricalPolicyQuickSetup.selectedActionCountVector = parameterDictionary.selectedActionCountVector
+	
+	NewSingleCategoricalPolicyQuickSetup:setReinforceFunction(function(currentFeatureVector, rewardValue, returnOriginalOutput)
+		
+		local Model = NewSingleCategoricalPolicyQuickSetup.Model
+		
+		if (not Model) then error("No model.") end
+		
+		local isOriginalValueNotAVector = (type(currentFeatureVector) ~= "table")
+		
+		if (isOriginalValueNotAVector) then currentFeatureVector = {{currentFeatureVector}} end
+		
+		local numberOfReinforcementsPerEpisode = NewSingleCategoricalPolicyQuickSetup.numberOfReinforcementsPerEpisode
+
+		local currentNumberOfReinforcements = NewSingleCategoricalPolicyQuickSetup.currentNumberOfReinforcements
+
+		local currentNumberOfEpisodes = NewSingleCategoricalPolicyQuickSetup.currentNumberOfEpisodes
+		
+		local ExperienceReplay = NewSingleCategoricalPolicyQuickSetup.ExperienceReplay
+		
+		local previousFeatureVector = NewSingleCategoricalPolicyQuickSetup.previousFeatureVector
+		
+		local previousAction = NewSingleCategoricalPolicyQuickSetup.previousAction
+
+		local ActionsList = Model:getActionsList()
+
+		local actionVector = Model:predict(currentFeatureVector, true)
+		
+		local terminalStateValue = 0
+
+		local temporalDifferenceError
+		
+		if (isOriginalValueNotAVector) then currentFeatureVector = currentFeatureVector[1][1] end
+		
+		local actionIndex = NewSingleCategoricalPolicyQuickSetup:selectAction(actionVector)
+
+		local action = ActionsList[actionIndex]
+
+		local actionValue = actionVector[1][actionIndex]
+		
+		if (currentNumberOfReinforcements >= numberOfReinforcementsPerEpisode) then terminalStateValue = 1 end
+
+		if (previousFeatureVector) then
+			
+			local updateFunction = NewSingleCategoricalPolicyQuickSetup.updateFunction
+
+			currentNumberOfReinforcements = currentNumberOfReinforcements + 1
+
+			temporalDifferenceError = Model:categoricalUpdate(previousFeatureVector, previousAction, rewardValue, currentFeatureVector, terminalStateValue)
+
+			if (updateFunction) then updateFunction() end
+
+		end
+
+		if (currentNumberOfReinforcements >= numberOfReinforcementsPerEpisode) then
+
+			local episodeUpdateFunction = NewSingleCategoricalPolicyQuickSetup.episodeUpdateFunction
+
+			currentNumberOfReinforcements = 0
+
+			currentNumberOfEpisodes = currentNumberOfEpisodes + 1
+
+			Model:episodeUpdate(terminalStateValue)
+
+			if episodeUpdateFunction then episodeUpdateFunction() end
+
+		end
+		
+		if (previousFeatureVector) then
+			
+			if (ExperienceReplay) then
+
+				ExperienceReplay:addExperience(previousFeatureVector, previousAction, rewardValue, currentFeatureVector, terminalStateValue)
+
+				ExperienceReplay:addTemporalDifferenceError(temporalDifferenceError)
+
+				ExperienceReplay:run(function(storedPreviousFeatureVector, storedAction, storedRewardValue, storedCurrentFeatureVector, storedTerminalStateValue)
+
+					return Model:categoricalUpdate(storedPreviousFeatureVector, storedAction, storedRewardValue, storedCurrentFeatureVector, storedTerminalStateValue)
+
+				end)
+
+			end
+			
+		end
+
+		NewSingleCategoricalPolicyQuickSetup.currentNumberOfReinforcements = currentNumberOfReinforcements
+
+		NewSingleCategoricalPolicyQuickSetup.currentNumberOfEpisodes = currentNumberOfEpisodes
+
+		NewSingleCategoricalPolicyQuickSetup.previousFeatureVector = currentFeatureVector
+		
+		NewSingleCategoricalPolicyQuickSetup.previousAction = action
+		
+		if (NewSingleCategoricalPolicyQuickSetup.isOutputPrinted) then print("Episode: " .. currentNumberOfEpisodes .. "\t\tReinforcement Count: " .. currentNumberOfReinforcements) end
+
+		if (returnOriginalOutput) then return actionVector end
+
+		return action, actionValue
+		
+	end)
+	
+	return NewSingleCategoricalPolicyQuickSetup
+	
+end
+
+return SingleCategoricalPolicyQuickSetup

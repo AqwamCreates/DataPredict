c46f57c13db4362c942a06172ac618e70532d4e2
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-17 01:24:53 +0800

    Create DiagonalGaussianPolicyBaseQuickSetup.lua

---
+++ src/RobloxVersion/QuickSetups/DiagonalGaussianPolicyBaseQuickSetup.lua
@@ -0,0 +1,61 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+
+local ReinforcementLearningBaseQuickSetup = require(script.Parent.ReinforcementLearningBaseQuickSetup)
+
+DiagonalGaussianPolicyBaseQuickSetup = {}
+
+DiagonalGaussianPolicyBaseQuickSetup.__index = DiagonalGaussianPolicyBaseQuickSetup
+
+setmetatable(DiagonalGaussianPolicyBaseQuickSetup, ReinforcementLearningBaseQuickSetup)
+
+function DiagonalGaussianPolicyBaseQuickSetup.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local NewDiagonalGaussianPolicyBaseQuickSetup = ReinforcementLearningBaseQuickSetup.new(parameterDictionary)
+	
+	setmetatable(NewDiagonalGaussianPolicyBaseQuickSetup, DiagonalGaussianPolicyBaseQuickSetup)
+	
+	NewDiagonalGaussianPolicyBaseQuickSetup:setName("DiagonalGaussianPolicyBaseQuickSetup")
+	
+	NewDiagonalGaussianPolicyBaseQuickSetup:setClassName("DiagonalGaussianPolicyQuickSetup")
+	
+	local actionStandardDeviationVector = parameterDictionary.actionStandardDeviationVector
+	
+	NewDiagonalGaussianPolicyBaseQuickSetup.actionStandardDeviationVector = actionStandardDeviationVector
+	
+	NewDiagonalGaussianPolicyBaseQuickSetup.actionNoiseVector = parameterDictionary.actionNoiseVector
+	
+	return NewDiagonalGaussianPolicyBaseQuickSetup
+	
+end
+
+return DiagonalGaussianPolicyBaseQuickSetup

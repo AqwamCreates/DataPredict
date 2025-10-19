4570cc11ec3666dff6ebbfdd3a28412fb9dad19d
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-19 13:45:08 +0800

    Update KMeans.lua

--- src/RobloxVersion/Models/KMeans.lua
+++ src/RobloxVersion/Models/KMeans.lua
@@ -30,6 +30,8 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local IterativeMethodBaseModel = require(script.Parent.IterativeMethodBaseModel)
 
+local distanceFunctionDictionary = require(script.Parent.Parent.Cores.DistanceFunctionDictionary)
+
 KMeansModel = {}
 
 KMeansModel.__index = KMeansModel
@@ -47,62 +49,6 @@ local defaultMode = "Hybrid"
 local defaultSetInitialCentroidsOnDataPoints = true
 
 local defaultSetTheCentroidsDistanceFarthest = true
-
-local distanceFunctionList = {
-
-	["Manhattan"] = function (x1, x2)
-
-		local part1 = AqwamTensorLibrary:subtract(x1, x2)
-
-		part1 = AqwamTensorLibrary:applyFunction(math.abs, part1)
-
-		local distance = AqwamTensorLibrary:sum(part1)
-
-		return distance 
-
-	end,
-
-	["Euclidean"] = function (x1, x2)
-
-		local part1 = AqwamTensorLibrary:subtract(x1, x2)
-
-		local part2 = AqwamTensorLibrary:power(part1, 2)
-
-		local part3 = AqwamTensorLibrary:sum(part2)
-
-		local distance = math.sqrt(part3)
-
-		return distance 
-
-	end,
-	
-	["Cosine"] = function(x1, x2)
-
-		local dotProductedX = AqwamTensorLibrary:dotProduct(x1, AqwamTensorLibrary:transpose(x2))
-
-		local x1MagnitudePart1 = AqwamTensorLibrary:power(x1, 2)
-
-		local x1MagnitudePart2 = AqwamTensorLibrary:sum(x1MagnitudePart1)
-
-		local x1Magnitude = math.sqrt(x1MagnitudePart2)
-
-		local x2MagnitudePart1 = AqwamTensorLibrary:power(x2, 2)
-
-		local x2MagnitudePart2 = AqwamTensorLibrary:sum(x2MagnitudePart1)
-
-		local x2Magnitude = math.sqrt(x2MagnitudePart2)
-
-		local normX = x1Magnitude * x2Magnitude
-
-		local similarity = dotProductedX / normX
-
-		local cosineDistance = 1 - similarity
-
-		return cosineDistance
-
-	end,
-
-}
 
 local function assignToCluster(distanceMatrix) -- Number of columns -> number of clusters
 	
@@ -481,7 +427,7 @@ function KMeansModel:train(featureMatrix)
 
 	if (not kMeansFunction) then error("Unknown mode.") end
 	
-	local distanceFunctionToApply = distanceFunctionList[distanceFunction]
+	local distanceFunctionToApply = distanceFunctionDictionary[distanceFunction]
 
 	if (not distanceFunctionToApply) then error("Unknown distance function.") end
 	
@@ -559,7 +505,7 @@ end
 
 function KMeansModel:predict(featureMatrix, returnOriginalOutput)
 	
-	local distanceFunctionToApply = distanceFunctionList[self.distanceFunction]
+	local distanceFunctionToApply = distanceFunctionDictionary[self.distanceFunction]
 	
 	local ModelParameters = self.ModelParameters
 	

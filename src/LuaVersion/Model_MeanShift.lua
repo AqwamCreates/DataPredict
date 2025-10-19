83185baef3b377b8e1e4ba270a5faab8c0055e8b
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-19 13:47:40 +0800

    Update Model_MeanShift.lua

--- src/LuaVersion/Model_MeanShift.lua
+++ src/LuaVersion/Model_MeanShift.lua
@@ -30,6 +30,8 @@ local AqwamTensorLibrary = require("AqwamTensorLibrary")
 
 local IterativeMethodBaseModel = require("Model_IterativeMethodBaseModel")
 
+local distanceFunctionDictionary = require("Core_DistanceFunctionDictionary")
+
 MeanShiftModel = {}
 
 MeanShiftModel.__index = MeanShiftModel
@@ -49,62 +51,6 @@ local defaultDistanceFunction = "Euclidean"
 local defaultKernelFunction = "Gaussian"
 
 local defaultLambda = 50
-
-local distanceFunctionList = {
-
-	["Manhattan"] = function (x1, x2)
-
-		local part1 = AqwamTensorLibrary:subtract(x1, x2)
-
-		part1 = AqwamTensorLibrary:applyFunction(math.abs, part1)
-
-		local distance = AqwamTensorLibrary:sum(part1)
-
-		return distance 
-
-	end,
-
-	["Euclidean"] = function (x1, x2)
-
-		local part1 = AqwamTensorLibrary:subtract(x1, x2)
-
-		local part2 = AqwamTensorLibrary:power(part1, 2)
-
-		local part3 = AqwamTensorLibrary:sum(part2)
-
-		local distance = math.sqrt(part3)
-
-		return distance 
-
-	end,
-	
-	["Cosine"] = function(x1, x2)
-
-		local dotProductedX = AqwamTensorLibrary:dotProduct(x1, AqwamTensorLibrary:transpose(x2))
-
-		local x1MagnitudePart1 = AqwamTensorLibrary:power(x1, 2)
-
-		local x1MagnitudePart2 = AqwamTensorLibrary:sum(x1MagnitudePart1)
-
-		local x1Magnitude = math.sqrt(x1MagnitudePart2, 2)
-
-		local x2MagnitudePart1 = AqwamTensorLibrary:power(x2, 2)
-
-		local x2MagnitudePart2 = AqwamTensorLibrary:sum(x2MagnitudePart1)
-
-		local x2Magnitude = math.sqrt(x2MagnitudePart2, 2)
-
-		local normX = x1Magnitude * x2Magnitude
-
-		local similarity = dotProductedX / normX
-
-		local cosineDistance = 1 - similarity
-
-		return cosineDistance
-
-	end,
-
-}
 
 local kernelFunctionList = {
 
@@ -438,7 +384,7 @@ function MeanShiftModel:train(featureMatrix)
 
 	end
 
-	local distanceFunctionToApply = distanceFunctionList[distanceFunction]
+	local distanceFunctionToApply = distanceFunctionDictionary[distanceFunction]
 
 	if (not distanceFunctionToApply) then error("Unknown distance function.") end
 	
@@ -528,7 +474,7 @@ end
 
 function MeanShiftModel:predict(featureMatrix, returnOriginalOutput)
 	
-	local distanceFunctionToApply = distanceFunctionList[self.distanceFunction]
+	local distanceFunctionToApply = distanceFunctionDictionary[self.distanceFunction]
 
 	local ModelParameters = self.ModelParameters
 

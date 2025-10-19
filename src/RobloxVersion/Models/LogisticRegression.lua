732747cced7318e64fc2b9263a80e2ad24833747
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-10-19 10:27:50 +0800

    Update LogisticRegression.lua

--- src/RobloxVersion/Models/LogisticRegression.lua
+++ src/RobloxVersion/Models/LogisticRegression.lua
@@ -54,7 +54,7 @@ local sigmoidFunctionList = {
 		
 		local x = (z + 1) / 2
 		
-		if x < 0 then return 0 elseif x > 1 then return 1 else return x end
+		if (x < 0) then return 0 elseif (x > 1) then return 1 else return x end
 		
 	end,
 
@@ -115,9 +115,9 @@ local lossFunctionList = {
 
 local cutOffList = {
 	
-	["0.5"] = {"Sigmoid", "HardSigmoid", "Swish"}, -- 0.5 threshold for [0,1] functions
+	["0.5"] = {"Sigmoid", "HardSigmoid", "Swish"}, -- 0.5 threshold for [0,1] functions.
 
-	["0"] = {"Tanh", "Softsign", "ArcTangent", "BipolarSigmoid"}, -- 0 threshold for [-1,1] functions
+	["0"] = {"Tanh", "Softsign", "ArcTangent", "BipolarSigmoid"}, -- 0 threshold for [-1,1] functions.
 	
 }
 

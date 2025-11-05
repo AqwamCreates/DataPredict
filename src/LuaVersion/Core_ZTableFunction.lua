021ecaab6fe055c1afd87797ef9e32c6a36327be
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-05 21:09:50 +0800

    Update Core_ZTableFunction.lua

--- src/LuaVersion/Core_ZTableFunction.lua
+++ src/LuaVersion/Core_ZTableFunction.lua
@@ -603,28 +603,28 @@ local rowStringFormat = "%.1f"
 
 local columnStringFormat = "%.2f"
 
-function zTableFunction:getStandardNormalCumulativeDistributionFunction(zValue)
-	
+function zTableFunction:calculateStandardNormalCumulativeDistributionValue(zValue)
+
 	local rowStringFormat = rowStringFormat
 
 	local columnStringFormat = columnStringFormat
-	
+
 	local stringFormatFunction = string.format
-	
+
 	local isPositive = (zValue > 0)
-	
+
 	local finalZValue = (isPositive and -zValue) or zValue
-	
+
 	local clampedZValue = math.clamp(finalZValue, -3.9, 0)
-	
+
 	local rowValue = math.floor(clampedZValue * 10) / 10
-	
+
 	local rowString = stringFormatFunction(rowStringFormat, rowValue)
-	
+
 	local rowTable = zTable[rowString]
-	
+
 	if (not rowTable) then return end
-	
+
 	local columnValue = finalZValue - rowValue
 
 	local absoluteColumnValue = math.abs(columnValue)
@@ -636,101 +636,101 @@ function zTableFunction:getStandardNormalCumulativeDistributionFunction(zValue)
 	local lowerColumnString = stringFormatFunction(columnStringFormat, lowerColumnValue)
 
 	local upperColumnString = stringFormatFunction(columnStringFormat, upperColumnValue)
-	
+
 	local lowerCumulativeDistributionFunctionValue = rowTable[lowerColumnString]
 
 	local upperCumulativeDistributionFunctionValue = rowTable[upperColumnString]
-	
+
 	if (not lowerCumulativeDistributionFunctionValue) then return upperCumulativeDistributionFunctionValue end
-	
+
 	if (not upperCumulativeDistributionFunctionValue) then return lowerCumulativeDistributionFunctionValue end
-	
+
 	local fraction = (absoluteColumnValue - lowerColumnValue) / (upperColumnValue - lowerColumnValue)
 
 	local cumulativeDistributionFunctionValue = lowerCumulativeDistributionFunctionValue + (upperCumulativeDistributionFunctionValue - lowerCumulativeDistributionFunctionValue) * fraction
-	
+
 	if (isPositive) then cumulativeDistributionFunctionValue = 1 - cumulativeDistributionFunctionValue end
-	
+
 	return cumulativeDistributionFunctionValue
-	
+
 end
 
-function zTableFunction:getStandardNormalInverseCumulativeDistributionFunction(probability)
-	
+function zTableFunction:calculateStandardNormalInverseCumulativeDistributionValue(probability)
+
 	local rowStringFormat = rowStringFormat
 
 	local columnStringFormat = columnStringFormat
-	
+
 	local stringFormatFunction = string.format
-	
+
 	local isFlipped = (probability > 0.5)
 
 	local finalProbability = (isFlipped and (1 - probability)) or probability
-	
+
 	local clampedProbability = math.clamp(finalProbability, 0.00005, 0.5)
 
 	local closestZValue
-	
+
 	local rowString
-	
+
 	local rowTable
-	
+
 	local columnValueArray
-	
+
 	local columnValue1
-	
+
 	local columnValue2
-	
+
 	local probabilityValue1
-	
+
 	local probabilityValue2
-	
+
 	local fraction
-	
+
 	local closestZValue
 
 	for _, rowValue in ipairs(cachedRowValueArray) do
-		
+
 		rowString = stringFormatFunction(rowStringFormat, rowValue)
-		
+
 		rowTable = zTable[rowString]
 
 		columnValueArray = {}
-		
+
 		for k in pairs(rowTable) do table.insert(columnValueArray, tonumber(k)) end
-		
+
 		table.sort(columnValueArray)
 
 		for i = 1, #columnValueArray - 1 do
-			
+
 			columnValue1 = columnValueArray[i]
-			
+
 			columnValue2 = columnValueArray[i + 1]
-			
+
 			probabilityValue1 = rowTable[stringFormatFunction(columnStringFormat, columnValue1)]
-			
+
 			probabilityValue2 = rowTable[stringFormatFunction(columnStringFormat, columnValue2)]
 
 			if (clampedProbability >= probabilityValue1) and (clampedProbability <= probabilityValue2) then
 
 				fraction = (clampedProbability - probabilityValue1) / (probabilityValue2 - probabilityValue1)
-				
+
 				closestZValue = rowValue - (columnValue1 + (fraction * (columnValue2 - columnValue1)))
-				
+
 				break
-				
+
 			end
-			
+
 		end
-		
+
 		if (closestZValue) then break end
-		
+
 	end
-	
+
 	if (isFlipped) and (closestZValue) then closestZValue = -closestZValue end
 
 	return closestZValue
-	
+
 end
 
 return zTableFunction

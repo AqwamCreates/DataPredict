6e1b7d56f8729c0bace342814b08f896971b9c5e
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 20:53:24 +0800

    Update ValueScheduler_BaseValueScheduler.lua

--- src/LuaVersion/ValueScheduler_BaseValueScheduler.lua
+++ src/LuaVersion/ValueScheduler_BaseValueScheduler.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require("AqwamTensorLibrary")
 
 local BaseInstance = require("Core_BaseInstance")
 
-BaseValueScheduler = {}
+local BaseValueScheduler = {}
 
 BaseValueScheduler.__index = BaseValueScheduler
 

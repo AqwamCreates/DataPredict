000d98ace2a1367a75285755138118119e5b097e
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 21:01:22 +0800

    Update RectifiedAdaptiveMomentEstimation.lua

--- src/RobloxVersion/Optimizers/RectifiedAdaptiveMomentEstimation.lua
+++ src/RobloxVersion/Optimizers/RectifiedAdaptiveMomentEstimation.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local BaseOptimizer = require(script.Parent.BaseOptimizer)
 
-RectifiedAdaptiveMomentEstimationOptimizer = {}
+local RectifiedAdaptiveMomentEstimationOptimizer = {}
 
 RectifiedAdaptiveMomentEstimationOptimizer.__index = RectifiedAdaptiveMomentEstimationOptimizer
 

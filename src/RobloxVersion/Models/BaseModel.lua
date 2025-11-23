6bd1cf889bbc079bfbfa5f2b9052bb90e6de2da4
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 22:38:32 +0800

    Update BaseModel.lua

--- src/RobloxVersion/Models/BaseModel.lua
+++ src/RobloxVersion/Models/BaseModel.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local BaseInstance = require(script.Parent.Parent.Cores.BaseInstance)
 
-BaseModel = {}
+local BaseModel = {}
 
 BaseModel.__index = BaseModel
 

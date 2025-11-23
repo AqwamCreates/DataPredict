72f73d484717320d70dd00d3198e9e0b3d1a46da
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 20:54:31 +0800

    Update ValueScheduler_Sequential.lua

--- src/LuaVersion/ValueScheduler_Sequential.lua
+++ src/LuaVersion/ValueScheduler_Sequential.lua
@@ -28,7 +28,7 @@
 
 local BaseValueScheduler = require("ValueScheduler_BaseValueScheduler")
 
-SequentialValueScheduler = {}
+local SequentialValueScheduler = {}
 
 SequentialValueScheduler.__index = SequentialValueScheduler
 

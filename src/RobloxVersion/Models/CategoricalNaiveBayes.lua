07b018a6c62dd1ef60dc122ce20e699e9467f2f5
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 23:03:46 +0800

    Update CategoricalNaiveBayes.lua

--- src/RobloxVersion/Models/CategoricalNaiveBayes.lua
+++ src/RobloxVersion/Models/CategoricalNaiveBayes.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local NaiveBayesBaseModel = require(script.Parent.NaiveBayesBaseModel)
 
-CategoricalNaiveBayesModel = {}
+local CategoricalNaiveBayesModel = {}
 
 CategoricalNaiveBayesModel.__index = CategoricalNaiveBayesModel
 

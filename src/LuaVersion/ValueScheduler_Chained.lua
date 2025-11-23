59e1381b3275f7ef18a4f0876387d04807ee7cb5
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 20:59:25 +0800

    Update ValueScheduler_Chained.lua

--- src/LuaVersion/ValueScheduler_Chained.lua
+++ src/LuaVersion/ValueScheduler_Chained.lua
@@ -28,7 +28,7 @@
 
 local BaseValueScheduler = require("ValueScheduler_BaseValueScheduler")
 
-ChainedValueScheduler = {}
+local ChainedValueScheduler = {}
 
 ChainedValueScheduler.__index = ChainedValueScheduler
 

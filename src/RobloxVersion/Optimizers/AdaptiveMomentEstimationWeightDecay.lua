f947a545f6172e7601f227608838de358bc5247b
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 21:02:23 +0800

    Update AdaptiveMomentEstimationWeightDecay.lua

--- src/RobloxVersion/Optimizers/AdaptiveMomentEstimationWeightDecay.lua
+++ src/RobloxVersion/Optimizers/AdaptiveMomentEstimationWeightDecay.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local BaseOptimizer = require(script.Parent.BaseOptimizer)
 
-AdaptiveMomentEstimationWeightDecayOptimizer = {}
+local AdaptiveMomentEstimationWeightDecayOptimizer = {}
 
 AdaptiveMomentEstimationWeightDecayOptimizer.__index = AdaptiveMomentEstimationWeightDecayOptimizer
 

76007749f9648feed5da9da84fe7ae4be256ded7
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 20:55:05 +0800

    Update ValueScheduler_Polynomial.lua

--- src/LuaVersion/ValueScheduler_Polynomial.lua
+++ src/LuaVersion/ValueScheduler_Polynomial.lua
@@ -28,7 +28,7 @@
 
 local BaseValueScheduler = require("ValueScheduler_BaseValueScheduler")
 
-PolynomialValueScheduler = {}
+local PolynomialValueScheduler = {}
 
 PolynomialValueScheduler.__index = PolynomialValueScheduler
 

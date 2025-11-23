bfd40e2247c2067ed8ee9aea6fe7ec5bbf78d0b0
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-24 00:10:55 +0800

    Update SoftActorCritic.lua

--- src/RobloxVersion/Models/SoftActorCritic.lua
+++ src/RobloxVersion/Models/SoftActorCritic.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local DeepReinforcementLearningActorCriticBaseModel = require(script.Parent.DeepReinforcementLearningActorCriticBaseModel)
 
-SoftActorCriticModel = {}
+local SoftActorCriticModel = {}
 
 SoftActorCriticModel.__index = SoftActorCriticModel
 

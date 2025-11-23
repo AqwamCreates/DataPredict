e6652c6c472156344793e8d74032f1c56cfdc667
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 19:44:15 +0800

    Update ParallelDiagonalGaussianPolicy.lua

--- src/RobloxVersion/QuickSetups/ParallelDiagonalGaussianPolicy.lua
+++ src/RobloxVersion/QuickSetups/ParallelDiagonalGaussianPolicy.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local DiagonalGaussianPolicyBaseQuickSetup = require(script.Parent.DiagonalGaussianPolicyBaseQuickSetup)
 
-ParallelDiagonalGaussianPolicyQuickSetup = {}
+local ParallelDiagonalGaussianPolicyQuickSetup = {}
 
 ParallelDiagonalGaussianPolicyQuickSetup.__index = ParallelDiagonalGaussianPolicyQuickSetup
 

80d12e6c501c129567488b847f5977975be1958e
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 21:02:38 +0800

    Update AdaptiveMomentEstimationMaximum.lua

--- src/RobloxVersion/Optimizers/AdaptiveMomentEstimationMaximum.lua
+++ src/RobloxVersion/Optimizers/AdaptiveMomentEstimationMaximum.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local BaseOptimizer = require(script.Parent.BaseOptimizer)
 
-AdaptiveMomentEstimationMaximumOptimizer = {}
+local AdaptiveMomentEstimationMaximumOptimizer = {}
 
 AdaptiveMomentEstimationMaximumOptimizer.__index = AdaptiveMomentEstimationMaximumOptimizer
 

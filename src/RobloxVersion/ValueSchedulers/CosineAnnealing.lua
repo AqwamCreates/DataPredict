e8e18cc8643e31477a1db8c2d639b2464d3b8e23
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 20:50:34 +0800

    Update CosineAnnealing.lua

--- src/RobloxVersion/ValueSchedulers/CosineAnnealing.lua
+++ src/RobloxVersion/ValueSchedulers/CosineAnnealing.lua
@@ -28,7 +28,7 @@
 
 local BaseValueScheduler = require(script.Parent.BaseValueScheduler)
 
-CosineAnnealingValueScheduler = {}
+local CosineAnnealingValueScheduler = {}
 
 CosineAnnealingValueScheduler.__index = CosineAnnealingValueScheduler
 

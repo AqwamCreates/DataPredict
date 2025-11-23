f353aa81c0a2bfc1db06249e3c321fcb3bef50a7
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 20:50:10 +0800

    Update Chained.lua

--- src/RobloxVersion/ValueSchedulers/Chained.lua
+++ src/RobloxVersion/ValueSchedulers/Chained.lua
@@ -28,7 +28,7 @@
 
 local BaseValueScheduler = require(script.Parent.BaseValueScheduler)
 
-ChainedValueScheduler = {}
+local ChainedValueScheduler = {}
 
 ChainedValueScheduler.__index = ChainedValueScheduler
 

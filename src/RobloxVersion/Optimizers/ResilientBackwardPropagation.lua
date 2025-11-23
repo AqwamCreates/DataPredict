317b3575a8b059f0be7aa6c9780716823b85a5ca
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 21:01:11 +0800

    Update ResilientBackwardPropagation.lua

--- src/RobloxVersion/Optimizers/ResilientBackwardPropagation.lua
+++ src/RobloxVersion/Optimizers/ResilientBackwardPropagation.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local BaseOptimizer = require(script.Parent.BaseOptimizer)
 
-ResilientBackwardPropagationOptimizer = {}
+local ResilientBackwardPropagationOptimizer = {}
 
 ResilientBackwardPropagationOptimizer.__index = ResilientBackwardPropagationOptimizer
 

0c1ed05255aac66771706a8cdf4e336e34abcc26
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 20:56:43 +0800

    Update ValueScheduler_Linear.lua

--- src/LuaVersion/ValueScheduler_Linear.lua
+++ src/LuaVersion/ValueScheduler_Linear.lua
@@ -28,7 +28,7 @@
 
 local BaseValueScheduler = require("ValueScheduler_BaseValueScheduler")
 
-LinearValueScheduler = {}
+local LinearValueScheduler = {}
 
 LinearValueScheduler.__index = LinearValueScheduler
 

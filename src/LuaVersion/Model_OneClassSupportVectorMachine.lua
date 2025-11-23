5307854824f3fb94fcc8b451ad1be5a85d4540b7
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 23:54:18 +0800

    Update Model_OneClassSupportVectorMachine.lua

--- src/LuaVersion/Model_OneClassSupportVectorMachine.lua
+++ src/LuaVersion/Model_OneClassSupportVectorMachine.lua
@@ -32,7 +32,7 @@ local IterativeMethodBaseModel = require("Model_IterativeMethodBaseModel")
 
 local distanceFunctionDictionary = require("Core_DistanceFunctionDictionary")
 
-OneClassSupportVectorMachineModel = {}
+local OneClassSupportVectorMachineModel = {}
 
 OneClassSupportVectorMachineModel.__index = OneClassSupportVectorMachineModel
 

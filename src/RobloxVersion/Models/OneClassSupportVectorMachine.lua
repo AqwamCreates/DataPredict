b76d8881bc86edb96bf40f89d8b9c5e688e39ad3
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 23:55:26 +0800

    Update OneClassSupportVectorMachine.lua

--- src/RobloxVersion/Models/OneClassSupportVectorMachine.lua
+++ src/RobloxVersion/Models/OneClassSupportVectorMachine.lua
@@ -32,7 +32,7 @@ local IterativeMethodBaseModel = require(script.Parent.IterativeMethodBaseModel)
 
 local distanceFunctionDictionary = require(script.Parent.Parent.Cores.DistanceFunctionDictionary)
 
-OneClassSupportVectorMachineModel = {}
+local OneClassSupportVectorMachineModel = {}
 
 OneClassSupportVectorMachineModel.__index = OneClassSupportVectorMachineModel
 

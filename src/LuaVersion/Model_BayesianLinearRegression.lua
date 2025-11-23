04faf24aabf28c79e754f93054ed1e151a738315
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 22:48:51 +0800

    Update Model_BayesianLinearRegression.lua

--- src/LuaVersion/Model_BayesianLinearRegression.lua
+++ src/LuaVersion/Model_BayesianLinearRegression.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require("AqwamTensorLibrary")
 
 local BaseModel = require("Model_BaseModel")
 
-BayesianLinearRegressionModel = {}
+local BayesianLinearRegressionModel = {}
 
 BayesianLinearRegressionModel.__index = BayesianLinearRegressionModel
 

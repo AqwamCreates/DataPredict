b262dfec953759c7f11ce661b86c5e85e91e0f09
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-23 19:18:32 +0800

    Update DistributedGradientsCoordinator.lua

--- src/RobloxVersion/DistributedTrainingStrategies/DistributedGradientsCoordinator.lua
+++ src/RobloxVersion/DistributedTrainingStrategies/DistributedGradientsCoordinator.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local BaseInstance = require(script.Parent.Parent.Cores.BaseInstance)
 
-DistributedGradientsCoordinator = {}
+local DistributedGradientsCoordinator = {}
 
 DistributedGradientsCoordinator.__index = DistributedGradientsCoordinator
 

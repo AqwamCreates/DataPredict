a758552750674950ea15cd087ac5f431c2a8b83e
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-24 10:45:51 +0800

    Update Model_TabularMonteCarloControl.lua

--- src/LuaVersion/Model_TabularMonteCarloControl.lua
+++ src/LuaVersion/Model_TabularMonteCarloControl.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require("AqwamTensorLibrary")
 
 local TabularReinforcementLearningBaseModel = require("Model_TabularReinforcementLearningBaseModel")
 
-TabularMonteCarloControlModel = {}
+local TabularMonteCarloControlModel = {}
 
 TabularMonteCarloControlModel.__index = TabularMonteCarloControlModel
 

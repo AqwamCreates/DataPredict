aa936029ba6162c3dc8c3ee32c5ea049ed0f7b53
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-24 10:38:56 +0800

    Update TabularTemporalDifference.lua

--- src/RobloxVersion/Models/TabularTemporalDifference.lua
+++ src/RobloxVersion/Models/TabularTemporalDifference.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local TabularReinforcementLearningBaseModel = require(script.Parent.TabularReinforcementLearningBaseModel)
 
-TabularTemporalDifferenceModel = {}
+local TabularTemporalDifferenceModel = {}
 
 TabularTemporalDifferenceModel.__index = TabularTemporalDifferenceModel
 

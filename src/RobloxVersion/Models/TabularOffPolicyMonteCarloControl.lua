12fc626722ded8df51a8534f7715f749ac35a2d3
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-24 10:36:42 +0800

    Update TabularOffPolicyMonteCarloControl.lua

--- src/RobloxVersion/Models/TabularOffPolicyMonteCarloControl.lua
+++ src/RobloxVersion/Models/TabularOffPolicyMonteCarloControl.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local TabularReinforcementLearningBaseModel = require(script.Parent.TabularReinforcementLearningBaseModel)
 
-TabularOffPolicyMonteCarloControlModel = {}
+local TabularOffPolicyMonteCarloControlModel = {}
 
 TabularOffPolicyMonteCarloControlModel.__index = TabularOffPolicyMonteCarloControlModel
 

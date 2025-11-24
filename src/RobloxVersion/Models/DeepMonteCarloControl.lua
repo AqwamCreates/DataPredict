0e2841e1c82e70a36600098f404317ab2df2dd00
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-24 12:21:25 +0800

    Update DeepMonteCarloControl.lua

--- src/RobloxVersion/Models/DeepMonteCarloControl.lua
+++ src/RobloxVersion/Models/DeepMonteCarloControl.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local DeepReinforcementLearningBaseModel = require(script.Parent.DeepReinforcementLearningBaseModel)
 
-DeepMonteCarloControlModel = {}
+local DeepMonteCarloControlModel = {}
 
 DeepMonteCarloControlModel.__index = DeepMonteCarloControlModel
 

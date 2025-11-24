bc0ccdd2b4d81664c02e774b4b170b61e7d31b19
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-24 12:19:24 +0800

    Update DeepDoubleExpectedStateActionRewardStateActionV2.lua

--- src/RobloxVersion/Models/DeepDoubleExpectedStateActionRewardStateActionV2.lua
+++ src/RobloxVersion/Models/DeepDoubleExpectedStateActionRewardStateActionV2.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local DeepReinforcementLearningBaseModel = require(script.Parent.DeepReinforcementLearningBaseModel)
 
-DeepDoubleExpectedStateActionRewardStateActionModel = {}
+local DeepDoubleExpectedStateActionRewardStateActionModel = {}
 
 DeepDoubleExpectedStateActionRewardStateActionModel.__index = DeepDoubleExpectedStateActionRewardStateActionModel
 

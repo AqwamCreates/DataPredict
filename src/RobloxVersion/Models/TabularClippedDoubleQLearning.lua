41bcc63b0c45c40424dd2fa8196f7092f03209ed
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-24 10:33:36 +0800

    Update TabularClippedDoubleQLearning.lua

--- src/RobloxVersion/Models/TabularClippedDoubleQLearning.lua
+++ src/RobloxVersion/Models/TabularClippedDoubleQLearning.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local TabularReinforcementLearningBaseModel = require(script.Parent.TabularReinforcementLearningBaseModel)
 
-TabularClippedDoubleQLearningModel = {}
+local TabularClippedDoubleQLearningModel = {}
 
 TabularClippedDoubleQLearningModel.__index = TabularClippedDoubleQLearningModel
 

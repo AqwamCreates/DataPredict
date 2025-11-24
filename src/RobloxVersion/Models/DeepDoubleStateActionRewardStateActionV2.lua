a4b8bf7ad6d78a7aa213e6e2995029a2500495db
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-24 12:20:40 +0800

    Update DeepDoubleStateActionRewardStateActionV2.lua

--- src/RobloxVersion/Models/DeepDoubleStateActionRewardStateActionV2.lua
+++ src/RobloxVersion/Models/DeepDoubleStateActionRewardStateActionV2.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local DeepReinforcementLearningBaseModel = require(script.Parent.DeepReinforcementLearningBaseModel)
 
-DeepDoubleStateActionRewardStateActionModel = {}
+local DeepDoubleStateActionRewardStateActionModel = {}
 
 DeepDoubleStateActionRewardStateActionModel.__index = DeepDoubleStateActionRewardStateActionModel
 

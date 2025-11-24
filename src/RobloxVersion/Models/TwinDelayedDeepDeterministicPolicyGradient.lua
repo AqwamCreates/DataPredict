724f40cee9f1338f16138ef0ad0023d4360ef944
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-24 10:38:39 +0800

    Update TwinDelayedDeepDeterministicPolicyGradient.lua

--- src/RobloxVersion/Models/TwinDelayedDeepDeterministicPolicyGradient.lua
+++ src/RobloxVersion/Models/TwinDelayedDeepDeterministicPolicyGradient.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local DeepReinforcementLearningActorCriticBaseModel = require(script.Parent.DeepReinforcementLearningActorCriticBaseModel)
 
-TwinDelayedDeepDeterministicPolicyGradientModel = {}
+local TwinDelayedDeepDeterministicPolicyGradientModel = {}
 
 TwinDelayedDeepDeterministicPolicyGradientModel.__index = TwinDelayedDeepDeterministicPolicyGradientModel
 

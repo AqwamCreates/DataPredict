5ae900375df17c7bbf5ca2145d5b0aa696562cd6
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-24 12:22:25 +0800

    Update DeepReinforcementLearningActorCriticBaseModel.lua

--- src/RobloxVersion/Models/DeepReinforcementLearningActorCriticBaseModel.lua
+++ src/RobloxVersion/Models/DeepReinforcementLearningActorCriticBaseModel.lua
@@ -28,7 +28,7 @@
 
 local BaseInstance = require(script.Parent.Parent.Cores.BaseInstance)
 
-DeepReinforcementLearningActorCriticBaseModel = {}
+local DeepReinforcementLearningActorCriticBaseModel = {}
 
 DeepReinforcementLearningActorCriticBaseModel.__index = DeepReinforcementLearningActorCriticBaseModel
 

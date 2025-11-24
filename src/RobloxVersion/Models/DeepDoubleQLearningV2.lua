39f461409c6955a02674001cdfda492530ddc2e4
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-24 12:20:05 +0800

    Update DeepDoubleQLearningV2.lua

--- src/RobloxVersion/Models/DeepDoubleQLearningV2.lua
+++ src/RobloxVersion/Models/DeepDoubleQLearningV2.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local DeepReinforcementLearningBaseModel = require(script.Parent.DeepReinforcementLearningBaseModel)
 
-DeepDoubleQLearningModel = {}
+local DeepDoubleQLearningModel = {}
 
 DeepDoubleQLearningModel.__index = DeepDoubleQLearningModel
 

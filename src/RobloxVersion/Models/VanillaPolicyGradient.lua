c7009b928c321c8cf45acef5300f21ebdd4307bc
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-28 11:50:09 +0800

    Update VanillaPolicyGradient.lua

--- src/RobloxVersion/Models/VanillaPolicyGradient.lua
+++ src/RobloxVersion/Models/VanillaPolicyGradient.lua
@@ -30,186 +30,282 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local DeepReinforcementLearningActorCriticBaseModel = require(script.Parent.DeepReinforcementLearningActorCriticBaseModel)
 
-local VanillaPolicyGradientModel = {}
+local SoftActorCriticModel = {}
 
-VanillaPolicyGradientModel.__index = VanillaPolicyGradientModel
+SoftActorCriticModel.__index = SoftActorCriticModel
 
-setmetatable(VanillaPolicyGradientModel, DeepReinforcementLearningActorCriticBaseModel)
+setmetatable(SoftActorCriticModel, DeepReinforcementLearningActorCriticBaseModel)
 
-local function calculateProbability(valueVector)
+local defaultAlpha = 0.1
 
-	local highestActionValue = AqwamTensorLibrary:findMaximumValue(valueVector)
+local defaultAveragingRate = 0.995
 
-	local subtractedZVector = AqwamTensorLibrary:subtract(valueVector, highestActionValue)
+local function rateAverageModelParameters(averagingRate, TargetModelParameters, PrimaryModelParameters)
+
+	local averagingRateComplement = 1 - averagingRate
+
+	for layer = 1, #TargetModelParameters, 1 do
+
+		local TargetModelParametersPart = AqwamTensorLibrary:multiply(averagingRate, TargetModelParameters[layer])
+
+		local PrimaryModelParametersPart = AqwamTensorLibrary:multiply(averagingRateComplement, PrimaryModelParameters[layer])
+
+		TargetModelParameters[layer] = AqwamTensorLibrary:add(TargetModelParametersPart, PrimaryModelParametersPart)
+
+	end
+
+	return TargetModelParameters
+
+end
+
+local function calculateCategoricalProbability(valueTensor)
+
+	local highestActionValue = AqwamTensorLibrary:findMaximumValue(valueTensor)
+
+	local subtractedZVector = AqwamTensorLibrary:subtract(valueTensor, highestActionValue)
 
 	local exponentActionVector = AqwamTensorLibrary:applyFunction(math.exp, subtractedZVector)
 
 	local exponentActionSumVector = AqwamTensorLibrary:sum(exponentActionVector, 2)
 
-	local targetActionVector = AqwamTensorLibrary:divide(exponentActionVector, exponentActionSumVector)
+	local targetActionTensor = AqwamTensorLibrary:divide(exponentActionVector, exponentActionSumVector)
 
-	return targetActionVector
+	return targetActionTensor
 
 end
 
-local function calculateRewardToGo(rewardHistory, discountFactor)
+local function calculateDiagonalGaussianProbability(meanVector, standardDeviationVector, noiseVector)
 
-	local rewardToGoArray = {}
+	local valueVectorPart1 = AqwamTensorLibrary:multiply(standardDeviationVector, noiseVector)
 
-	local discountedReward = 0
+	local valueVector = AqwamTensorLibrary:add(meanVector, valueVectorPart1)
 
-	for h = #rewardHistory, 1, -1 do
+	local zScoreVectorPart1 = AqwamTensorLibrary:subtract(valueVector, meanVector)
 
-		discountedReward = rewardHistory[h] + (discountFactor * discountedReward)
+	local zScoreVector = AqwamTensorLibrary:divide(zScoreVectorPart1, standardDeviationVector)
 
-		table.insert(rewardToGoArray, 1, discountedReward)
+	local squaredZScoreVector = AqwamTensorLibrary:power(zScoreVector, 2)
+
+	local logValueVectorPart1 = AqwamTensorLibrary:logarithm(standardDeviationVector)
+
+	local logValueVectorPart2 = AqwamTensorLibrary:multiply(2, logValueVectorPart1)
+
+	local logValueVectorPart3 = AqwamTensorLibrary:add(squaredZScoreVector, logValueVectorPart2)
+
+	local logValueVector = AqwamTensorLibrary:add(logValueVectorPart3, math.log(2 * math.pi))
+
+	logValueVector = AqwamTensorLibrary:multiply(-0.5, logValueVector)
+
+	return logValueVector
+
+end
+
+function SoftActorCriticModel.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local NewSoftActorCritic = DeepReinforcementLearningActorCriticBaseModel.new(parameterDictionary)
+	
+	setmetatable(NewSoftActorCritic, SoftActorCriticModel)
+	
+	NewSoftActorCritic:setName("SoftActorCritic")
+	
+	NewSoftActorCritic.alpha = parameterDictionary.alpha or defaultAlpha
+	
+	NewSoftActorCritic.averagingRate = parameterDictionary.averagingRate or defaultAveragingRate
+	
+	NewSoftActorCritic.CriticModelParametersArray = parameterDictionary.CriticModelParametersArray or {}
+	
+	NewSoftActorCritic:setCategoricalUpdateFunction(function(previousFeatureVector, previousAction, rewardValue, currentFeatureVector, currentAction, terminalStateValue)
+		
+		local ActorModel = NewSoftActorCritic.ActorModel
+		
+		local CriticModel = NewSoftActorCritic.CriticModel
+
+		local previousActionVector = ActorModel:forwardPropagate(previousFeatureVector, true)
+		
+		local currentActionVector = ActorModel:forwardPropagate(currentFeatureVector, true)
+
+		local previousActionProbabilityVector = calculateCategoricalProbability(previousActionVector)
+		
+		local currentActionProbabilityVector = calculateCategoricalProbability(currentActionVector)
+
+		local previousLogActionProbabilityVector = AqwamTensorLibrary:logarithm(previousActionProbabilityVector)
+		
+		local currentLogActionProbabilityVector = AqwamTensorLibrary:logarithm(currentActionProbabilityVector)
+		
+		return NewSoftActorCritic:update(previousFeatureVector, previousLogActionProbabilityVector, currentLogActionProbabilityVector, previousAction, rewardValue, currentFeatureVector, terminalStateValue)
+		
+	end)
+	
+	NewSoftActorCritic:setDiagonalGaussianUpdateFunction(function(previousFeatureVector, previousActionMeanVector, previousActionStandardDeviationVector, previousActionNoiseVector, rewardValue, currentFeatureVector, currentActionMeanVector, terminalStateValue)
+		
+		if (not previousActionNoiseVector) then previousActionNoiseVector = AqwamTensorLibrary:createRandomNormalTensor({1, #previousActionMeanVector[1]}) end
+		
+		local dimensionSizeArray = AqwamTensorLibrary:getDimensionSizeArray(previousActionNoiseVector)
+		
+		local currentActionNoiseVector = AqwamTensorLibrary:createRandomUniformTensor(dimensionSizeArray)
+		
+		local previousLogActionProbabilityVector = calculateDiagonalGaussianProbability(previousActionMeanVector, previousActionStandardDeviationVector, previousActionStandardDeviationVector)
+		
+		local currentLogActionProbabilityVector = calculateDiagonalGaussianProbability(currentActionMeanVector, previousActionStandardDeviationVector, currentActionNoiseVector)
+		
+		return NewSoftActorCritic:update(previousFeatureVector, previousLogActionProbabilityVector, currentLogActionProbabilityVector, nil, rewardValue, currentFeatureVector, terminalStateValue)
+		
+	end)
+	
+	NewSoftActorCritic:setEpisodeUpdateFunction(function(terminalStateValue) end)
+	
+	NewSoftActorCritic:setResetFunction(function() end)
+	
+	return NewSoftActorCritic
+	
+end
+
+function SoftActorCriticModel:update(previousFeatureVector, previousLogActionProbabilityVector, currentLogActionProbabilityVector, previousAction, rewardValue, currentFeatureVector, terminalStateValue)
+	
+	local CriticModelParametersArray = self.CriticModelParametersArray
+	
+	local CriticModel = self.CriticModel
+
+	local ActorModel = self.ActorModel
+
+	local alpha = self.alpha
+	
+	local averagingRate = self.averagingRate
+	
+	local PreviousCriticModelParametersArray = {}
+	
+	local previousLogActionProbabilityValue
+	
+	if (previousAction) then
+		
+		local ClassesList = ActorModel:getClassesList()
+		
+		local actionIndex = table.find(ClassesList, previousAction)
+		
+		previousLogActionProbabilityValue = previousLogActionProbabilityVector[1][actionIndex]
+		
+	else
+		
+		previousLogActionProbabilityValue = AqwamTensorLibrary:sum(previousLogActionProbabilityVector)
+		
+	end
+
+	local currentCriticValueArray = {}
+
+	for i = 1, 2, 1 do 
+
+		CriticModel:setModelParameters(CriticModelParametersArray[i])
+
+		currentCriticValueArray[i] = CriticModel:forwardPropagate(currentFeatureVector)[1][1] 
+		
+		local CriticModelParameters = CriticModel:getModelParameters(true)
+		
+		PreviousCriticModelParametersArray[i] = CriticModelParameters
 
 	end
 
-	return rewardToGoArray
+	local minimumCurrentCriticValue = math.min(table.unpack(currentCriticValueArray))
+	
+	local yValuePart1 = (1 - terminalStateValue) * (minimumCurrentCriticValue - (alpha * previousLogActionProbabilityValue))
+
+	local yValue = rewardValue + (self.discountFactor * yValuePart1)
+
+	local temporalDifferenceErrorVector = AqwamTensorLibrary:createTensor({1, 2}, 0)
+	
+	local previousCriticValueArray = {}
+
+	for i = 1, 2, 1 do 
+
+		CriticModel:setModelParameters(PreviousCriticModelParametersArray[i], true)
+
+		local previousCriticValue = CriticModel:forwardPropagate(previousFeatureVector, true)[1][1] 
+
+		local criticLoss = previousCriticValue - yValue
+
+		temporalDifferenceErrorVector[1][i] = -criticLoss -- We perform gradient descent here, so the critic loss is negated so that it can be used as temporal difference value.
+		
+		previousCriticValueArray[i] = previousCriticValue
+
+		CriticModel:update(criticLoss, true)
+		
+		local TargetModelParameters = CriticModel:getModelParameters(true)
+		
+		CriticModelParametersArray[i] = rateAverageModelParameters(averagingRate, TargetModelParameters, PreviousCriticModelParametersArray[i])
+
+	end
+	
+	local minimumCurrentCriticValue = math.min(table.unpack(previousCriticValueArray))
+
+	local actorLossVector = AqwamTensorLibrary:multiply(alpha, previousLogActionProbabilityVector)
+
+	actorLossVector = AqwamTensorLibrary:subtract(minimumCurrentCriticValue, actorLossVector)
+	
+	actorLossVector = AqwamTensorLibrary:unaryMinus(actorLossVector)
+
+	ActorModel:forwardPropagate(previousFeatureVector, true)
+
+	ActorModel:update(actorLossVector, true)
+	
+	return temporalDifferenceErrorVector
+	
+end
+
+function SoftActorCriticModel:setCrtiticModelParameters1(CriticModelParameters1, doNotDeepCopy)
+
+	if (doNotDeepCopy) then
+
+		self.CriticModelParametersArray[1] = CriticModelParameters1
+
+	else
+
+		self.CriticModelParametersArray[1] = self:deepCopyTable(CriticModelParameters1)
+
+	end
 
 end
 
-function VanillaPolicyGradientModel.new(parameterDictionary)
+function SoftActorCriticModel:setCriticModelParameters2(CriticModelParameters2, doNotDeepCopy)
 
-	local NewVanillaPolicyGradientModel = DeepReinforcementLearningActorCriticBaseModel.new(parameterDictionary)
+	if (doNotDeepCopy) then
 
-	setmetatable(NewVanillaPolicyGradientModel, VanillaPolicyGradientModel)
+		self.CriticModelParametersArray[2] = CriticModelParameters2
 
-	NewVanillaPolicyGradientModel:setName("VanillaPolicyGradient")
+	else
 
-	local featureVectorHistory = {}
+		self.CriticModelParametersArray[2] = self:deepCopyTable(CriticModelParameters2)
 
-	local actionProbabilityGradientVectorHistory = {}
-
-	local rewardValueHistory = {}
-
-	local advantageValueHistory = {}
-
-	NewVanillaPolicyGradientModel:setCategoricalUpdateFunction(function(previousFeatureVector, previousAction, rewardValue, currentFeatureVector, currentAction, terminalStateValue)
-
-		local ActorModel = NewVanillaPolicyGradientModel.ActorModel
-
-		local CriticModel = NewVanillaPolicyGradientModel.CriticModel
-
-		local actionVector = ActorModel:forwardPropagate(previousFeatureVector)
-
-		local actionProbabilityVector = calculateProbability(actionVector)
-
-		local previousCriticValue = CriticModel:forwardPropagate(previousFeatureVector)[1][1]
-
-		local currentCriticValue = CriticModel:forwardPropagate(currentFeatureVector)[1][1]
-
-		local advantageValue = rewardValue + (NewVanillaPolicyGradientModel.discountFactor * currentCriticValue) - previousCriticValue
-
-		local ClassesList = ActorModel:getClassesList()
-
-		local classIndex = table.find(ClassesList, previousAction)
-
-		local actionProbabilityGradientVector = {}
-
-		for i, _ in ipairs(ClassesList) do
-
-			actionProbabilityGradientVector[i] = (((i == classIndex) and 1) or 0) - actionProbabilityVector[1][i]
-
-		end
-
-		actionProbabilityGradientVector = {actionProbabilityGradientVector}
-
-		table.insert(featureVectorHistory, previousFeatureVector)
-
-		table.insert(actionProbabilityGradientVectorHistory, actionProbabilityGradientVector)
-
-		table.insert(rewardValueHistory, rewardValue)
-
-		table.insert(advantageValueHistory, advantageValue)
-
-		return advantageValue
-
-	end)
-
-	NewVanillaPolicyGradientModel:setDiagonalGaussianUpdateFunction(function(previousFeatureVector, previousActionMeanVector, previousActionStandardDeviationVector, previousActionNoiseVector, rewardValue, currentFeatureVector, currentActionMeanVector, terminalStateValue)
-
-		if (not previousActionNoiseVector) then previousActionNoiseVector = AqwamTensorLibrary:createRandomNormalTensor({1, #previousActionMeanVector[1]}) end
-
-		local CriticModel = NewVanillaPolicyGradientModel.CriticModel
-
-		local actionVectorPart1 = AqwamTensorLibrary:multiply(previousActionStandardDeviationVector, previousActionNoiseVector)
-
-		local actionVector = AqwamTensorLibrary:add(previousActionMeanVector, actionVectorPart1)
-
-		local actionProbabilityGradientVectorPart1 = AqwamTensorLibrary:subtract(actionVector, previousActionMeanVector)
-
-		local actionProbabilityGradientVectorPart2 = AqwamTensorLibrary:power(previousActionStandardDeviationVector, 2)
-
-		local actionProbabilityGradientVector = AqwamTensorLibrary:divide(actionProbabilityGradientVectorPart1, actionProbabilityGradientVectorPart2)
-
-		local previousCriticValue = CriticModel:forwardPropagate(previousFeatureVector)[1][1]
-
-		local currentCriticValue = CriticModel:forwardPropagate(currentFeatureVector)[1][1]
-
-		local advantageValue = rewardValue + (NewVanillaPolicyGradientModel.discountFactor * currentCriticValue) - previousCriticValue
-
-		table.insert(featureVectorHistory, previousFeatureVector)
-
-		table.insert(actionProbabilityGradientVectorHistory, actionProbabilityGradientVector)
-
-		table.insert(rewardValueHistory, rewardValue)
-
-		table.insert(advantageValueHistory, advantageValue)
-
-		return advantageValue
-
-	end)
-
-	NewVanillaPolicyGradientModel:setEpisodeUpdateFunction(function(terminalStateValue)
-
-		local ActorModel = NewVanillaPolicyGradientModel.ActorModel
-
-		local CriticModel = NewVanillaPolicyGradientModel.CriticModel
-
-		for h, featureVector in ipairs(featureVectorHistory) do
-
-			local advantageValue = advantageValueHistory[h]
-
-			advantageValue = AqwamTensorLibrary:unaryMinus(advantageValue)
-
-			local actorLossVector = AqwamTensorLibrary:multiply(actionProbabilityGradientVectorHistory[h], advantageValue)
-
-			CriticModel:forwardPropagate(featureVector, true)
-
-			ActorModel:forwardPropagate(featureVector, true)
-
-			CriticModel:update(advantageValue, true)
-
-			ActorModel:update(actorLossVector, true)
-
-		end
-
-		table.clear(featureVectorHistory)
-
-		table.clear(actionProbabilityGradientVectorHistory)
-
-		table.clear(rewardValueHistory)
-
-		table.clear(advantageValueHistory)
-
-	end)
-
-	NewVanillaPolicyGradientModel:setResetFunction(function()
-
-		table.clear(featureVectorHistory)
-
-		table.clear(actionProbabilityGradientVectorHistory)
-
-		table.clear(rewardValueHistory)
-
-		table.clear(advantageValueHistory)
-
-	end)
-
-	return NewVanillaPolicyGradientModel
+	end
 
 end
 
-return VanillaPolicyGradientModel
+function SoftActorCriticModel:getCriticModelParameters1(doNotDeepCopy)
+
+	if (doNotDeepCopy) then
+
+		return self.CriticModelParametersArray[1]
+
+	else
+
+		return self:deepCopyTable(self.CriticModelParametersArray[1])
+
+	end
+
+end
+
+function SoftActorCriticModel:getCriticModelParameters2(doNotDeepCopy)
+
+	if (doNotDeepCopy) then
+
+		return self.CriticModelParametersArray[2]
+
+	else
+
+		return self:deepCopyTable(self.CriticModelParametersArray[2])
+
+	end
+
+end
+
+return SoftActorCriticModel

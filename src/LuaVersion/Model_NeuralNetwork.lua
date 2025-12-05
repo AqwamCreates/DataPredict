90d9dfa3d61516c268c63baeff6836d06487baf5
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-12-05 15:58:17 +0800

    Update Model_NeuralNetwork.lua

--- src/LuaVersion/Model_NeuralNetwork.lua
+++ src/LuaVersion/Model_NeuralNetwork.lua
@@ -28,7 +28,7 @@
 
 local AqwamTensorLibrary = require("AqwamTensorLibrary")
 
-local GradientMethodBaseModel = require("Model_GradientMethodBaseModel")
+local GradientMethodBaseModel = require("GradientMethodBaseModel")
 
 local ZTableFunction = require("Core_ZTableFunction")
 

9907d871614cbf8ceb97a9a27d80c467048206f1
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-12-14 21:10:33 +0800

    Update Model_NegativeBinomialRegression.lua

--- src/LuaVersion/Model_NegativeBinomialRegression.lua
+++ src/LuaVersion/Model_NegativeBinomialRegression.lua
@@ -44,7 +44,7 @@ local defaultDispersion = 0.5 -- alpha
 
 local defaultEpsilon = 1e-14
 
--- Approximate Gamma function (Stirling's approximation).
+-- Approximate gamma function (Stirling's approximation).
 
 local function approximateGammaFunction(x)
 	

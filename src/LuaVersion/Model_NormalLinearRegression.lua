22d5274b68cae8edfc1203f70f14b6244b9b2e1b
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-01-04 04:40:23 +0800

    Update Model_NormalLinearRegression.lua

--- src/LuaVersion/Model_NormalLinearRegression.lua
+++ src/LuaVersion/Model_NormalLinearRegression.lua
@@ -26,9 +26,9 @@
 
 --]]
 
-local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+local AqwamTensorLibrary = require("AqwamTensorLibrary")
 
-local BaseModel = require(script.Parent.BaseModel)
+local BaseModel = require("Model_BaseModel")
 
 local NormalLinearRegressionModel = {}
 

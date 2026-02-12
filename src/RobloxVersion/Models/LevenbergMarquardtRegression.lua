0267a0f1f896dd5245265edffe050c7dbdb8b3f6
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-02-12 22:01:49 +0800

    Create LevenbergMarquardtRegression.lua

---
+++ src/RobloxVersion/Models/LevenbergMarquardtRegression.lua
@@ -0,0 +1,191 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+
+local IterativeMethodBaseModel = require(script.Parent.IterativeMethodBaseModel)
+
+local LevenbergMarquardtRegressionModel = {}
+
+LevenbergMarquardtRegressionModel.__index = LevenbergMarquardtRegressionModel
+
+setmetatable(LevenbergMarquardtRegressionModel, IterativeMethodBaseModel)
+
+local defaultMaximumNumberOfIterations = 500
+
+local defaultLambda = 0
+
+local defaultLearningRate = 1
+
+local defaultModelParametersInitializationMode = "Zero"
+
+function LevenbergMarquardtRegressionModel.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	parameterDictionary.maximumNumberOfIterations = parameterDictionary.maximumNumberOfIterations or defaultMaximumNumberOfIterations
+
+	parameterDictionary.modelParametersInitializationMode = parameterDictionary.modelParametersInitializationMode or defaultModelParametersInitializationMode
+
+	local NewLevenbergMarquardtRegressionModel = IterativeMethodBaseModel.new(parameterDictionary)
+
+	setmetatable(NewLevenbergMarquardtRegressionModel, LevenbergMarquardtRegressionModel)
+
+	NewLevenbergMarquardtRegressionModel:setName("LevenbergMarquardtRegression")
+	
+	NewLevenbergMarquardtRegressionModel.lambda = parameterDictionary.lambda or defaultLambda
+	
+	NewLevenbergMarquardtRegressionModel.learningRate = parameterDictionary.learningRate or defaultLearningRate
+
+	return NewLevenbergMarquardtRegressionModel
+
+end
+
+function LevenbergMarquardtRegressionModel:train(featureMatrix, labelVector)
+	
+	local numberOfData = #featureMatrix
+
+	if (numberOfData ~= #labelVector) then error("The feature matrix and the label vector does not contain the same number of rows.") end
+	
+	local numberOfFeatures = #featureMatrix[1]
+	
+	local betaVector = self.ModelParameters
+
+	if (betaVector) then
+
+		if (numberOfFeatures ~= #betaVector) then error("The number of features are not the same as the model parameters.") end
+
+	else
+
+		betaVector = self:initializeMatrixBasedOnMode({numberOfFeatures, 1})
+
+	end
+	
+	local maximumNumberOfIterations = self.maximumNumberOfIterations
+	
+	local lambda = self.lambda
+	
+	local learningRate = self.learningRate
+	
+	local lambdaIdentityMatrix = AqwamTensorLibrary:createIdentityTensor({numberOfFeatures, numberOfFeatures}, lambda)
+	
+	local diagonalMatrix = AqwamTensorLibrary:createIdentityTensor({numberOfData, numberOfData})
+	
+	local costArray = {}
+	
+	local numberOfIterations = 0
+	
+	local transposedFeatureMatrix
+	
+	local dotProductFeatureMatrix
+	
+	local inverseDotProductMatrix
+	
+	local responseVector
+	
+	local errorVector
+	
+	local squaredErrorVector
+	
+	local betaChangeVector
+	
+	local cost
+	
+	repeat
+
+		numberOfIterations = numberOfIterations + 1
+
+		self:iterationWait()
+		
+		transposedFeatureMatrix = AqwamTensorLibrary:transpose(featureMatrix)
+		
+		dotProductFeatureMatrix = AqwamTensorLibrary:dotProduct(transposedFeatureMatrix, diagonalMatrix, featureMatrix)
+		
+		dotProductFeatureMatrix = AqwamTensorLibrary:add(dotProductFeatureMatrix, lambdaIdentityMatrix)
+		
+		inverseDotProductMatrix = AqwamTensorLibrary:inverse(dotProductFeatureMatrix)
+		
+		responseVector = AqwamTensorLibrary:dotProduct(featureMatrix, betaVector)
+		
+		errorVector = AqwamTensorLibrary:subtract(labelVector, responseVector)
+		
+		betaChangeVector = AqwamTensorLibrary:dotProduct(inverseDotProductMatrix, transposedFeatureMatrix, diagonalMatrix, errorVector)
+		
+		betaChangeVector = AqwamTensorLibrary:multiply(learningRate, betaChangeVector)
+		
+		betaVector = AqwamTensorLibrary:add(betaVector, betaChangeVector)
+		
+		cost = self:calculateCostWhenRequired(numberOfIterations, function()
+			
+			squaredErrorVector = AqwamTensorLibrary:power(errorVector, 2)
+
+			return AqwamTensorLibrary:sum(squaredErrorVector)
+
+		end)
+
+		if (cost) then 
+
+			table.insert(costArray, cost)
+
+			self:printNumberOfIterationsAndCost(numberOfIterations, cost)
+
+		end
+		
+	until (numberOfIterations == maximumNumberOfIterations) or self:checkIfTargetCostReached(cost) or self:checkIfConverged(cost)
+	
+	if (self.isOutputPrinted) then
+
+		if (cost == math.huge) then warn("The model diverged.") end
+
+		if (cost ~= cost) then warn("The model produced nan (not a number) values.") end
+
+	end
+
+	self.ModelParameters = betaVector
+	
+	return costArray
+
+end
+
+function LevenbergMarquardtRegressionModel:predict(featureMatrix)
+
+	local betaVector = self.ModelParameters
+
+	if (not betaVector) then
+
+		betaVector = self:initializeMatrixBasedOnMode({#featureMatrix[1], 1})
+
+		self.ModelParameters = betaVector
+
+	end
+
+	return AqwamTensorLibrary:dotProduct(featureMatrix, betaVector)
+
+end
+
+return LevenbergMarquardtRegressionModel

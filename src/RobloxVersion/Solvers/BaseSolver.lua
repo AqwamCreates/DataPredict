1017aaee3314f872523ebf93b3b2a40d98f2bde7
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-02-16 12:40:35 +0800

    Create BaseSolver.lua

---
+++ src/RobloxVersion/Solvers/BaseSolver.lua
@@ -0,0 +1,89 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+
+local BaseInstance = require(script.Parent.Parent.Cores.BaseInstance)
+
+local BaseSolver = {}
+
+BaseSolver.__index = BaseSolver
+
+setmetatable(BaseSolver, BaseInstance)
+
+function BaseSolver.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local NewBaseSolver = BaseInstance.new()
+	
+	setmetatable(NewBaseSolver, BaseSolver)
+	
+	NewBaseSolver:setName("BaseSolver")
+	
+	NewBaseSolver:setClassName("Solver")
+	
+	NewBaseSolver.calculateFunction = parameterDictionary.calculateFunction
+	
+	NewBaseSolver.cache = parameterDictionary.cache
+	
+	return NewBaseSolver
+	
+end
+
+function BaseSolver:setCalculateFunction(calculateFunction)
+	
+	self.calculateFunction = calculateFunction
+	
+end
+
+function BaseSolver:calculate(matrix, firstDerivativeMatrix, firstDerivativeLossMatrix)
+	
+	return self.calculateFunction(matrix, firstDerivativeMatrix, firstDerivativeLossMatrix)
+	
+end
+
+function BaseSolver:setCache(cache)
+	
+	self.cache = cache
+	
+end
+
+function BaseSolver:getCache(cache)
+
+	return self.cache
+
+end
+
+function BaseSolver:clearCache()
+
+	self.cache = nil
+
+end
+
+return BaseSolver

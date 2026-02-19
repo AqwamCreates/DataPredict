8fa686be32de3f6ac61e280341e2f96590717731
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-02-19 18:27:43 +0800

    Update Jacobi.lua

--- src/RobloxVersion/Solvers/Jacobi.lua
+++ src/RobloxVersion/Solvers/Jacobi.lua
@@ -42,6 +42,84 @@ local function safeguardedInversionFunction(numerator)
 	
 end
 
+local function rearrangeMatrixToDominantDiagonalMatrix(matrix)
+	
+	local dimensionSizeArray = AqwamTensorLibrary:getDimensionSizeArray(matrix)
+	
+	local numberOfRows = dimensionSizeArray[1]
+	
+	local numberOfColumns = dimensionSizeArray[2]
+	
+	local dominantDiagonalMatrix = {}
+	
+	local diagonaMatrixIndexArray = {}
+	
+	local duplicateValueBlacklistArray = {}
+	
+	local maximumAbsoluteValue
+	
+	local columnIndexWithMaximumValue
+	
+	local absoluteValue
+	
+	local sumAbsoluteValue
+	
+	for rowIndex, unwrappedVector in ipairs(matrix) do
+		
+		maximumAbsoluteValue = 0
+		
+		columnIndexWithMaximumValue = nil
+		
+		for primaryColumnIndex, value in ipairs(unwrappedVector) do
+			
+			absoluteValue = math.abs(value)
+			
+			sumAbsoluteValue = 0
+			
+			for secondaryColumnIndex, value in ipairs(unwrappedVector) do
+				
+				if (secondaryColumnIndex ~= primaryColumnIndex) then sumAbsoluteValue = sumAbsoluteValue + math.abs(value) end
+				
+			end
+			
+			if (absoluteValue > sumAbsoluteValue) then
+				
+				if (absoluteValue > maximumAbsoluteValue) then
+					
+					maximumAbsoluteValue = absoluteValue
+
+					columnIndexWithMaximumValue = primaryColumnIndex
+					
+				end
+				
+			end
+			
+		end
+		
+		diagonaMatrixIndexArray[rowIndex] = columnIndexWithMaximumValue
+		
+	end
+	
+	for i, value in ipairs(diagonaMatrixIndexArray) do
+		
+		if (table.find(duplicateValueBlacklistArray, value)) then return matrix end
+		
+		table.insert(duplicateValueBlacklistArray, value)
+		
+	end
+	
+	for rowIndex, unwrappedVector in ipairs(matrix) do
+		
+		columnIndexWithMaximumValue = diagonaMatrixIndexArray[rowIndex]
+		
+		dominantDiagonalMatrix[columnIndexWithMaximumValue] = unwrappedVector
+		
+	end
+	
+	return dominantDiagonalMatrix
+	
+end
+
 function JacobiSolver.new(parameterDictionary)
 	
 	local NewJacobiSolver = BaseSolver.new(parameterDictionary)
@@ -79,6 +157,105 @@ function JacobiSolver.new(parameterDictionary)
 		local diagonalMatrix
 
 		if (not aMatrix) then
+
+			local transposedFirstDerivativeMatrix = AqwamTensorLibrary:transpose(firstDerivativeMatrix)
+
+			aMatrix = AqwamTensorLibrary:dotProduct(transposedFirstDerivativeMatrix, firstDerivativeMatrix)
+			
+			aMatrix = rearrangeMatrixToDominantDiagonalMatrix(aMatrix)
+			
+			if (isLinearInput) then cache[2] = aMatrix end
+
+		end
+		
+		if (inverseDiagonalMatrix) then
+			
+			if (not isLinearInput) then 
+				
+				diagonalMatrix = AqwamTensorLibrary:multiply(diagonalMatrix, aMatrix)
+
+				inverseDiagonalMatrix = AqwamTensorLibrary:applyFunction(safeguardedInversionFunction, diagonalMatrix)
+				
+			end
+		
+		else
+			
+			diagonalMatrix = AqwamTensorLibrary:createIdentityTensor(weightMatrixDimensionSizeArray)
+			
+			if (isLinearInput) then
+
+				diagonalMatrix = AqwamTensorLibrary:multiply(diagonalMatrix, aMatrix)
+				
+				inverseDiagonalMatrix = AqwamTensorLibrary:applyFunction(safeguardedInversionFunction, diagonalMatrix)
+
+				cache[3] = diagonalMatrix
+
+			else
+
+				cache[3] = diagonalMatrix
+
+				diagonalMatrix = AqwamTensorLibrary:multiply(diagonalMatrix, aMatrix)
+				
+				inverseDiagonalMatrix = AqwamTensorLibrary:applyFunction(safeguardedInversionFunction, diagonalMatrix)
+
+			end
+			
+		end
+		
+		if (lAndUMatrix) then
+			
+			if (not isLinearInput) then
+				
+				lAndUMatrix = AqwamTensorLibrary:multiply(lAndUMatrix, aMatrix)
+				
+				lAndUMatrix = AqwamTensorLibrary:dotProduct(lAndUMatrix, weightMatrix)
+				
+			end
+			
+		else
+			
+			local upperTriangularTensor = AqwamTensorLibrary:createUpperTriangularTensor(weightMatrixDimensionSizeArray, 0, 1)
+
+			local lowerTriangularTensor = AqwamTensorLibrary:createLowerTriangularTensor(weightMatrixDimensionSizeArray, 0, 1)
+			
+			lAndUMatrix = AqwamTensorLibrary:add(upperTriangularTensor, lowerTriangularTensor)
+			
+			if (isLinearInput) then
+				
+				lAndUMatrix = AqwamTensorLibrary:multiply(lAndUMatrix, aMatrix)
+				
+				lAndUMatrix = AqwamTensorLibrary:dotProduct(lAndUMatrix, weightMatrix)
+				
+				cache[4] = lAndUMatrix
+				
+				
+			else
+				
+				cache[4] = lAndUMatrix
+				
+				lAndUMatrix = AqwamTensorLibrary:multiply(lAndUMatrix, aMatrix)
+				
+				lAndUMatrix = AqwamTensorLibrary:dotProduct(lAndUMatrix, weightMatrix)
+				
+			end
+			
+		end
+		
+		local newWeightMatrix = AqwamTensorLibrary:subtract(bMatrix, lAndUMatrix)
+
+		newWeightMatrix = AqwamTensorLibrary:dotProduct(inverseDiagonalMatrix, newWeightMatrix)
+		
+		NewJacobiSolver.cache = cache
+		
+		return AqwamTensorLibrary:subtract(newWeightMatrix, weightMatrix)
+		
+	end)
+	
+	return NewJacobiSolver
+	
+end
+
+return JacobiSolver		if (not aMatrix) then
 
 			local transposedFirstDerivativeMatrix = AqwamTensorLibrary:transpose(firstDerivativeMatrix)
 

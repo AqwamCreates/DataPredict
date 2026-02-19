506380263b68857e185c982acff627c146228ca9
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-02-19 15:34:36 +0800

    Add JacobiSolver implementation in Jacobi.lua

---
+++ src/RobloxVersion/Solvers/Jacobi.lua
@@ -0,0 +1,178 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Machine, Deep And Reinforcement Learning Library (DataPredict)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+
+local BaseSolver = require(script.Parent.BaseSolver)
+
+local JacobiSolver = {}
+
+JacobiSolver.__index = JacobiSolver
+
+setmetatable(JacobiSolver, BaseSolver)
+
+local function safeguardedInversionFunction(numerator)
+	
+	return (numerator == 0 and 0) or (1 / numerator)
+	
+end
+
+function JacobiSolver.new(parameterDictionary)
+	
+	local NewJacobiSolver = BaseSolver.new(parameterDictionary)
+	
+	setmetatable(NewJacobiSolver, JacobiSolver)
+	
+	NewJacobiSolver:setName("Jacobi")
+	
+	NewJacobiSolver:setCalculateFunction(function(weightMatrix, firstDerivativeMatrix, firstDerivativeLossMatrix)
+		
+		-- Can only cache from linear models since the derivative is a feature matrix. Hence, these values are constant.
+		
+		local isNonLinearInput = NewJacobiSolver.isNonLinearInput
+		
+		local isLinearInput = (not isNonLinearInput)
+		
+		local cache = NewJacobiSolver.cache or {}
+		
+		local transposedFirstDerivativeMatrix = (isLinearInput and cache[1]) or AqwamTensorLibrary:transpose(firstDerivativeMatrix)
+		
+		local aMatrix = (isLinearInput and cache[2])
+		
+		local inverseDiagonalMatrix = cache[3]
+		
+		local lAndUMatrix = cache[4]
+		
+		local weightMatrixDimensionSizeArray = AqwamTensorLibrary:getDimensionSizeArray(weightMatrix)
+
+		local numberOfFeatures = weightMatrixDimensionSizeArray[1]
+
+		local numberOfOutputs = weightMatrixDimensionSizeArray[2]
+		
+		local bMatrix = AqwamTensorLibrary:dotProduct(transposedFirstDerivativeMatrix, firstDerivativeLossMatrix)
+		
+		local diagonalMatrix
+
+		if (not aMatrix) then
+
+			local transposedFirstDerivativeMatrix = AqwamTensorLibrary:transpose(firstDerivativeMatrix)
+
+			aMatrix = AqwamTensorLibrary:dotProduct(transposedFirstDerivativeMatrix, firstDerivativeMatrix)
+			
+			if (isLinearInput) then cache[2] = aMatrix end
+
+		end
+		
+		if (inverseDiagonalMatrix) then
+			
+			if (not isLinearInput) then 
+				
+				diagonalMatrix = AqwamTensorLibrary:multiply(diagonalMatrix, aMatrix)
+
+				inverseDiagonalMatrix = AqwamTensorLibrary:applyFunction(safeguardedInversionFunction, diagonalMatrix)
+				
+			end
+		
+		else
+			
+			diagonalMatrix = AqwamTensorLibrary:createIdentityTensor(weightMatrixDimensionSizeArray)
+			
+			if (isLinearInput) then
+
+				diagonalMatrix = AqwamTensorLibrary:multiply(diagonalMatrix, aMatrix)
+				
+				inverseDiagonalMatrix = AqwamTensorLibrary:applyFunction(safeguardedInversionFunction, diagonalMatrix)
+
+				cache[3] = diagonalMatrix
+
+			else
+
+				cache[3] = diagonalMatrix
+
+				diagonalMatrix = AqwamTensorLibrary:multiply(diagonalMatrix, aMatrix)
+				
+				inverseDiagonalMatrix = AqwamTensorLibrary:applyFunction(safeguardedInversionFunction, diagonalMatrix)
+
+			end
+			
+		end
+		
+		if (lAndUMatrix) then
+			
+			if (not isLinearInput) then
+				
+				lAndUMatrix = AqwamTensorLibrary:multiply(lAndUMatrix, aMatrix)
+				
+				lAndUMatrix = AqwamTensorLibrary:dotProduct(lAndUMatrix, weightMatrix)
+				
+			end
+			
+		else
+			
+			local upperTriangularTensor = AqwamTensorLibrary:createUpperTriangularTensor(weightMatrixDimensionSizeArray, 0, 1)
+
+			local lowerTriangularTensor = AqwamTensorLibrary:createLowerTriangularTensor(weightMatrixDimensionSizeArray, 0, 1)
+			
+			lAndUMatrix = AqwamTensorLibrary:add(upperTriangularTensor, lowerTriangularTensor)
+			
+			if (isLinearInput) then
+				
+				lAndUMatrix = AqwamTensorLibrary:multiply(lAndUMatrix, aMatrix)
+				
+				lAndUMatrix = AqwamTensorLibrary:dotProduct(lAndUMatrix, weightMatrix)
+				
+				cache[4] = lAndUMatrix
+				
+				
+			else
+				
+				cache[4] = lAndUMatrix
+				
+				lAndUMatrix = AqwamTensorLibrary:multiply(lAndUMatrix, aMatrix)
+				
+				lAndUMatrix = AqwamTensorLibrary:dotProduct(lAndUMatrix, weightMatrix)
+				
+			end
+			
+		end
+		
+		local newWeightMatrix = AqwamTensorLibrary:subtract(bMatrix, lAndUMatrix)
+
+		newWeightMatrix = AqwamTensorLibrary:dotProduct(inverseDiagonalMatrix, newWeightMatrix)
+		
+		NewJacobiSolver.cache = cache
+		
+		return AqwamTensorLibrary:subtract(newWeightMatrix, weightMatrix)
+		
+	end)
+	
+	return NewJacobiSolver
+	
+end
+
+return JacobiSolver

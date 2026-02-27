04cc1e2694e2c5e90b8091e2d139b835696fd1a7
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-02-27 08:16:08 +0800

    Update OrdinalRegression.lua

--- src/RobloxVersion/Models/OrdinalRegression.lua
+++ src/RobloxVersion/Models/OrdinalRegression.lua
@@ -664,7 +664,7 @@ function OrdinalRegressionModel:train(featureMatrix, labelVector)
 
 		self:update(labelVector, hasBias, true)
 
-	until (numberOfIterations == maximumNumberOfIterations) or self:checkIfTargetCostReached(cost) or self:checkIfConverged(cost)
+	until (numberOfIterations >= maximumNumberOfIterations) or self:checkIfTargetCostReached(cost) or self:checkIfConverged(cost) or self:checkIfNan(cost)
 	
 	if (self.isOutputPrinted) then
 		
@@ -673,6 +673,8 @@ function OrdinalRegressionModel:train(featureMatrix, labelVector)
 		if (cost ~= cost) then warn("The model produced nan (not a number) values.") end
 		
 	end
+	
+	if (self.autoResetConvergenceCheck) then self:resetConvergenceCheck() end
 	
 	if (self.autoResetOptimizers) then
 		

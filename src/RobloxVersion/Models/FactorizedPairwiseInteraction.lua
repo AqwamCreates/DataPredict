d7a9658846727974584c87d0101ddda44fa3fdf7
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-03-19 07:39:14 +0800

    Update FactorizedPairwiseInteraction.lua

--- src/RobloxVersion/Models/FactorizedPairwiseInteraction.lua
+++ src/RobloxVersion/Models/FactorizedPairwiseInteraction.lua
@@ -128,12 +128,6 @@ local lossFunctionList = {
 
 	["MeanAbsoluteError"] = function (h, y) return math.abs(h - y) end,
 
-	["BinaryCrossEntropy"] = function (h, y) return -((y * math.log(h)) + ((1 - y) * math.log(1 - h))) end,
-
-	["HingeLoss"] = function (h, y) return math.max(0, (1 - (h * y))) end,
-
-	["SquaredHingeLoss"] = function (h, y) return math.pow(math.max(0, (1 - (h * y))), 2) end,
-
 	["MeanPoissonDeviance"] = function (h, y) return (2 * (y * math.log(y / h) - y + h)) end,
 
 	["MeanGammaDeviance"] = function (h, y) 
@@ -144,6 +138,14 @@ local lossFunctionList = {
 
 	end,
 
+	["BinaryCrossEntropy"] = function (h, y) return -((y * math.log(h)) + ((1 - y) * math.log(1 - h))) end,
+
+	["HingeLoss"] = function (h, y) return math.max(0, (1 - (h * y))) end,
+
+	["SquaredHingeLoss"] = function (h, y) return math.pow(math.max(0, (1 - (h * y))), 2) end,
+
+
+
 }
 
 local lossFunctionGradientList = {
@@ -151,6 +153,10 @@ local lossFunctionGradientList = {
 	["MeanSquaredError"] = function (h, y) return (2 * (h - y)) end,
 
 	["MeanAbsoluteError"] = function (h, y) return math.sign(h - y) end,
+
+	["MeanPoissonDeviance"] = function (h, y) return (2 * (1 - (y / h))) end,
+
+	["MeanGammaDeviance"] = function (h, y) return (2 * ((h - y) / math.pow(h, 2))) end,
 
 	["BinaryCrossEntropy"] = function (h, y) return ((h - y) / (h * (1 - h))) end,
 
@@ -171,10 +177,6 @@ local lossFunctionGradientList = {
 		return -(2 * y * scale)
 
 	end,
-
-	["MeanPoissonDeviance"] = function (h, y) return (2 * (1 - (y / h))) end,
-
-	["MeanGammaDeviance"] = function (h, y) return (2 * ((h - y) / math.pow(h, 2))) end,
 
 }
 

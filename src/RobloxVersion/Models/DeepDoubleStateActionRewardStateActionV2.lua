ecf0c105122ad8bc03b62b3f5a0a79eafaf97416
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-03-22 00:51:12 +0800

    Update DeepDoubleStateActionRewardStateActionV2.lua

--- src/RobloxVersion/Models/DeepDoubleStateActionRewardStateActionV2.lua
+++ src/RobloxVersion/Models/DeepDoubleStateActionRewardStateActionV2.lua
@@ -84,13 +84,7 @@ function DeepDoubleStateActionRewardStateActionModel.new(parameterDictionary)
 		
 		local PrimaryModelParameters = Model:getModelParameters(true)
 
-		if (not PrimaryModelParameters) then 
-			
-			Model:generateLayers()
-			
-			PrimaryModelParameters = Model:getModelParameters(true)
-			
-		end
+		if (not PrimaryModelParameters) then PrimaryModelParameters = Model:generateLayers() end
 		
 		if (not TargetModelParameters) then TargetModelParameters = PrimaryModelParameters end
 		

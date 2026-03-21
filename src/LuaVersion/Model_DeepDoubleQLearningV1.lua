27c08ffa22d9ec2e53f243ae2bed9e827ced42bc
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-03-22 01:16:56 +0800

    Update Model_DeepDoubleQLearningV1.lua

--- src/LuaVersion/Model_DeepDoubleQLearningV1.lua
+++ src/LuaVersion/Model_DeepDoubleQLearningV1.lua
@@ -107,18 +107,8 @@ end
 function DeepDoubleQLearningModel:loadModelParametersFromModelParametersArray(index)
 
 	local Model = self.Model
-
-	local ModelParametersArray = self.ModelParametersArray
-
-	if (not ModelParametersArray[index]) then
-
-		Model:generateLayers()
-
-		self:saveModelParametersFromModelParametersArray(index)
-
-	end
-
-	local CurrentModelParameters = ModelParametersArray[index]
+	
+	local CurrentModelParameters = self.ModelParametersArray[index] or Model:generateLayers()
 
 	Model:setModelParameters(CurrentModelParameters, true)
 

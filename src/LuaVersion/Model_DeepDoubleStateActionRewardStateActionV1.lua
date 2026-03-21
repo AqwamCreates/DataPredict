dc1b51804233e9f9589008be1e12c7745b51b609
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-03-21 16:41:12 +0800

    Update Model_DeepDoubleStateActionRewardStateActionV1.lua

--- src/LuaVersion/Model_DeepDoubleStateActionRewardStateActionV1.lua
+++ src/LuaVersion/Model_DeepDoubleStateActionRewardStateActionV1.lua
@@ -148,9 +148,11 @@ function DeepDoubleStateActionRewardStateActionModel:generateTemporalDifferenceE
 
 	local currentActionIndex = table.find(ClassesList, currentAction)
 
-	local targetValue = rewardValue + (discountFactor * currentQVector[1][currentActionIndex] * (1 - terminalStateValue))
+	local targetQValue = rewardValue + (discountFactor * currentQVector[1][currentActionIndex] * (1 - terminalStateValue))
+	
+	local previousQValue = previousQVector[1][previousActionIndex] 
 
-	local temporalDifferenceError = targetValue - previousQVector[1][previousActionIndex] 
+	local temporalDifferenceError = targetQValue - previousQValue
 
 	local outputDimensionSizeArray = {1, numberOfClasses}
 

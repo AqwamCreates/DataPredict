e661fb12736aa45ace9ab76b1b1e6c1fdd6a089d
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-03-27 14:57:13 +0800

    Update Model_ProximalPolicyOptimization.lua

--- src/LuaVersion/Model_ProximalPolicyOptimization.lua
+++ src/LuaVersion/Model_ProximalPolicyOptimization.lua
@@ -146,7 +146,7 @@ local function calculateRewardToGo(rewardHistory, discountFactor)
 
 end
 
-local function maskAdvantageValue(ratio, advantage, epsilon)
+local function calculateActorLossValue(ratio, advantage, epsilon, actorGradientValue)
 	
 	local upperRatioValue = 1 + epsilon
 	
@@ -158,7 +158,7 @@ local function maskAdvantageValue(ratio, advantage, epsilon)
 	
 	local canUseNegativeAdvantageValue = (not isAdvantageValuePositive) and (ratio > lowerRatioValue)
 	
-	if (canUsePositiveAdvantageValue) or (canUseNegativeAdvantageValue) then return advantage end
+	if (canUsePositiveAdvantageValue) or (canUseNegativeAdvantageValue) then return -(advantage * ratio * actorGradientValue) end
 	
 	return 0
 
@@ -377,12 +377,6 @@ function ProximalPolicyOptimizationClipModel.new(parameterDictionary)
 		end
 
 		local rewardToGoArray = calculateRewardToGo(rewardValueHistory, discountFactor)
-		
-		local clipFunctionToApply = function(ratioActionProbability)
-
-			return math.clamp(ratioActionProbability, 1 - epsilon, 1 + epsilon)
-
-		end
 
 		for h, featureVector in ipairs(featureVectorHistory) do
 			
@@ -396,11 +390,7 @@ function ProximalPolicyOptimizationClipModel.new(parameterDictionary)
 			
 			local advantageVector = AqwamTensorLibrary:createTensor(outputDimensionSizeArray, advantageValue)
 			
-			local maskedAdvantageValue = AqwamTensorLibrary:applyFunction(maskAdvantageValue, ratioActionProbabilityVector, advantageVector, epsilonVector)
-			
-			local actorLossVector = AqwamTensorLibrary:multiply(maskedAdvantageValue, ratioActionProbabilityVector, actorGradientVector)
-
-			actorLossVector = AqwamTensorLibrary:unaryMinus(actorLossVector)
+			local actorLossVector = AqwamTensorLibrary:applyFunction(calculateActorLossValue, ratioActionProbabilityVector, advantageVector, epsilonVector, actorGradientVector)
 
 			ActorModel:forwardPropagate(featureVector, true)
 
